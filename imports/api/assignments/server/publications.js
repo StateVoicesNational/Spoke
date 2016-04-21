d7671ab7fc9e6971b24a6f69f0472d23eaaa99c8
Saikat Chakrabarti <saikat1@gmail.com>
2016-04-21 16:37:54 -0400

    Add airbnb eslint styles

--- imports/api/assignments/server/publications.js
+++ imports/api/assignments/server/publications.js
@@ -2,12 +2,12 @@ import { Meteor } from 'meteor/meteor';
 
 import { Assignments } from '../assignments.js';
 
-Meteor.publish('assignments', function assignmentsPublication() {
+Meteor.publish('assignments', () => {
   // TODO: actually filter correctly and return public fields only
   return Assignments.find({});
 });
 
-Meteor.publish('assignment', function (assignmentId) {
+Meteor.publish('assignment', (assignmentId) => {
   // TODO: check if you have access
   console.log("here in assignment fetch");
   return Assignments.find({ _id: assignmentId });
