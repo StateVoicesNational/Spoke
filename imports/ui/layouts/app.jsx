6850772bb97205acfc23966d39916ffd513eb6c9
Saikat Chakrabarti <saikat1@gmail.com>
2016-04-21 16:06:52 -0400

    Fix a typo in a filename and add eslint

--- imports/ui/layouts/app.jsx
+++ imports/ui/layouts/app.jsx
@@ -3,4 +3,10 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 const muiTheme = getMuiTheme();
 
-export const App = ({content}) => <div><MuiThemeProvider muiTheme={muiTheme}>{content()}</MuiThemeProvider></div>
+export const App = ({content}) => (
+  <div>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      {content()}
+    </MuiThemeProvider>
+  </div>
+)
