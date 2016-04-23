0915ae9d1ce37492996ef6d1a716dbbf27ee0b41
Saikat Chakrabarti <saikat1@gmail.com>
2016-04-23 10:06:51 -0400

    Lint routes.jsx

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,32 +1,28 @@
-import {FlowRouter} from 'meteor/kadira:flow-router'
+import { FlowRouter } from 'meteor/kadira:flow-router'
 import React from 'react'
 import { mount } from 'react-mounter'
-
 import AssignmentContainer from '../../ui/containers/assignment_container'
-import AppContainer from '../../ui/containers/app_container'
-import {App} from '../../ui/layouts/app'
+import { App } from '../../ui/layouts/app'
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectTapEventPlugin from 'react-tap-event-plugin'
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+injectTapEventPlugin()
 
 FlowRouter.route('/', {
   name: 'texting',
-  action: function(params) {
+  action: (params) => {
     mount(App, {
-      content: () =>  <AssignmentContainer id={params.id}/>
+      content: () => <AssignmentContainer id={params.id} />
     })
   }
 })
 
 FlowRouter.route('/assignments/:id', {
   name: 'assignment',
-  action: function(params) {
-    console.log(params);
-
+  action: (params) => {
     mount(App, {
-      content: () =>  <AssignmentContainer id={params.id}/>
+      content: () => <AssignmentContainer id={params.id} />
     })
   }
 })
