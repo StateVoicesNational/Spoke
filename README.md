8f6367f8c2288f4736fa37ff68a29cd854c4860c
Sheena Pakanati <spakanati@gmail.com>
2016-06-04 20:54:26 -0700

    Add a sample settings. Include some npm modules that are necessary

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Running the app
 
+1. Install necessary packages.
 ```
 meteor npm install
-meteor
+
 ```
+
+2. Run script to set up settings
+```
+./setup
+
+```
+
+3. Edit the newly created `private/settings.json` to add any API keys, etc.
+
+4. Start app
+`meteor --settings private/settings.json`
--- imports/api/campaign_contacts/methods.js
+++ imports/api/campaign_contacts/methods.js
@@ -26,35 +26,6 @@ export const getContactsToText = new ValidatedMethod({
 export const wrappedGetContactsToText = Meteor.wrapAsync(getContactsToText)
 
 
-// TODO We don't actually want to loop this -- we want to bulk insert
-export const insertContact = new ValidatedMethod({
-  name: 'campaignContacts.insert',
-  validate: new SimpleSchema({
-    campaignId: { type: String },
-    assignmentId: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
-    cell: { type: String },
-    customFields: { type: Object, blackbox: true },
-  }).validator(),
-  run(contact) {
-    contact.createdAt = new Date()
-
-    if (Meteor.server){
-      const { e164} = require('libphonenumber')
-      e164(contact.cell, 'US', (error, result) => {
-        if (error) {
-        }
-        else {
-          contact.cell = result
-          CampaignContacts.insert(contact)
-        }
-      })
-    }
-
-  }
-})
-
 export const updateSurveyResponse = new ValidatedMethod({
   name: 'campaignContacts.updateSurveyResponse',
   validate: new SimpleSchema({
