9333e2769f7f34477dfc460343ff48ac190d8af4
Sheena Pakanati <spakanati@gmail.com>
2016-06-08 18:00:31 -0700

    Move meteor.startup.js to the right folder

--- imports/startup/client/routes/index.js
+++ imports/startup/client/routes/index.js
@@ -8,4 +8,7 @@ import './appRoutes'
 import './publicRoutes'
 
 
-FlowRouter.initialize();
+Meteor.startup(function() {
+    FlowRouter.initialize();
+})
+
--- meteor.startup.js
+++ meteor.startup.js
@@ -1,3 +1,4 @@
+console.log("WAITING")
 // This file is sent directly to Meteor without going through Webpack
 // You can initialize anything you need before your app start here
 
