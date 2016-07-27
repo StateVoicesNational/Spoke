559fa60676962f66a259f9408dcddbeef83e0fbb
Sheena Pakanati <spakanati@gmail.com>
2016-07-27 09:47:01 -0700

    Commit TexterStats file

---
+++ src/components/TexterStats.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import LinearProgress from 'material-ui/LinearProgress';
+
+class TexterStats extends React.Component {
+  renderAssignment(assignment) {
+    const { contacts, texter } = assignment
+    const { unmessagedCount, count } = contacts
+
+    const percentComplete = ((count - unmessagedCount) * 100) / count
+
+    return (
+      <div>
+        {texter.firstName}
+        <div>{percentComplete}%</div>
+        <LinearProgress
+          mode="determinate"
+          value={percentComplete}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    const { campaign } = this.props
+    const { assignments } = campaign
+    return (
+      <div>
+        { assignments.map((assignment) => this.renderAssignment(assignment)) }
+      </div>
+    )
+  }
+}
+
+TexterStats.propTypes = {
+  campaign: React.PropTypes.object
+}
+export default TexterStats
