83a021e558c794b7ac6f38e7c5efd2baabf8d9e5
saikat <saikat@gomockingbird.com>
2016-07-28 15:32:52 -0400

    Fix the stupid fat top app bar stupidity stupid. Closes #43.

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,48 +1,80 @@
 import React from 'react'
-import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
 import ArrowBackIcon from 'material-ui/svg-icons/navigation/arrow-back'
 import { Link } from 'react-router'
 import UserMenu from '../containers/UserMenu'
 import theme from '../styles/theme'
+import { StyleSheet, css } from 'aphrodite'
 
-
-const inlineStyles = {
-  // Material UI adds a stupid margin above the appbar that comes from the icon style
-  iconLeftStyle: {
-    margin: 0
+const styles = StyleSheet.create({
+  container: {
+    ...theme.layouts.multiColumn.container,
+    backgroundColor: theme.colors.green,
+    color: theme.colors.white,
+    height: 65,
+    verticalAlign: 'middle',
+    paddingLeft: 15,
+    paddingRight: 15
   },
-  // Material UI screws up the flex display when you try to add user agents
-  appBar: {
-    display: 'flex !important'
+  inline: {
+    display: 'inline-block',
+    marginLeft: 5,
+    marginTop: 'auto',
+    marginBottom: 'auto'
+  },
+  userMenu: {
+    marginTop: 'auto',
+    marginBottom: 'auto'
+  },
+  header: {
+    ...theme.text.header,
+    fontSize: 24,
+    color: theme.colors.white
+  },
+  flexColumn: {
+    flex: 1,
+    textAlign: 'left',
+    display: 'flex'
   }
-}
+})
 
-const TopNav = ({ backToURL, title }) => (
-  <AppBar
-    style={inlineStyles.appBar}
-    iconStyleLeft={inlineStyles.iconLeftStyle}
-    iconStyleRight={inlineStyles.iconRightStyle}
-    iconElementLeft={
-      backToURL ? (
+class TopNav extends React.Component {
+  state = {
+    userMenuOpen: false
+  }
+
+  renderBack(backToURL) {
+    if (backToURL) {
+      return (
         <Link to={backToURL}>
-          <IconButton style={{
-            marginTop: 7
-          }}>
-            <ArrowBackIcon
-              style={{
-                fill: theme.colors.white
-              }}
-            />
+          <IconButton>
+            <ArrowBackIcon style={{ fill: 'white' }} color={theme.colors.white} />
           </IconButton>
         </Link>
-      ) : <div />
+      )
     }
+    return <div />
+  }
 
-    title={title}
-    iconElementRight={<UserMenu />}
-  />
-)
+  render() {
+    const { backToURL, title } = this.props
+    return (
+      <div className={css(styles.container)}>
+        <div className={css(styles.flexColumn)}>
+          <div className={css(styles.inline)}>
+            {this.renderBack(backToURL)}
+          </div>
+          <div className={css(styles.inline, styles.header)}>
+            {title}
+          </div>
+        </div>
+        <div className={css(styles.userMenu)}>
+          <UserMenu />
+        </div>
+      </div>
+    )
+  }
+}
 
 TopNav.propTypes = {
   backToURL: React.PropTypes.string,
