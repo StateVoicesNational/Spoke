ad17e6ec1dca6f62309111e5cc5c3c3f7a591701
Sheena Pakanati <spakanati@gmail.com>
2016-07-31 17:56:53 -0700

    Basic start to billing system and adding credits.

--- src/containers/Billing.jsx
+++ src/containers/Billing.jsx
@@ -2,60 +2,170 @@ import React from 'react'
 import loadData from './hoc/load-data'
 import gql from 'graphql-tag'
 import wrapMutations from './hoc/wrap-mutations'
-import { withRouter } from 'react-router'
+import { ReactScriptLoaderMixin } from 'react-script-loader'
+import GSForm from '../components/forms/GSForm'
+import Form from 'react-formal'
+import yup from 'yup'
 
+const Billing = React.createClass({
+  mixins: [ ReactScriptLoaderMixin ],
 
-class Billing extends React.Component {
-  render() {
+  getInitialState: function() {
+    return {
+      stripeLoading: true,
+      stripeLoadingError: false
+    };
+  },
+
+  getScriptURL: function() {
+    return 'https://js.stripe.com/v2/'
+  },
+
+  onScriptLoaded: function() {
     const { stripePublishableKey,  organization } = this.props.data
     const { creditAmount, creditCurrrency } = organization.billingDetails
+    Stripe.setPublishableKey(stripePublishableKey);
+    this.setState({ stripeLoading: false, stripeLoadingError: false })
+  },
+
+  onScriptError: () =>  this.setState({ stripeLoading: false, stripeLoadingError: true }),
+
+  handleStripeResponse: async function(status, response) {
+    if (response.error) {
+      console.log(error)
+    }
+    else {
+      console.log("response.", response)
+      await this.props.mutations.updateCard(response.id)
+    }
+  },
+
+  handleSubmitCardForm: function(formValues) {
+    console.log("ONSUBMIT", formValues)
+    Stripe.card.createToken(formValues, this.handleStripeResponse)
+  },
+
+  handleSubmitAccountCreditForm: async function({ creditAmount }) {
+    console.log(creditAmount, "credit amount submission")
+    await this.props.mutations.addAccountCredit(creditAmount * 100)
+  },
+
+  renderCardForm() {
+    const cardFormSchema = yup.object({
+      'number': yup.string().required(),
+      'exp_year': yup.string().required(),
+      'exp_month': yup.string().required(),
+    })
+
     return (
       <div>
-        { creditAmount } { creditCurrrency }
-        <form action="" method="POST">
-          <script
-            src="https://checkout.stripe.com/checkout.js" className="stripe-button"
-            data-key="pk_test_i0KKPhp0YFDX29qKVSaSIXm5"
-            data-amount="999"
-            data-name="Axle Factory Inc."
-            data-description="Widget"
-            data-image="/img/documentation/checkout/marketplace.png"
-            data-locale="auto">
-          </script>
-        </form>
+        <GSForm
+          schema={cardFormSchema}
+          onSubmit={this.handleSubmitCardForm}
+        >
+          <Form.Field
+            name='number'
+            data-stripe
+            label='Card number'
+            fullWidth
+          />
+          <Form.Field
+            name='exp_month'
+            label='Expiration month'
+            hintText="01"
+          />
+          <Form.Field
+            name='exp_year'
+            label='Expiration year'
+            hintText="2019"
+          />
+          <Form.Button
+            type='submit'
+            label='Change card'
+            name='submit'
+            fullWidth
+            secondary
+            style={{ marginTop: 40 }}
+          />
+        </GSForm>
+      </div>
+    )
+  },
+
+  renderCreditForm() {
+    const formSchema = yup.object({
+      'creditAmount': yup.number().required()
+    })
+
+    return (
+      <GSForm
+        schema={formSchema}
+        onSubmit={this.handleSubmitAccountCreditForm}
+      >
+        <Form.Field
+          name='creditAmount'
+          label='Credit to add'
+        />
+        <Form.Button
+          type='submit'
+          label='Add credit'
+          name='submit'
+          fullWidth
+          secondary
+          style={{ marginTop: 40 }}
+        />
+      </GSForm>
+    )
+  },
+  render: function() {
+    return (
+      <div>
+        { this.renderCardForm() }
+        { this.renderCreditForm() }
       </div>
     )
   }
-}
+
+})
+
+const mapMutationsToProps = ({ ownProps }) => ({
+  addAccountCredit: (creditAmount) => ({
+    mutation: gql`
+      mutation addAccountCredit($creditAmount: Int!, $organizationId: String!) {
+        addAccountCredit(creditAmount: $creditAmount, organizationId: $organizationId)
+      }`,
+    variables: {
+      organizationId: ownProps.params.organizationId,
+      creditAmount
+    }
+  }),
+  updateCard: (stripeToken) => ({
+    mutation: gql`
+      mutation updateCard($stripeToken: String!, $organizationId: String!) {
+        updateCard(stripeToken: $stripeToken, organizationId: $organizationId)
+      }`,
+    variables: {
+      organizationId: ownProps.params.organizationId,
+      stripeToken
+    }
+  })
+})
 
 const mapQueriesToProps = ({ ownProps }) => ({
   data: {
-    query: gql`query adminGetBilling($organizationId: String!) {
+    query: gql`query adminGetCampaigns($organizationId: String!) {
       organization(id: $organizationId) {
         billingDetails {
           creditAmount
           creditCurrency
         }
-      },
+      }
       stripePublishableKey
     }`,
     variables: {
       organizationId: ownProps.params.organizationId
-    }
+    },
+    forceFetch: true
   }
 })
-
-// const mapMutationsToProps = ({ ownProps }) => ({
-//   joinOrganization: () => ({
-//     mutation: gql`
-//       mutation joinOrganization($organizationId: String!) {
-//         joinOrganization(organizationId: $organizationId) {
-//           id
-//         }
-//       }`,
-//     variables: { organizationId: ownProps.params.organizationId }
-//   })
-// })
-
-
-export default loadData(wrapMutations(Billing), { mapQueriesToProps })
+export default loadData(wrapMutations(Billing), { mapQueriesToProps, mapMutationsToProps })
