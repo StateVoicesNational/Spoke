e5adc829205be56b5705e4a904a147e67ec62c58
Sheena Pakanati <spakanati@gmail.com>
2016-08-11 15:08:31 -0700

    Increase sleep time in message sender to try to alleviate throughput errors

--- src/server/message-sender.js
+++ src/server/message-sender.js
@@ -20,7 +20,7 @@ async function sendMessages() {
 (async () => {
   while (true) {
     try {
-      await sleep(1000)
+      await sleep(1500)
       await sendMessages()
     } catch (ex) {
       log.error(ex)
