958e5a6434992ea8a85cc6a0a8ddf7b3297d596d
Sheena Pakanati <spakanati@gmail.com>
2016-10-01 22:55:49 -0700

    Update instructions in dummy mig

--- src/migrations/dummy-migration.js
+++ src/migrations/dummy-migration.js
@@ -4,7 +4,7 @@ import { r } from '../server/models'
 2. Start an SSH tunnel to production with:
 ssh -f -N -i ~/.ssh/id_rsa_gearshift_admin -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -L 127.0.0.1:28015:10.211.104.3:28015 -p 11236 compose@aws-us-east-1-portal.5.dblayer.com
 3. Add in the production database credentials to .env.  This means copying over the values for DB_HOST, DB_PORT, DB_NAME and DB_KEY from heroku config and putting them into .env
-4. Run this script with ./dev-tools/babel-run-with-env ./src/migrations/dummy-migration.js
+4. Run this script with ./dev-tools/babel-run-with-env.js ./src/migrations/dummy-migration.js
 5. Close the SSH tunnel with 'killall ssh'
 6. Undo your .env changes
 */
