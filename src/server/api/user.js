a2d64f4251aa9a48c3c90ae78bab85db6b1adbb7
Sheena Pakanati <spakanati@gmail.com>
2016-10-20 17:53:04 -0700

    Fix due_by filter hiding some assignments based on how we store the due date

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -54,7 +54,7 @@ export const resolvers = {
         .filter((row) =>
           r.and(
             row('right')('is_started').eq(true),
-            row('right')('due_by').gt(new Date()),
+            r.now().date().lt(row('right')('due_by') + 24 * 60 * 60),
             row('right')('organization_id').eq(organizationId),
             row('right')('is_archived').eq(false)
           )
