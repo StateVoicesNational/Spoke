082b717a3b41b06fff45f38af4ee9f7dbc9499fa
shakalee14 <shakalee14@gmail.com>
2017-06-01 10:18:22 -0400

    Added admin incoming message list component that keeps track of incoming messages

---
+++ src/containers/AdminIncomingMessageList.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+// import { CampaignList } from '../components/campaign_list'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import { moment } from 'moment'
+import Subheader from 'material-ui/Subheader'
+import SpeakerNotesIcon from 'material-ui/svg-icons/action/speaker-notes'
+import axios from 'axios'
+import Empty from '../components/Empty'
+
+const styles = {
+  floatingButton: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed'
+  }
+}
+
+export default class AdminIncomingMessageList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      incomingmessages: []
+    };
+  }
+  componentWillMount() {
+    axios.get(`http://localhost:3000/allmessages`)
+      .then(res => this.setState({ incomingmessages: res.data }))
+  }
+
+  render(){
+    return (
+      <div>
+        <h1> Incoming Messages </h1>
+        <ul>
+          {this.state.incomingmessages.map( message => {
+            if(message.direction == 'inbound' && message.from !=='+19282491850'){
+              return <li key={message.id}>{message.date_sent} {message.from } {message.to } {message.body}</li>
+            }
+          }
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
