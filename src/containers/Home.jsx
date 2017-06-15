5dcb9d35837e428cfae3cffac24848c0b989d863
Sandra Chung <sandra@moveon.org>
2017-06-15 17:51:17 -0600

    i guess we can get rid of the old link

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -82,9 +82,6 @@ class Home extends React.Component {
         Spoke is a new way to run campaigns using text messaging. We are currently in private beta.
         </div>
         <div>
-          <a className={css(styles.link)} href='mailto:help@gearshift.co'>Get in touch if you'd like an invitation.</a>
-        </div>
-        <div>
           <a className={css(styles.link)} href='#' onClick={this.handleOrgInviteClick}>Generate an organization invite to get started</a>
         </div>
       </div>
