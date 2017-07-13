4f9e441ac7f9cab866f1c17f0824c6f0ded83813
Scott Reynen <scott.reynen@moveon.org>
2017-07-13 10:58:45 -0600

    add default values on webpack vars

--- webpack/server.js
+++ webpack/server.js
@@ -3,9 +3,9 @@ import webpack from 'webpack'
 import config from './config'
 import { log } from '../src/lib'
 
-const webpackPort = process.env.WEBPACK_PORT
+const webpackPort = process.env.WEBPACK_PORT || 3000
 const appPort = process.env.DEV_APP_PORT
-const webpackHost = process.env.WEBPACK_HOST
+const webpackHost = process.env.WEBPACK_HOST || 'localhost'
 
 Object.keys(config.entry)
 .forEach((key) => {
