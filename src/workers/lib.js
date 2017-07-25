a0a38aaa0ff5c7f99959144609662dee597562dc
Schuyler Duveen <sky@skyb.us>
2017-07-25 14:58:39 -0400

    remove job queues/locking: if we do not do this, will have to rewrite for knex support anyway

--- src/workers/lib.js
+++ src/workers/lib.js
@@ -13,35 +13,17 @@ export async function updateJob(job, percentComplete) {
 }
 
 export async function getNextJob() {
-  const lockedQueues = await r.table('job_request')
-   .group({index: 'queue_name'})
-   .filter({
-     assigned: true,
-     locks_queue: true
-   })
-   .count()
-   .ungroup()
-   .filter((row) => row('reduction').gt(0))
-   .pluck('group')('group')
-
-  let availableQueues = await r.table('job_request')
-    .filter((row) => r.not(r.expr(lockedQueues).contains(row('queue_name'))))('queue_name')
-  availableQueues = availableQueues.sort()
-  const nextQueue = availableQueues[0]
-  let nextJob = null
-  if (nextQueue) {
-    nextJob = await r.table('job_request')
-      .getAll(nextQueue, { index: 'queue_name' })
+  let nextJob = await r.table('job_request')
+      .filter({'assigned': false})
       .orderBy('created_at')
       .limit(1)(0)
-    if (nextJob) {
+  if (nextJob) {
       const updateResults = await r.table('job_request')
         .get(nextJob.id)
         .update({ assigned: true })
       if (updateResults.replaced !== 1) {
         nextJob = null
       }
-    }
   }
   return nextJob
 }
