fc96a99d10400160226c65585b673870818cd199
Scott Reynen <scott.reynen@moveon.org>
2017-10-05 12:58:33 -0600

    add postgresql test as default and document how to run both options

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: "node",
+  globals: {
+    DB_JSON: "{\"client\":\"pg\",\"connection\":{\"host\":\"127.0.0.1\",\"port\":\"5432\",\"database\":\"spoke_test\",\"password\":\"spoke_test\",\"user\":\"spoke_test\"}}"
+  },
+  moduleFileExtensions: [
+    "js",
+    "jsx"
+  ],
+  transform: {
+    ".*.js": "<rootDir>/node_modules/babel-jest"
+  },
+  moduleDirectories: [
+    "node_modules"
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+  }
+};
