fbdcaeb6d35e7fc64cd64bc661152b8d77fe528c
Schuyler Duveen <sky@skyb.us>
2017-10-05 12:19:36 -0400

    document required service api points

--- src/server/api/lib/services.js
+++ src/server/api/lib/services.js
@@ -1,6 +1,11 @@
 import nexmo from './nexmo'
 import twilio from './twilio'
 
+// Each service needs the following api points:
+// async sendMessage(message) -> void
+// async handleIncomingMessage(<native message format>) -> saved (new) messagePart.id
+// async convertMessagePartsToMessage(messagePartsGroupedByMessage) -> new Message() <unsaved>
+
 const serviceMap = {
   nexmo,
   twilio
