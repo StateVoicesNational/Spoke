9263e9e2d31d158f5fbfd297db55b5dd059f6382
Scott Reynen <scott.reynen@moveon.org>
2017-10-05 13:01:10 -0600

    clear test database after test and standardize syntax a bit (no semi)

--- __test__/test_helpers.js
+++ __test__/test_helpers.js
@@ -1,17 +1,29 @@
-import { createLoaders } from '../src/server/models/';
+import { createLoaders, r } from '../src/server/models/'
 
 async function clearTestData() {
-  // delete test.sqlite?
+  r.knex.schema.dropTable('assignment')
+  r.knex.schema.dropTable('campaign')
+  r.knex.schema.dropTable('campaign_contact')
+  r.knex.schema.dropTable('canned_response')
+  r.knex.schema.dropTable('interaction_step')
+  r.knex.schema.dropTable('invite')
+  r.knex.schema.dropTable('job_request')
+  r.knex.schema.dropTable('message')
+  r.knex.schema.dropTable('migrations')
+  r.knex.schema.dropTable('opt_out')
+  r.knex.schema.dropTable('organization')
+  r.knex.schema.dropTable('pending_message_part')
+  r.knex.schema.dropTable('question_response')
+  r.knex.schema.dropTable('user')
+  r.knex.schema.dropTable('user_organization')
+  r.knex.schema.dropTable('zip_code')
 }
 
 async function setupTest() {
-  // await createTestDatabase();
-  await clearTestData();
 }
 
-// customize 
 async function cleanupTest() {
-  await clearTestData();
+  await clearTestData()
 }
 
 export function getContext(context) {
@@ -19,8 +31,7 @@ export function getContext(context) {
     ...context,
     req: {},
     loaders: createLoaders(),
-  };
+  }
 }
 
 export { setupTest, cleanupTest, getContext }
-
