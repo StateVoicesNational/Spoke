7368d3c6bb70a305f933bc24ae37d828932bada0
Joshua Carp <jm.carp@gmail.com>
2017-11-12 22:06:44 -0500

    Drop deprecated babyparse library.

--- dev-tools/export-query.js
+++ dev-tools/export-query.js
@@ -1,5 +1,5 @@
 import { r } from '../src/server/models'
-import Baby from 'babyparse'
+import Papa from 'papaparse'
 
 (async function () {
   try {
@@ -9,16 +9,16 @@ import Baby from 'babyparse'
       .filter({ campaign_id: process.env.CAMPAIGN_ID })
     const finalResults = res.map((row) => (
       {
-        assignment_id: row['assignment_id'],
-        campaign_id: row['campaign_id'],
-        contact_number: row['contact_number'],
-        user_number: row['user_number'],
-        is_from_contact: row['is_from_contact'],
-        send_status: row['send_status'],
-        text: row['text']
+        assignment_id: row.assignment_id,
+        campaign_id: row.campaign_id,
+        contact_number: row.contact_number,
+        user_number: row.user_number,
+        is_from_contact: row.is_from_contact,
+        send_status: row.send_status,
+        text: row.text
       }
     ))
-    const csvResults = Baby.unparse(finalResults)
+    const csvResults = Papa.unparse(finalResults)
     console.log(csvResults)
   } catch (ex) {
     console.log(ex)
