d372d8aed793099a380549f4122bb40792a8f917
Joshua Carp <jm.carp@gmail.com>
2017-11-19 19:53:25 -0500

    Record test coverage.

--- jest.config.sqlite.js
+++ jest.config.sqlite.js
@@ -1,22 +1,6 @@
-module.exports = {
-  testEnvironment: "node",
-  globals: {
-    DB_JSON: "{\"client\":\"sqlite3\",\"connection\":{\"filename\":\"./test.sqlite\"},\"defaultsUnsupported\":true}",
-    JOBS_SYNC: true,
-    JOBS_SAME_PROCESS: true
-  },
-  moduleFileExtensions: [
-    "js",
-    "jsx"
-  ],
-  transform: {
-    ".*.js": "<rootDir>/node_modules/babel-jest"
-  },
-  moduleDirectories: [
-    "node_modules"
-  ],
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
-  }
-};
+module.exports = require('./jest.config')
+module.exports.globals.DB_JSON = JSON.stringify({
+  client: "sqlite3",
+  connection: {filename:"./test.sqlite"},
+  defaultsUnsupported: true
+})
