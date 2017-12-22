ef820de69dba9707755dc105186a08794c161caf
Schuyler Duveen <sky@skyb.us>
2017-12-22 17:11:28 -0500

    address code review suggestions -- hopefully this fixes code climate

--- __test__/lib.test.js
+++ __test__/lib.test.js
@@ -13,5 +13,10 @@ it('should parse a message with a media url', () => {
   expect(twilio.parseMessageText({ text: 'foo bar [http://example.com/foo.jpg]' }).body).toBe('foo bar ')
   expect(twilio.parseMessageText({ text: 'foo bar [http://example.com/foo.jpg]' }).mediaUrl).toBe('http://example.com/foo.jpg')
   expect(twilio.parseMessageText({ text: 'foo bar [ https://example.com/foo.jpg ]' }).mediaUrl).toBe('https://example.com/foo.jpg')
+
+  const doubleShouldOnlyUseFirst = 'foo bar [ https://example.com/foo.jpg ] and this other image! [ https://example.com/bar.jpg ]'
+  expect(twilio.parseMessageText({ text: doubleShouldOnlyUseFirst }).mediaUrl).toBe('https://example.com/foo.jpg')
+  expect(twilio.parseMessageText({ text: doubleShouldOnlyUseFirst }).body).toBe('foo bar  and this other image! [ https://example.com/bar.jpg ]')
+
   expect(twilio.parseMessageText({ text: undefined }).body).toBe('')
 })
