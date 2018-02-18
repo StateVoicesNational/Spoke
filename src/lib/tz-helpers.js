ce4cdc8647a224bb057ae5de259c5903f8517165
Larry <lp10011@gmail.com>
2018-02-18 17:37:02 -0500

    if not provided in env or global DST_REFERENCE_TIMEZONE will default to AMERICA/NEW_YORK via hardcode

--- src/lib/tz-helpers.js
+++ src/lib/tz-helpers.js
@@ -1,5 +1,5 @@
 export function getProcessEnvTz() { return process.env.TZ; }
 
 export function getProcessEnvDstReferenceTimezone() {
-  return process.env.DST_REFERENCE_TIMEZONE || global.DST_REFERENCE_TIMEZONE || ''; }
+  return process.env.DST_REFERENCE_TIMEZONE || global.DST_REFERENCE_TIMEZONE || 'America/New_York'; }
 
