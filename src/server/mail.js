245ba49e00be71338fa93e383ba7d967d2acf268
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-26 14:28:36 -0500

    add mailgun-js option

--- src/server/mail.js
+++ src/server/mail.js
@@ -1,22 +1,52 @@
 import { log } from '../lib'
 import nodemailer from 'nodemailer'
+import mailgunConstructor from 'mailgun-js'
 
-let transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_HOST_PORT,
-  secure: typeof process.env.EMAIL_HOST_SECURE !== 'undefined' ? process.env.EMAIL_HOST_SECURE : true,
-  auth: {
-    user: process.env.EMAIL_HOST_USER,
-    pass: process.env.EMAIL_HOST_PASSWORD
-  }
-})
+const mailgun =
+  process.env.MAILGUN_API_KEY &&
+  process.env.MAILGUN_DOMAIN &&
+  mailgunConstructor({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  })
+
+const sender =
+  process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN
+    ? {
+      sendMail: ({ from, to, subject, replyTo, text }) =>
+          new Promise((resolve, reject) =>
+            mailgun.messages.send(
+              {
+                from: `${from} <${replyTo}>`,
+                to,
+                subject,
+                text
+              },
+              (err, resp) => (err ? reject(err) : resolve(resp))
+            )
+          )
+    }
+    : nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_HOST_PORT,
+      secure:
+          typeof process.env.EMAIL_HOST_SECURE !== 'undefined'
+            ? process.env.EMAIL_HOST_SECURE
+            : true,
+      auth: {
+        user: process.env.EMAIL_HOST_USER,
+        pass: process.env.EMAIL_HOST_PASSWORD
+      }
+    })
 
 export const sendEmail = async ({ to, subject, text, replyTo }) => {
   log.info(`Sending e-mail to ${to} with subject ${subject}.`)
+
   if (process.env.NODE_ENV === 'development') {
     log.debug(`Would send e-mail with subject ${subject} and text ${text}.`)
     return null
   }
+
   const params = {
     from: process.env.EMAIL_FROM,
     to,
@@ -27,5 +57,6 @@ export const sendEmail = async ({ to, subject, text, replyTo }) => {
   if (replyTo) {
     params['replyTo'] = replyTo
   }
+
   return transporter.sendMail(params)
 }
