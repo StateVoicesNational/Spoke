3e689bc1881f6470566fe3b4f03ce8854e3eb30f
Dan Lewis-Toakley <daniel@avaaz.org>
2018-03-28 12:09:32 -0400

    314: refactored TexterFaqs and added a unit test

---
+++ src/components/TexterFaqs.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Card, CardTitle, CardText } from 'material-ui/Card'
+
+const TexterFaqs = ({ faqs }) => {
+
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      {faqs.map((faq, idx) => (
+        <Card>
+          <CardTitle
+            title={`${idx}. ${faq.question}`}
+          />
+          <CardText>
+            <p>{faq.answer}</p>
+          </CardText>
+        </Card>
+      ))}
+    </div>
+  )
+}
+
+TexterFaqs.propTypes = {
+  faqs: PropTypes.array
+}
+
+export default TexterFaqs
---
+++ src/lib/faqs.js
@@ -0,0 +1,12 @@
+const FAQs = [
+  {
+    question: 'What is Spoke?',
+    answer: 'An answer!'
+  },
+  {
+    question: 'Another question!',
+    answer: 'Another answer!'
+  }
+]
+
+export default FAQs
