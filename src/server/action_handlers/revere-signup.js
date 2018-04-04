e6bbdb5bf4d402eb5cc25ca66c692812c9ba2c6c
shakalee14 <shakalee14@gmail.com>
2018-04-04 15:20:14 -0400

    Successfully sends number to revere, adds them to ak and updates phone number at the record

--- src/server/action_handlers/revere-signup.js
+++ src/server/action_handlers/revere-signup.js
@@ -7,7 +7,8 @@ export const displayName = () => 'Revere Signup'
 const listId = process.env.REVERE_LIST_ID
 const mobileFlowId = process.env.REVERE_NEW_SUBSCRIBER_MOBILE_FLOW
 const mobileApiKey = process.env.REVERE_MOBILE_API_KEY
-const actionkitBaseUrl = process.env.AK_BASEURL
+const akAddUserUrl = process.env.AK_ADD_USER_URL
+const akAddPhoneUrl = process.env.AK_ADD_PHONE_URL
 
 // The Help text for the user after selecting the action
 export const instructions = () => (
@@ -22,7 +23,7 @@ export async function available(organizationId) {
 
 export async function processAction(questionResponse, interactionStep, campaignContactId) {
 
-   const contactRes = await r.knex('campaign_contact')
+    const contactRes = await r.knex('campaign_contact')
       .where('campaign_contact.id', campaignContactId)
       .leftJoin('campaign', 'campaign_contact.campaign_id', 'campaign.id')
       .leftJoin('organization', 'campaign.organization_id', 'organization.id')
@@ -39,7 +40,7 @@ export async function processAction(questionResponse, interactionStep, campaignC
         'Authorization': mobileApiKey
       },
       body: {
-        msisdns: [`${contactCell}`],
+        msisdns: [`00${contactCell}`],
         mobileFlow: `${mobileFlowId}`,
         list: listId
       },
@@ -50,25 +51,45 @@ export async function processAction(questionResponse, interactionStep, campaignC
       if (error) throw new Error(error)
       if(response.statusCode == 204 && process.env.AK_ADD_USER_URL){
         const userData = {
-          'email': contactCell +  '-smssubscriber@example.com',
+          'email': contactCell +  'smssubscriber@test.com',
           'first_name': contact.first_name,
           'last_name': contact.last_name,
           'sms_subscribed': true,
           'action_mobilesubscribe': true,
           'suppress_subscribe': true,
-          'phone': contactCell,
+          'phone': [contactCell],
           'phone_type': 'mobile',
-          'source': 'spoke-signup'
+          'source': 'spoke-signup',
         }
-        console.log('user data:', userData);
 
         request.post({
-          'url': `${actionkitBaseUrl}/act/`,
+          'url': akAddUserUrl,
+          headers: {
+            accept: 'application/json',
+            'content-type': 'application/json'
+          },
           'form': userData
         }, (error, response, body) => {
+
           if (error) throw new Error(error)
-          console.log('body:', body);
-          console.log('response:', response);
+          if(response.statusCode === 201){
+            request.post({
+              'url': akAddPhoneUrl,
+              headers: {
+                accept: 'application/json',
+                'content-type': 'application/json'
+              },
+              'form': {
+                'user': response.headers['location'],
+                'phone': contactCell,
+                'type': 'mobile'
+              }
+            }, (error, response, body) => {
+              if (error) throw new Error(error)
+              console.log('response', response);
+              console.log('body', body);
+            })
+          }
         })
       } else {
         return
