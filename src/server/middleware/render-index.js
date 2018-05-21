7a23dfa959c6a3a3b6dda88fc0b73eb2b30e81e2
accountablehq <kevin@accountablehq.com>
2018-05-21 13:20:14 -0500

    Update render-index.js

--- src/server/middleware/render-index.js
+++ src/server/middleware/render-index.js
@@ -33,7 +33,7 @@ export default function renderIndex(html, css, assetMap, store) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-    <title>Spoke</title>
+    <title>Bolster</title>
     ${externalLinks}
     <style>
       /* CSS declarations go here */
