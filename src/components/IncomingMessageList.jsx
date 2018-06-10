f54c74e3e9880e44f4a4cd8c3ff2ec427709ba14
Larry <lp10011@gmail.com>
2018-06-10 15:16:57 -0400

    wired in DataTables

--- src/components/IncomingMessageList.jsx
+++ src/components/IncomingMessageList.jsx
@@ -13,6 +13,68 @@ import loadData from '../containers/hoc/load-data'
 import { withRouter } from 'react-router'
 import gql from 'graphql-tag'
 import LoadingIndicator from '../components/LoadingIndicator'
+import DataTables from 'material-ui-datatables'
+
+function prepareDataTableData(organization) {
+  const assignments = getAssignmentsFromOrganization(organization)
+  const tableData = []
+  for (const assignment of assignments) {
+    for (const contact of assignment.contacts) {
+      tableData.push({
+        campaignTitle: assignment.campaign.title,
+        texter: assignment.texter.displayName,
+        to: contact.cell,
+        status: contact.messageStatus,
+        messages: contact.messages
+      })
+    }
+  }
+  return tableData
+}
+
+function prepareTableColumns() {
+  return [
+    {
+      key: 'campaignTitle',
+      label: 'Campaign'
+    },
+    {
+      key: 'texter',
+      label: 'Texter'
+    },
+    {
+      key: 'to',
+      label: 'To'
+    },
+    {
+      key: 'status',
+      label: 'Conversation Status'
+    },
+    {
+      key: 'messages',
+      label: 'Messages',
+      style: {
+        width: '30%'
+      },
+      render: (columnKey, row) => {
+        return (
+          <Card>
+            <CardHeader title={'Messages'} actAsExpander showExpandableButton />
+            <CardText expandable>
+              <div>
+                {row.messages.map(message => (
+                  <p style={message.isFromContact ? { color: 'red' } : { color: 'black' }}>
+                    {message.text}
+                  </p>
+                ))}
+              </div>
+            </CardText>
+          </Card>
+        )
+      }
+    }
+  ]
+}
 
 function getAssignmentsFromOrganization(organization) {
   const assignments = []
@@ -41,6 +103,36 @@ function prepareTableData(organization) {
 export class IncomingMessageList extends Component {
   constructor(props) {
     super(props)
+    this.state = { data: [], page: 1, rowSize: 10 }
+
+    this.handleNextPageClick = this.handleNextPageClick.bind(this)
+    this.handlePreviousPageClick = this.handlePreviousPageClick.bind(this)
+    this.handleRowSizeChanged = this.handleRowSizeChanged.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.organization.loading) {
+      this.setState({ data: [], count: 0, page: 1 })
+    } else {
+      const assignments = prepareDataTableData(nextProps.organization.organization)
+      this.setState({ data: assignments, count: assignments.length, page: 1 })
+    }
+  }
+
+  handleNextPageClick() {
+    this.setState({
+      page: this.state.page + 1
+    })
+  }
+
+  handlePreviousPageClick() {
+    this.setState({
+      page: this.state.page - 1
+    })
+  }
+
+  handleRowSizeChanged(index, value) {
+    this.setState({ rowSize: value })
   }
 
   render() {
@@ -49,62 +141,34 @@ export class IncomingMessageList extends Component {
         {this.props.organization.loading ? (
           <LoadingIndicator />
         ) : (
-          <div>
-            <Table
-              multiSelectable
-              onRowSelection={(rowsSelected) => {
-                if (
-                  this.props.onConversationSelected != null &&
-                  typeof this.props.onConversationSelected === 'function'
-                ) {
-                  this.props.onConversationSelected(
-                    rowsSelected,
-                    prepareTableData(this.props.organization.organization)
-                  )
-                }
-              }}
-            >
-              <TableHeader enableSelectAll>
-                <TableRow>
-                  <TableHeaderColumn> Texter </TableHeaderColumn>
-                  <TableHeaderColumn> To </TableHeaderColumn>
-                  <TableHeaderColumn> Conversation Status </TableHeaderColumn>
-                  <TableHeaderColumn style={{ width: '40%' }}> Messages </TableHeaderColumn>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {getAssignmentsFromOrganization(this.props.organization.organization).map(
-                  assignment => {
-                    return assignment.contacts.map(contact => (
-                      <TableRow key={contact.id} value={[assignment.campaign.id, contact.id]}>
-                        <TableRowColumn> {assignment.texter.displayName} </TableRowColumn>
-                        <TableRowColumn> {contact.cell} </TableRowColumn>
-                        <TableRowColumn> {contact.messageStatus} </TableRowColumn>
-                        <TableRowColumn style={{ width: '40%' }}>
-                          <Card>
-                            <CardHeader title={'Messages'} actAsExpander showExpandableButton />
-                            <CardText expandable>
-                              <div>
-                                {contact.messages.map(message => (
-                                  <p
-                                    style={
-                                      message.isFromContact ? { color: 'red' } : { color: 'black' }
-                                    }
-                                  >
-                                    {message.text}
-                                  </p>
-                                ))}
-                              </div>
-                            </CardText>
-                          </Card>
-                        </TableRowColumn>
-                      </TableRow>
-                    ))
-                  }
-                )}
-              </TableBody>
-            </Table>
-          </div>
+          <DataTables
+            data={this.state.data.slice(
+              (this.state.page - 1) * this.state.rowSize,
+              (this.state.page - 1) * this.state.rowSize + this.state.rowSize
+            )}
+            columns={prepareTableColumns()}
+            multiSelectable
+            selectable
+            enableSelectAll
+            showCheckboxes
+            page={this.state.page}
+            rowSize={this.state.rowSize}
+            count={this.state.count}
+            onNextPageClick={this.handleNextPageClick}
+            onPreviousPageClick={this.handlePreviousPageClick}
+            onRowSizeChange={this.handleRowSizeChanged}
+            onRowSelection={rowsSelected => {
+              if (
+                this.props.onConversationSelected != null &&
+                typeof this.props.onConversationSelected === 'function'
+              ) {
+                this.props.onConversationSelected(
+                  rowsSelected,
+                  prepareTableData(this.props.organization.organization)
+                )
+              }
+            }}
+          />
         )}
       </div>
     )
@@ -116,7 +180,7 @@ IncomingMessageList.propTypes = {
   contactsFilter: type.object,
   campaignsFilter: type.object,
   onConversationSelected: type.func,
-  utc: type.number
+  utc: type.string
 }
 
 const mapQueriesToProps = ({ ownProps }) => ({
@@ -128,7 +192,7 @@ const mapQueriesToProps = ({ ownProps }) => ({
         $campaignsFilter: CampaignsFilter
         $utc: String
       ) {
-        organization(id: $organizationId, utc:$utc) {
+        organization(id: $organizationId, utc: $utc) {
           id
           campaigns(campaignsFilter: $campaignsFilter) {
             id
@@ -136,6 +200,7 @@ const mapQueriesToProps = ({ ownProps }) => ({
             assignments {
               campaign {
                 id
+                title
               }
               texter {
                 id
