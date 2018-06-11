9dc1bf17a95663f38809793062bfd1a79afadd76
cp4r3z <cp4r3z@gmail.com>
2018-06-11 16:32:58 -0500

    Modifying test harness script to avoid running e2e tests during coverage

--- jest.config.js
+++ jest.config.js
@@ -27,11 +27,12 @@ module.exports = {
   },
   moduleDirectories: [
     "node_modules"
-  ],
+  ], 
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
   },
   collectCoverageFrom : ["**/*.{js,jsx}", "!**/node_modules/**", "!**/__test__/**", "!**/deploy/**", "!**/coverage/**"],
   setupTestFrameworkScriptFile: "<rootDir>/__test__/setup.js",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/__tests__/e2e/"]
 };
