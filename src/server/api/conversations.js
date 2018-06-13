3aea1d3ad5c3d8aa80bbf2a1abeeacf800f0a5f6
Larry <lp10011@gmail.com>
2018-06-13 01:02:33 -0400

    an iteration on implementing conversatons query

--- src/server/api/conversations.js
+++ src/server/api/conversations.js
@@ -1,6 +1,6 @@
-import _ from 'lodash';
-import User from '../models/user';
-import CampaignContact from '../models/campaign-contact';
+import _ from 'lodash'
+import User from '../models/user'
+import CampaignContact from '../models/campaign-contact'
 
 export const schema = `
   input ConversationFilter {
@@ -12,22 +12,51 @@ export const schema = `
   type Conversation {
     texter: User!
     contact: CampaignContact!
+    campaign: Campaign!
+    assignment: Assignment
   }
-`;
+`
+
+function coerceQueryFieldsToResolverFields(instance, fieldsToRemove) {
+  const fields = _.remove(Object.keys(instance), el => {
+    return !(fieldsToRemove.includes(el))
+  })
+  return _.pick(instance, fields)
+}
 
 export const resolvers = {
   Conversation: {
-    'texter': (instance) => {
-      const fields = _.remove(Object.keys(User._schema._schema), el === 'id'),
-            texterFields = _.pick(instance, fields);
-      texterFields.id = instance['texterId'];
-      return texterFields;
+    texter: instance => {
+      const texterFields = coerceQueryFieldsToResolverFields(instance, [
+        'u_id',
+        'u_first_name',
+        'u_last_name'
+      ])
+      texterFields.id = instance.u_id
+      texterFields.last_name = instance.u_first_name
+      texterFields.first_name = instance.u_last_name
+      return texterFields
     },
-    'contact': (instance) => {
-      const fields = _.remove(Object.keys(CampaignContact._schema._schema), el === 'id'),
-            contactFields = _.pick(instance, fields);
-      contactFields.id = instance['contactId'];
-      return contactFields;
+    contact: instance => {
+      const contactFields = coerceQueryFieldsToResolverFields(instance, [
+        'cc_id',
+        'cc_first_name',
+        'cc_last_name'
+      ])
+      contactFields.id = instance.cc_id
+      contactFields.first_name = instance.cc_first_name
+      contactFields.last_name = instance.cc_last_name
+      return contactFields
     },
-  },
-};
+    campaign: instance => {
+      const campaignFields = coerceQueryFieldsToResolverFields(instance, ['cmp_id'])
+      campaignFields.id = instance.cmp_id
+      return campaignFields
+    },
+    assignment: instance => {
+      const assignmentFields = coerceQueryFieldsToResolverFields(instance, ['ass_id'])
+      assignmentFields.id = instance.ass_id
+      return assignmentFields
+    }
+  }
+}
