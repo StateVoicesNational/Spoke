5587d9614344119f16e0685b615b3a0c0919283a
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-06-13 15:06:24 -0400

    Update react-router and switch to connected-react-router.

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,18 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
-import { routerReducer, routerMiddleware } from 'react-router-redux'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 import ReduxThunk from 'redux-thunk'
 import ApolloClientSingleton from '../network/apollo-client-singleton'
 import * as reducers from './reducers'
 
 export default class Store {
   constructor(history, initialState = {}) {
-    const reducer = combineReducers({
+    const rootReducer = combineReducers({
       ...reducers,
       apollo: ApolloClientSingleton.reducer(),
-      routing: routerReducer
     })
 
     this.data = createStore(
-      reducer,
+      connectRouter(history)(rootReducer),
       initialState,
       compose(
         applyMiddleware(
