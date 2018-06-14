f7787884bb6808157cb34456da900e401a1132e2
cp4r3z <cp4r3z@gmail.com>
2018-06-13 22:52:50 -0500

    Adding e2e tests to Sauce Labs

--- __test__/e2e/hello_spoke.test.js
+++ __test__/e2e/hello_spoke.test.js
@@ -7,18 +7,26 @@ describe('Signing up a new user to Spoke', () => {
   let driver
 
   beforeAll(async () => {
-    driver = await new Builder().forBrowser('chrome').build()
+    // driver = await new Builder().forBrowser('chrome').build()
+    driver = await new Builder()
+      .withCapabilities(config.sauceLabs.capabilities)
+      .usingServer(config.sauceLabs.server)
+      .build()
   })
 
   afterAll(async () => {
     await driver.quit()
   })
 
-  it('gets the landing page', async () => {
+  it('gets the guinea pig page', async () => {
+    await driver.get('http://saucelabs.com/test/guinea-pig')
+  })
+
+  xit('gets the landing page', async () => {
     await driver.get(config.baseUrl)
   })
 
-  it('clicks the login link', async () => {
+  xit('clicks the login link', async () => {
     const loginUrl = `${config.baseUrl}/login`
     const el = await driver.findElement(By.css('#login'))
     await driver.wait(until.elementIsVisible(el))
