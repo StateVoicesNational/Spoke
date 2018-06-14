17517d030b5ae37aa37d12ef09d3e0d091fa2001
cp4r3z <cp4r3z@gmail.com>
2018-06-13 22:52:50 -0500

    Adding e2e tests to Sauce Labs

--- __test__/e2e/util/config.js
+++ __test__/e2e/util/config.js
@@ -1,3 +1,30 @@
+const sauceLabs = {
+  username: 'cp4r3zsl',
+  accessKey: 'd00a6baf-4b34-4790-a9cb-ed0bdf1303ff'
+}
+// const sauceLabs = {
+//   username: process.env.SAUCE_USERNAME,
+//   accessKey: process.env.SAUCE_ACCESS_KEY
+// }
+
 module.exports = {
-  baseUrl: global.BASE_URL
+  baseUrl: global.BASE_URL,
+  // TODO: Figure out what the long term user is
+  sauceLabs: {
+    /**
+     * TODO: The Sauce Connect addon exports the SAUCE_USERNAME and SAUCE_ACCESS_KEY
+     * environment variables, and relays connections to the hub URL back to Sauce Labs.
+     */
+    username: sauceLabs.username,
+    accessKey: sauceLabs.accessKey,
+    capabilities: {
+      browserName: 'chrome',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      username: sauceLabs.username,
+      accessKey: sauceLabs.accessKey
+    },
+    server: `http://${sauceLabs.username}:${sauceLabs.accessKey}@ondemand.saucelabs.com:80/wd/hub`,
+    host: 'localhost',
+    port: 4445
+  }
 }
