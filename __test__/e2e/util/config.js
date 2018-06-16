4cb91e5288db61e1e38df6a430f8107fba832355
cp4r3z <cp4r3z@gmail.com>
2018-06-15 22:10:08 -0500

    Moving the SauceLabs username and access key to env vars

--- __test__/e2e/util/config.js
+++ __test__/e2e/util/config.js
@@ -1,31 +1,17 @@
-const sauceLabs = {
-  username: 'cp4r3zsl',
-  accessKey: 'd00a6baf-4b34-4790-a9cb-ed0bdf1303ff'
-}
-// const sauceLabs = {
-//   username: process.env.SAUCE_USERNAME,
-//   accessKey: process.env.SAUCE_ACCESS_KEY
-// }
-
 module.exports = {
   baseUrl: global.BASE_URL,
-  // TODO: Figure out what the long term user is
   sauceLabs: {
-    /**
-     * TODO: The Sauce Connect addon exports the SAUCE_USERNAME and SAUCE_ACCESS_KEY
-     * environment variables, and relays connections to the hub URL back to Sauce Labs.
-     */
-    username: sauceLabs.username,
-    accessKey: sauceLabs.accessKey,
+    username: process.env.SAUCE_USERNAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY,
     capabilities: {
-      name: 'Travis-CI - Chrome E2E Tests',
+      name: 'Spoke - Chrome E2E Tests',
       browserName: 'chrome',
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-      username: sauceLabs.username,
-      accessKey: sauceLabs.accessKey,
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
       build: process.env.TRAVIS_BUILD_NUMBER
     },
-    server: `http://${sauceLabs.username}:${sauceLabs.accessKey}@ondemand.saucelabs.com:80/wd/hub`,
+    server: `http://${process.env.SAUCE_USERNAME}:${process.env.SAUCE_ACCESS_KEY}@ondemand.saucelabs.com:80/wd/hub`,
     host: 'localhost',
     port: 4445
   }
