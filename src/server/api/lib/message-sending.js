3636038bbc9c6018997e04e2d17e5bb1e7f8d541
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-06-21 13:52:54 -0400

    Handle blank messages.

--- src/server/api/lib/message-sending.js
+++ src/server/api/lib/message-sending.js
@@ -22,6 +22,25 @@ export async function saveNewIncomingMessage(messageInstance) {
       console.error('DUPLICATE MESSAGE SAVED', countResult.count, messageInstance)
     }
   }
+
+  // Handle MMS
+  const mediaUrls = []
+  const serviceResponses = JSON.parse(messageInstance.service_response)
+  serviceResponses.forEach(response => {
+    const mediaUrlKeys = Object.keys(response).filter(key => key.startsWith('MediaUrl'))
+    mediaUrlKeys.forEach(key => mediaUrls.push(response[key]))
+  })
+  if (mediaUrls.length > 0) {
+    const warningText = 'This message contains multimedia attachments. Open the following attachments AT YOUR OWN RISK.'
+    const mediaText = mediaUrls.unshift(warningText).join('\n\n')
+
+    if (messageInstance.text === '') {
+      messageInstance.text = mediaText
+    } else {
+      messageInstance.text = `${messageInstance.text}\n\n${mediaText}`
+    }
+  }
+
   await messageInstance.save()
 
   await r.table('campaign_contact')
