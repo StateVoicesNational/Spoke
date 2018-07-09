de934df2a09581d0c1056bd59ba654c810b829cf
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-07-09 14:29:02 -0400

    knexfile working for some db access; partially functional initial "migration"

--- __test__/server/db/thinky-knex.test.js
+++ __test__/server/db/thinky-knex.test.js
@@ -1,12 +1,15 @@
 const { NODE_ENV = 'development' } = process.env
-const knex = require('knex')(NODE_ENV)
-import { r } from '../../../src/server/models'
+// console.log('using env', NODE_ENV)
+const knex = require('knex')(require('../../../knexfile.js')[NODE_ENV])
+console.log('knex config is', knex.client.config.connection)
 
 describe('The knexfile', () => {
+  beforeEach(async () => await knex.migrate.latest())
+  afterEach(async () => await knex.raw('DROP OWNED BY spoke_test;'))
+
   it('returns the same interaction_step schema as the thinky adapter', async () => {
-    const vanillaKnexSchema = await knex('interaction_step').columnInfo()
-    const rKnexSchema = await r.knex('interaction_step').columnInfo()
-    console.log('vanilla', vanillaKnexSchema, 'r.knex', rKnexSchema)
+    const interactionStepSchema = await knex('interaction_step').columnInfo()
+    console.log('interaction_step', interactionStepSchema)
     expect(1).toEqual(1)
   })
 })
