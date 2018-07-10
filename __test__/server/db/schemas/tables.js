a325ac81ccf054999052cf724c71130d35e3445d
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-07-10 15:47:47 -0400

    extend jest with a deep schema equality function

---
+++ __test__/server/db/schemas/tables.js
@@ -0,0 +1,1 @@
+export const tables = ['log', 'message', 'user_cell', 'job_request', 'migrations', 'pending_message_part', 'zip_code', 'invite', 'user', 'user_organization', 'campaign', 'interaction_step', 'assignment', 'organization', 'canned_response', 'opt_out', 'question_response', 'campaign_contact']
