bab9bb1ed7e06a30a8b57447bacc3bb56e814b75
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-07-10 12:33:06 -0400

    script to export the schema of every table for testing

---
+++ __test__/server/db/schemas/export.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: '../../../../.env' })
+const config = require('../../../../knexfile.js')
+const knex = require('knex')(config)
+import fs from 'fs'
+
+function getSchema(s) {
+  knex(s).columnInfo().then(schema => {
+    console.log('exported schema for', s)
+    fs.writeFile(`${s}.json`, JSON.stringify(schema, null, 2))
+  })
+}
+
+const tables = ['log', 'message', 'user_cell', 'job_request', 'migrations', 'pending_message_part', 'zip_code', 'invite', 'user', 'user_organization', 'campaign', 'interaction_step', 'assignment', 'organization', 'canned_response', 'opt_out', 'question_response', 'campaign_contact']
+
+tables.forEach(getSchema)
+
+// Run this file (probably with npx babel-node export.js) to get nice JSON representations of each table's schema, for testing.
