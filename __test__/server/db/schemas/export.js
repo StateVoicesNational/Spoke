23097dfaa27e880893d3f75185871ee177a5952a
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-07-10 15:47:47 -0400

    extend jest with a deep schema equality function

--- __test__/server/db/schemas/export.js
+++ __test__/server/db/schemas/export.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '../../../../.env' })
 const config = require('../../../../knexfile.js')
 const knex = require('knex')(config)
+import { tables } from './tables'
 import fs from 'fs'
 
 function getSchema(s) {
@@ -10,8 +11,6 @@ function getSchema(s) {
   })
 }
 
-export const tables = ['log', 'message', 'user_cell', 'job_request', 'migrations', 'pending_message_part', 'zip_code', 'invite', 'user', 'user_organization', 'campaign', 'interaction_step', 'assignment', 'organization', 'canned_response', 'opt_out', 'question_response', 'campaign_contact']
-
 tables.forEach(getSchema)
 
-// Run this file (probably with npx babel-node export.js) to get nice JSON representations of each table's schema, for testing.
+// Run this file _from this directory_ (e.g. with npx babel-node export.js) to get nice JSON representations of each table's schema, for testing.
