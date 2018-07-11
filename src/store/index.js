c22f68d9f9390c8fc23052deddfd1e665761dfb9
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-07-11 14:05:07 -0400

    Remove useless reducers.

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,10 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 import ReduxThunk from 'redux-thunk'
 import ApolloClientSingleton from '../network/apollo-client-singleton'
-import * as reducers from './reducers'
 
 export default class Store {
   constructor(history, initialState = {}) {
     const reducer = combineReducers({
-      ...reducers,
       apollo: ApolloClientSingleton.reducer(),
       routing: routerReducer
     })
