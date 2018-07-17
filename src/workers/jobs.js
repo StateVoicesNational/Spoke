8aa2d17c0babb344595ed2d3269d6e297e0d1d75
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-07-17 13:57:51 -0400

    Remove TODO comment.

--- src/workers/jobs.js
+++ src/workers/jobs.js
@@ -614,7 +614,6 @@ export async function sendMessages(queryFunc, defaultStatus) {
           message.service = message.service || process.env.DEFAULT_SERVICE
           const service = serviceMap[message.service]
           log.info(`Sending (${message.service}): ${message.user_number} -> ${message.contact_number}\nMessage: ${message.text}`)
-          // TODO: pass transaction to service.sendMessage
           await service.sendMessage(message, trx)
           pastMessages.push(message.id)
           pastMessages = pastMessages.slice(-100) // keep the last 100
