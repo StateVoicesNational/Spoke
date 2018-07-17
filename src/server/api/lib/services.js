e86ad27ba3cc677c7e6a0fbcb9bf54f52d43f793
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-07-17 12:17:23 -0400

    Pass transaction to service.sendMessage().

--- src/server/api/lib/services.js
+++ src/server/api/lib/services.js
@@ -3,7 +3,7 @@ import twilio from './twilio'
 import fakeservice from './fakeservice'
 
 // Each service needs the following api points:
-// async sendMessage(message) -> void
+// async sendMessage(message, trx) -> void
 // To receive messages from the outside, you will probably need to implement these, as well:
 // async handleIncomingMessage(<native message format>) -> saved (new) messagePart.id
 // async convertMessagePartsToMessage(messagePartsGroupedByMessage) -> new Message() <unsaved>
