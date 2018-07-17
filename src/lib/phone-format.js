1ca93655b8265effa761e10e5552ec57e58c0cb4
Schuyler Duveen <sky@skyb.us>
2018-07-17 19:32:46 -0400

    default to country=US (even if process.env.PHONE_NUMBER_COUNTRY is not set)

--- src/lib/phone-format.js
+++ src/lib/phone-format.js
@@ -5,7 +5,7 @@ export const getFormattedPhoneNumber = (cell, country = 'US') => {
   const phoneUtil = PhoneNumberUtil.getInstance()
 
   try {
-    const inputNumber = phoneUtil.parse(cell, country)
+    const inputNumber = phoneUtil.parse(cell, country || 'US')
     const isValid = phoneUtil.isValidNumber(inputNumber)
     if (isValid) {
       return phoneUtil.format(inputNumber, PhoneNumberFormat.E164)
