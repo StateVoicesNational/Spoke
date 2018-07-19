dda293348f58b7e19492c1ebc4cd1e4458a34615
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-07-19 16:43:20 -0400

    dump indexes for testing

--- __test__/server/db/schemas/export.js
+++ __test__/server/db/schemas/export.js
@@ -10,7 +10,17 @@ function getSchema(s) {
     fs.writeFile(`${s}.json`, JSON.stringify(schema, null, 2))
   })
 }
+function getIndexes() {
+  knex('pg_indexes')
+  .select()
+  .where({ schemaname: 'public' })
+  .then(indexes => {
+    fs.writeFile('indexes.json', JSON.stringify(indexes, null, 2))
+    console.log('exported indices')
+  })
+}
 
 tables.forEach(getSchema)
+getIndexes()
 
 // Run this file _from this directory_ (e.g. with npx babel-node export.js) to get nice JSON representations of each table's schema, for testing.
