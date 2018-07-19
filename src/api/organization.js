b238be8f40032ed0b1b4dca11be01d7dd698de75
lp10011 <LP10011@gmail.com>
2018-07-19 00:12:26 -0400

    got rid of some dead code

--- src/api/organization.js
+++ src/api/organization.js
@@ -1,13 +1,9 @@
 export const schema = `
-  input PeopleFilter {
-    campaignsFilter: CampaignsFilter
-  }
-
   type Organization {
     id: ID
     uuid: String
     name: String
-    campaigns(campaignsFilter: CampaignsFilter): [Campaign]
+    campaigns(cursor:OffsetLimitCursor, campaignsFilter: CampaignsFilter): CampaignsReturn
     people(role: String): [User]
     optOuts: [OptOut]
     threeClickEnabled: Boolean
