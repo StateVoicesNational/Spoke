a5dd7808dd1c2c40f2a46ed3f414e59e5eb2a1aa
lp10011 <LP10011@gmail.com>
2018-08-05 10:44:53 -0400

    wrapping, and retractable menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,38 +6,57 @@ import Divider from 'material-ui/Divider'
 import { withRouter } from 'react-router'
 import camelCase from 'camelcase'
 import { dataTest } from '../lib/attributes'
+import { FlatButton } from 'material-ui'
 
 const Navigation = function Navigation(props) {
   const { sections, switchListItem } = props
 
-  return (
-    <Paper
-      rounded={false}
-      zDepth={2}
-      style={{
-        height: '100%'
-      }}
-    >
-      <List>
-        {sections.map((section) => (
-          <ListItem
-            {...dataTest(camelCase(`nav ${section.path}`))}
-            key={section.name}
-            primaryText={section.name}
-            onTouchTap={() => props.router.push(section.url)}
+  if (props.showMenu) {
+    return (
+      <Paper
+        rounded={false}
+        zDepth={2}
+        style={{
+          height: '100%'
+        }}
+      >
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <FlatButton
+            style={{}}
+            label={'Close Menu'}
+            onClick={() => props.onToggleMenu()}
           />
-        ))}
-        <Divider />
-        {switchListItem}
-      </List>
-    </Paper>
-  )
+        </div>
+
+        <List>
+          {sections.map(section => (
+            <ListItem
+              {...dataTest(camelCase(`nav ${section.path}`))}
+              key={section.name}
+              primaryText={section.name}
+              onTouchTap={() => props.router.push(section.url)}
+            />
+          ))}
+          <Divider />
+          {switchListItem}
+        </List>
+      </Paper>
+    )
+  } else {
+    return (<div></div>)
+  }
+}
+
+Navigation.defaultProps = {
+  showMenu: true
 }
 
 Navigation.propTypes = {
   sections: PropTypes.array,
   switchListItem: PropTypes.object,
-  router: PropTypes.object
+  router: PropTypes.object,
+  onToggleMenu: PropTypes.func.isRequired,
+  showMenu: PropTypes.bool
 }
 
 export default withRouter(Navigation)
