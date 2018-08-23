e866ab0654360c66d9803626abf56ea37a769d26
Shaka Lee <shaka@Shakas-MacBook-Pro.local>
2018-08-23 09:17:56 -0500

    Moves ak signup to own function

--- src/server/action_handlers/revere-signup.js
+++ src/server/action_handlers/revere-signup.js
@@ -25,6 +25,54 @@ export async function available(organizationId) {
   return false
 }
 
+const actionKitSignup = (cell, contact) => {
+  if (akAddUserUrl && akAddPhoneUrl) {
+    const userData = {
+      email: cell + '-smssubscriber@example.com',
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      sms_subscribed: true,
+      action_mobilesubscribe: true,
+      suppress_subscribe: true,
+      phone: [contactCell],
+      phone_type: 'mobile',
+      source: 'spoke-signup'
+    }
+
+    request.post({
+      url: akAddUserUrl,
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json'
+      },
+      form: userData
+    }, (errorResponse, httpResponse) => {
+      if (errorResponse) throw new Error(errorResponse)
+      if (httpResponse.statusCode === 201) {
+        request.post({
+          url: akAddPhoneUrl,
+          headers: {
+            accept: 'application/json',
+            'content-type': 'application/json'
+          },
+          form: {
+            user: httpResponse.headers.location,
+            phone: contactCell,
+            type: 'mobile'
+          }
+        }, (lastError, lastResponse) => {
+          if (lastError) throw new Error(lastError)
+          if (lastResponse.statusCode === 201) {
+            return
+          }
+        })
+      }
+    })
+  } else {
+    console.log('No AK Post URLs Configured')
+  }
+}
+
 export async function processAction(questionResponse, interactionStep, campaignContactId) {
   const contactRes = await r.knex('campaign_contact')
       .where('campaign_contact.id', campaignContactId)
@@ -75,51 +123,7 @@ export async function processAction(questionResponse, interactionStep, campaignC
 
     return request(options, (error, response) => {
       if (error) throw new Error(error)
-      if (response.statusCode === 204 && akAddUserUrl) {
-        const userData = {
-          email: contactCell + '-smssubscriber@example.com',
-          first_name: contact.first_name,
-          last_name: contact.last_name,
-          sms_subscribed: true,
-          action_mobilesubscribe: true,
-          suppress_subscribe: true,
-          phone: [contactCell],
-          phone_type: 'mobile',
-          source: 'spoke-signup'
-        }
-
-        request.post({
-          url: akAddUserUrl,
-          headers: {
-            accept: 'application/json',
-            'content-type': 'application/json'
-          },
-          form: userData
-        }, (errorResponse, httpResponse) => {
-          if (errorResponse) throw new Error(errorResponse)
-          if (httpResponse.statusCode === 201) {
-            request.post({
-              url: akAddPhoneUrl,
-              headers: {
-                accept: 'application/json',
-                'content-type': 'application/json'
-              },
-              form: {
-                user: httpResponse.headers.location,
-                phone: contactCell,
-                type: 'mobile'
-              }
-            }, (lastError, lastResponse) => {
-              if (lastError) throw new Error(lastError)
-              if (lastResponse.statusCode === 201) {
-                return
-              }
-            })
-          }
-        })
-      } else {
-        return
-      }
     })
   }
+  actionKitSignup(contactCell, contact)
 }
