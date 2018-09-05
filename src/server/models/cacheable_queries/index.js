b2424af8f672e19c163f98e00b9edfe6687a7b46
Schuyler Duveen <sky@skyb.us>
2018-09-05 18:10:53 -0400

    fix cacheableData passing

--- src/server/models/cacheable_queries/index.js
+++ src/server/models/cacheable_queries/index.js
@@ -1,8 +1,14 @@
 export * from './user'
 import { organizationCache } from './organization'
 import { cannedResponseCache } from './canned-response'
+import { campaignContactCache } from './campaign-contact'
 
-export const cacheableData = {
+const cacheableData = {
   organization: organizationCache,
-  cannedResponse: cannedResponseCache
+  cannedResponse: cannedResponseCache,
+  campaignContact: campaignContactCache
+}
+
+export {
+  cacheableData
 }
