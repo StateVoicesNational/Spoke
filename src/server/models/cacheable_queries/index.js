5d2fd35ae7ca29df46f70719aed6f8aa6b1909a8
Schuyler Duveen <sky@skyb.us>
2018-09-06 17:14:59 -0400

    wip: campaignContacdt cache loading and structure. TODO: messageStatus handling with load, clear of course

--- src/server/models/cacheable_queries/index.js
+++ src/server/models/cacheable_queries/index.js
@@ -1,11 +1,11 @@
 export * from './user'
 import { organizationCache } from './organization'
 import { cannedResponseCache } from './canned-response'
-//import { campaignContactCache } from './campaign-contact'
+import { campaignContactCache } from './campaign-contact'
 import { optOutCache } from './opt-out'
 
 const cacheableData = {
-  //campaignContact: campaignContactCache,
+  campaignContact: campaignContactCache,
   cannedResponse: cannedResponseCache,
   optOut: optOutCache,
   organization: organizationCache,
