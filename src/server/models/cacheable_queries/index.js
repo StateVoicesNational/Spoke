9fd81a1196d09f9be74a554d38e3d53829c7e39f
Schuyler Duveen <sky@skyb.us>
2018-09-10 15:27:08 -0400

    export campaign in cacheableData

--- src/server/models/cacheable_queries/index.js
+++ src/server/models/cacheable_queries/index.js
@@ -1,10 +1,12 @@
 export * from './user'
 import { organizationCache } from './organization'
 import { cannedResponseCache } from './canned-response'
+import { campaignCache } from './campaign'
 import { campaignContactCache } from './campaign-contact'
 import { optOutCache } from './opt-out'
 
 const cacheableData = {
+  campaign: campaignCache,
   campaignContact: campaignContactCache,
   cannedResponse: cannedResponseCache,
   optOut: optOutCache,
