ec368dcddc1d279b376d14738ec1d7fe9d2d4973
Americo <ame.zuz@gmail.com>
2018-09-20 17:34:07 -0700

    Add interaction step ids and values to redis cache
    Note: To use a hash in redis, the db result (array of dictionaries) have 
    to be converted to an array (of dictionary values). To pass the values 
    to the front-end, the redis result (dictionary) has to be converted back 
    into an array of dictionaries.
    
    To think about: is there another redis data structure that requires less 
    data prepping?

--- src/server/models/cacheable_queries/question-response.js
+++ src/server/models/cacheable_queries/question-response.js
@@ -18,7 +18,16 @@ const responseCacheKey = (campaignContactId) => (
 const questionResponseCache = {
   query: async (campaignContactId, minimalObj) => {
     if (r.redis && minimalObj) {
-      return await r.redis.hgetallAsync(responseCacheKey(campaignContactId))
+      const cachedResponse = await r.redis.hgetallAsync(
+        responseCacheKey(campaignContactId)
+      )
+      if (cachedResponse) {
+        const formattedResponse = Object.keys(cachedResponse).reduce((acc, key) => {
+          acc.push({interaction_step_id: key, value: cachedResponse[key]})
+          return acc
+        }, [])
+        return formattedResponse
+      }
     }
     return await r.knex('question_response')
       .where('question_response.campaign_contact_id', campaignContactId)
@@ -34,15 +43,24 @@ const questionResponseCache = {
       .where('question_response.campaign_contact_id', campaignContactId)
       .select('value', 'interaction_step_id')
 
+    const valueInteractionArray = questionResponseValues.reduce((acc, qrv) => {
+      acc.push(qrv.interaction_step_id, qrv.value)
+      return acc
+    }, [])
+
     if (r.redis) {
       const cacheKey = responseCacheKey(campaignContactId)
       await r.redis.multi()
         .del(cacheKey)
-        .hmset(cacheKey, ...questionResponseValues)
+        .hmset(cacheKey, ...valueInteractionArray)
         .expire(cacheKey, 86400)
         .execAsync()
     }
   }
 }
+
+// questionResponseValues.reduce((acc, qrv) => {
+//   return acc.push(qrv.value, qrv.interaction_step)
+// }, [])
 
 export default questionResponseCache
