abe50a91ff6f590299bc659fb86b008b96c3cd6d
Shaka Lee <shakalee14@gmail.com>
2018-09-27 14:38:47 -0400

    Adds message for why were returning empty str vs null

--- src/lib/phone-format.js
+++ src/lib/phone-format.js
@@ -3,7 +3,9 @@ import { log } from './log'
 
 export const getFormattedPhoneNumber = (cell, country = 'US') => {
   const phoneUtil = PhoneNumberUtil.getInstance()
-
+  // we return an empty string vs null when the phone number is inValid
+  // because when the cell is null, batch inserts into campaign contacts fail
+  // then when contacts have cell.length < 12, it's deleted before assignments are created
   try {
     const inputNumber = phoneUtil.parse(cell, country)
     const isValid = phoneUtil.isValidNumber(inputNumber)
