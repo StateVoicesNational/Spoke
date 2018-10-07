11498b6dcffb255d88c8d12f3312afa4d37c2996
Larry Person <lp10011@gmail.com>
2018-10-07 11:09:50 -0400

    GET returns count, 404 if campaign not found, 405 if not GET, DELETE, POST

--- src/server/api/contactsApi.js
+++ src/server/api/contactsApi.js
@@ -1,10 +1,25 @@
 import { r } from '../models'
 
-export default async function contactsApi(req, res, next) {
+export default async function contactsApi(req, res) {
   const orgId = req.params.orgId
   const campaignId = req.params.campaignId
 
-  
+  if (!['GET', 'DELETE', 'POST'].includes(req.method)) {
+    res.writeHead(405, { Allow: 'GET, POST, DELETE' })
+    res.end('Not allowed')
+    return
+  }
+
+  const campaignCount = await r
+    .knex('campaign')
+    .where({ organization_id: orgId, id: campaignId })
+    .count('*')
+
+  if (parseInt(campaignCount[0].count) === 0) {
+    res.writeHead(404)
+    res.end('Not found')
+    return
+  }
 
   if (req.method === 'GET') {
     const count = await r
@@ -12,7 +27,7 @@ export default async function contactsApi(req, res, next) {
       .where({ campaign_id: campaignId })
       .count('*')
 
-    const resp = { contacts: count[0] }
+    const resp = { contacts: { count: parseInt(count[0].count) } }
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify(resp))
   }
