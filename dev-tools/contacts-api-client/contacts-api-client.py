4503b9fddcf57f70d1dc23f2d859bfc596a36b7d
Larry Person <lp10011@gmail.com>
2018-10-12 22:53:24 -0400

    improvements based on feedback from code review

--- dev-tools/contacts-api-client/contacts-api-client.py
+++ dev-tools/contacts-api-client/contacts-api-client.py
@@ -17,14 +17,18 @@ def _get_parsed_args():
     arg_parser.add_argument('-p', '--port', help='API port', default=3000)
     arg_parser.add_argument('-c', '--campaign_id', help='ID of campaign to add contacts to', required=True)
     arg_parser.add_argument('-o', '--organization_id', help='ID of organization', required=True)
+    arg_parser.add_argument(
+        '--delete_first', action='store_true', help='Delete all existing contacts for the campaign', required=False)
+    arg_parser.add_argument(
+        '--duplicate_existing',
+        action='store_true',
+        help='Add contacts that duplicate contacts already in the campaign',
+        required=False)
 
     return arg_parser.parse_args()
 
 
 def _prepare_batch(batch):
-    # ujson.dumps({'contacts': batch})
-    # return gzip.compress(bytearray(as_string, 'utf-8'), 9)
-    # return base64.encodebytes(gzipped)
     return batch
 
 
@@ -37,26 +41,22 @@ def main():
     batches = list()
 
     response = requests.get(url)
-    print(response.text)
+    print('Starting campaign status: {campaign_status}\n'.format(campaign_status=response.text))
 
-    # response = requests.delete(url)
-    # print(response.text)
+    if args.delete_first:
+        response = requests.delete(url)
+        print('Delete results: {delete_results}\n'.format(delete_results=response.text))
 
-    # response = requests.get(url)
-    # print(response.text)
+        response = requests.get(url)
+        print('Campaign status after delete: {campaign_status}\n'.format(campaign_status=response.text))
 
     with open(args.file, mode='r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         batch = list()
-        for i, row in enumerate(reader):
-            batch.append(
-                dict(
-                    first_name=row.get('firstName'),
-                    last_name=row.get('lastName'),
-                    cell=row.get('cell'),
-                    external_id=row.get('external_id'),
-                    # custom_fields=row.get('customFields'),
-                    zip=row.get('zip')))
+        for i, row in enumerate(reader, start=1):
+            contact = dict(first_name=row.get('firstName'), last_name=row.get('lastName'))
+            contact.update({key: row[key] for key in row.keys() if key not in ['firstName', 'lastName']})
+            batch.append(contact)
 
             if i > 0 and not i % args.batch_size:
                 batches.append(_prepare_batch(batch))
@@ -66,9 +66,15 @@ def main():
             batches.append(_prepare_batch(batch))
 
     for batch in batches:
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, json=batch, headers=headers)
+        post_url = url
+        if args.duplicate_existing:
+            post_url = post_url+'?duplicate_existing'
+
+        response = requests.post(post_url, json=batch, headers={'Content-Type': 'application/json'})
         print(response.text)
+
+    response = requests.get(url)
+    print('\nCampaign status after upload: {campaign_status}\n'.format(campaign_status=response.text))
 
 
 if __name__ == '__main__':
--- src/server/api/contactsApi.js
+++ src/server/api/contactsApi.js
@@ -4,9 +4,9 @@ import { getTimezoneByZip, getOptOutSubQuery } from '../../workers/jobs'
 
 import { getValidatedData } from '../../lib'
 
-function campaignaStatusShortCircuit(campaign, res) {
+function campaignStatusShortCircuit(campaign, res) {
   let message = ''
-  if (campaign.is_archivedj) {
+  if (campaign.is_archived) {
     message = 'Campaign is archived'
   } else if (campaign.is_started) {
     message = 'Campaign is started'
@@ -21,7 +21,6 @@ function campaignaStatusShortCircuit(campaign, res) {
   return false
 }
 
-// TODO(lperson) enforce that this is not reentrant
 // TODO(lperson) enforce auth
 export default async function contactsApi(req, res) {
   const orgId = req.params.orgId
@@ -33,133 +32,144 @@ export default async function contactsApi(req, res) {
     return
   }
 
-  const campaignArray = await r
+  const [campaign] = await r
     .knex('campaign')
     .select()
     .where({ organization_id: orgId, id: campaignId })
 
-  if (campaignArray.length !== 1) {
+  if (!campaign) {
     res.writeHead(404)
     res.end('Not found')
     return
   }
 
-  const campaign = campaignArray[0]
+  if (['DELETE', 'POST'].includes(req.method)) {
+    if (campaignStatusShortCircuit(campaign, res)) {
+      return
+    }
+  }
 
-  let resp = {}
+  let resp = null
 
-  switch (req.method) {
-    case 'GET':
-      const count = await r
+  if (req.method === 'GET') {
+    const count = await r.getCount(
+      r.knex('campaign_contact').where({ campaign_id: campaignId })
+    )
+
+    resp = {
+      contacts: {
+        count: parseInt(count)
+      },
+      status: {
+        started: campaign.is_started,
+        archived: campaign.is_archived
+      }
+    }
+  } else if (req.method === 'DELETE') {
+    const deleted = await r
+      .knex('campaign_contact')
+      .where({ campaign_id: campaignId })
+      .delete()
+
+    resp = { contacts: { deleted } }
+  } else if (req.method === 'POST') {
+    const { validationStats, validatedData } = getValidatedData(req.body, [])
+
+    const successResponse = {
+      invalid: _.concat(
+        validationStats.invalidCellRows,
+        validationStats.missingCellRows
+      ),
+      dupes_in_batch: validationStats.dupeCount,
+      number_submitted: req.body.length
+    }
+
+    let validatedContactsToSave = validatedData
+
+    if (!('duplicate_existing' in req.query)) {
+      const existingContacts = await r
         .knex('campaign_contact')
-        .where({ campaign_id: campaignId })
-        .count('*')
+        .select('cell')
+        .whereIn('cell', validatedData.map(contact => contact.cell))
+        .andWhere({ campaign_id: campaignId })
 
-      resp = { contacts: { count: parseInt(count[0].count) } }
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(resp))
-      break
+      const existingContactCells = new Set(
+        existingContacts.map(contact => contact.cell)
+      )
 
-    case 'DELETE':
-      if (campaignaStatusShortCircuit(campaign, res)) {
-        break
-      }
+      const dedupedContactsToSave = _.filter(
+        validatedData,
+        contact => !existingContactCells.has(contact.cell)
+      )
 
-      const deleted = await r
-        .knex('campaign_contact')
-        .delete()
-        .where({ campaign_id: campaignId })
+      successResponse.dupes_in_campaign =
+        validatedData.length - dedupedContactsToSave.length
 
-      resp = { contacts: { deleted } }
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(resp))
-      break
+      validatedContactsToSave = dedupedContactsToSave
+    }
 
-    case 'POST':
-      if (campaignaStatusShortCircuit(campaign, res)) {
-        break
-      }
+    const standardFields = [
+      'first_name',
+      'last_name',
+      'cell',
+      'external_id',
+      'zip',
+      'timezone_offset'
+    ]
 
-      const { validationStats, validatedData } = getValidatedData(req.body, [])
-
-      const successResponse = {
-        invalid: _.concat(
-          validationStats.invalidCellRows,
-          validationStats.missingCellRows
-        ),
-        dupesInBatch: validationStats.dupeCount,
-        numberSubmitted: req.body.length
-      }
-
-      const standardFields = [
-        'first_name',
-        'last_name',
-        'cell',
-        'external_id',
-        'zip'
-      ]
-
-      const contactsToSavePromises = validatedData.map(async contact => {
+    const contactsToSavePromises = validatedContactsToSave.map(
+      async contact => {
         const contactToSave = _.pick(contact, standardFields)
         const customFields = _.omit(contact, standardFields)
         contactToSave.custom_fields = JSON.stringify(customFields)
-        contactToSave.timezone_offset = await getTimezoneByZip(
-          contactToSave.zip
-        )
+        if (!contactToSave.timezone_offset) {
+          contactToSave.timezone_offset = await getTimezoneByZip(
+            contactToSave.zip
+          )
+        }
         return contactToSave
+      }
+    )
+
+    const contactsToSave = await Promise.all(contactsToSavePromises)
+
+    await r.knex
+      .transaction(async tr => {
+        await tr
+          .batchInsert(
+            'campaign_contact',
+            contactsToSave.map(row => {
+              row.campaign_id = campaignId
+              return _.omitBy(row, v => v === null)
+            }),
+            req.body.length
+          )
+          .then(async () => {
+            const optOutCellCount = await tr('campaign_contact')
+              .whereIn('cell', getOptOutSubQuery(orgId))
+              .where('campaign_id', campaignId)
+              .delete()
+
+            successResponse.opted_out = optOutCellCount
+            successResponse.added =
+              validatedContactsToSave.length - optOutCellCount
+          })
       })
+      .then(function() {
+        resp = successResponse
+      })
+      .catch(function(error) {
+        resp = { error }
+        console.log(error)
+      })
+  }
 
-      const contactsToSave = await Promise.all(contactsToSavePromises)
-
-      await r.knex
-        .transaction(async tr => {
-          const existingContacts = await tr('campaign_contact')
-            .select('cell')
-            .whereIn('cell', contactsToSave.map(contact => contact.cell))
-            .andWhere({ campaign_id: campaignId })
-
-          const existingContactCells = new Set(
-            existingContacts.map(contact => contact.cell)
-          )
-
-          const dedupedContactsToSave = _.filter(
-            contactsToSave,
-            contact => !existingContactCells.has(contact.cell)
-          )
-
-          successResponse['dupesInCampaign'] =
-            contactsToSave.length - dedupedContactsToSave.length
-
-          await tr
-            .batchInsert(
-              'campaign_contact',
-              dedupedContactsToSave.map(row => {
-                row.campaign_id = campaignId
-                return _.omitBy(row, (v, k) => v === null)
-              }),
-              req.body.length
-            )
-            .then(async () => {
-              const optOutCellCount = await tr('campaign_contact')
-                .whereIn('cell', getOptOutSubQuery(orgId))
-                .where('campaign_id', campaignId)
-                .delete()
-
-              successResponse['optedOut'] = optOutCellCount
-              successResponse['added'] =
-                dedupedContactsToSave.length - optOutCellCount
-            })
-        })
-        .then(function() {
-          resp = successResponse
-        })
-        .catch(function(error) {
-          resp = { error }
-          console.log(error)
-        })
-
-      resp = { resp }
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(resp))
+  if (resp) {
+    resp = { resp }
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(resp))
+  } else {
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ error: 'Internal server error' }))
   }
 }
