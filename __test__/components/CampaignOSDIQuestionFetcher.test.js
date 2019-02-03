2fe1f695b9a637988b6845006552dbb997aff007
Harpo Jaeger <harpo.jaeger@gmail.com>
2019-02-02 20:56:42 -0500

    add a test assertion for when no OSDI questions are present

--- __test__/components/CampaignOSDIQuestionFetcher.test.js
+++ __test__/components/CampaignOSDIQuestionFetcher.test.js
@@ -10,26 +10,49 @@ import sampleOsdiQuestions from '../test_data/sample_osdi_questions.json'
 // }
 
 describe('The OSDI question fetcher component', () => {
-  it('renders the correct number of questions in response to valid OSDI input', () => {
-    const fakeQuestionMapFn = jest.fn();
-    const questionObject = {
+  let questionObject
+  beforeEach(() => {
+    // Set up the question object to be passed to
+    // <CampaignOSDIQuestionFetcher /> as a prop.
+    questionObject = {
       organization: {
-        osdiQuestions: []
+        // Annoyingly, because of the way our GraphQL API is set up, this
+        // component expects an array stringified JSON objects representing the
+        // questions.
+        osdiQuestions: sampleOsdiQuestions.map(JSON.stringify)
       }
     }
-    sampleOsdiQuestions.forEach(q => questionObject.organization.osdiQuestions.push(JSON.stringify(q)))
+  })
+  
+  it('renders correctly when no OSDI questions are present', () => {
+    // Set the question object to have no questions
+    questionObject.organization.osdiQuestions = []
+
+    // We're not interested in the behavior of the mapQuestion function at this
+    // point, so we won't bother mocking it. It will be tested later.
     const wrapper = shallow(
       <CampaignOSDIQuestionFetcher
         organizationId = {'42'}
         osdiQuestions = {questionObject}
-        mapQuestion = {fakeQuestionMapFn}
+        mapQuestion = {() => null}
+      />
+    )
+    const noQuestionsFoundDiv = <div>No questions were found. Please check your OSDI credentials and make sure questions exist in the target system.</div>
+    expect(wrapper.containsMatchingElement(noQuestionsFoundDiv)).toBe(true)
+  })
+
+  it('renders the correct number of list items when OSDI questions are present', () => {
+    const wrapper = shallow(
+      <CampaignOSDIQuestionFetcher
+        organizationId = {'42'}
+        osdiQuestions = {questionObject}
+        mapQuestion = {() => null}
       />
     )
     const questionsSelectField = wrapper.find('div SelectField');
     const questionsSelectFieldItems = questionsSelectField.children();
-    // The question fetcher prepends a "Select a question..." item to the list, so we expect one more list items than there are questions in our fake input.
-    expect(questionsSelectFieldItems.length).toBe(sampleOsdiQuestions.length + 1);
+    // The question fetcher prepends a "Select a question..." item to the list,
+    // so we expect one more item than there are questions in our fake input.
+    expect(questionsSelectFieldItems.length).toBe(4);
   })
-
-
 })
