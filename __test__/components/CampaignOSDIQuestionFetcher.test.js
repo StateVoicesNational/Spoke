f1c415408b21cef8685923cb9eb978aa3e937730
Harpo Jaeger <harpo.jaeger@gmail.com>
2019-02-02 21:42:01 -0500

    test that the OSDI question mapper calls the mapQuestion function it receives from its parent component

--- __test__/components/CampaignOSDIQuestionFetcher.test.js
+++ __test__/components/CampaignOSDIQuestionFetcher.test.js
@@ -23,7 +23,7 @@ describe('The OSDI question fetcher component', () => {
       }
     }
   })
-  
+
   it('renders correctly when no OSDI questions are present', () => {
     // Set the question object to have no questions
     questionObject.organization.osdiQuestions = []
@@ -54,5 +54,36 @@ describe('The OSDI question fetcher component', () => {
     // The question fetcher prepends a "Select a question..." item to the list,
     // so we expect one more item than there are questions in our fake input.
     expect(questionsSelectFieldItems.length).toBe(4);
+  })
+
+  it('triggers the mapQuestion function from its parent component', () => {
+    const mockedQuestionMapFn = jest.fn()
+    const wrapper = shallow(
+      <CampaignOSDIQuestionFetcher
+        organizationId = {'42'}
+        osdiQuestions = {questionObject}
+        mapQuestion = {mockedQuestionMapFn}
+      />
+    )
+    const questionsSelectField = wrapper.find('div SelectField');
+
+    // Simulate selection of the first question in the list
+    // (element 0 is the "Select a question..." item). Only the final argument
+    // here is actually used by the CampaignOSDIQuestionFetcher.
+    questionsSelectField.simulate('change', {target: {value: null}}, 1, 1)
+
+    // Simulate the user clicking "Map" after selecting the question.
+    const mapQuestionButton = wrapper.find('div RaisedButton')
+    mapQuestionButton.simulate('touchTap')
+
+    // Clicking "Map" should have triggered the mapQuestion mock function.
+    expect(mockedQuestionMapFn.mock.calls.length).toEqual(1)
+
+    // We call the mapQuestion prop with a big old object as the only argument,
+    // so we destructure twice here to get it out in the open for assertions.
+    const [[argument]] = mockedQuestionMapFn.mock.calls
+
+    // Ensure we passed the correct question to mapQuestion.
+    expect(argument.questionId).toEqual('https://osdi-sample-system.org/api/v1/questions/a91b4b2e-ae0e-4cd3-9ed7-d0ec501b0baa');
   })
 })
