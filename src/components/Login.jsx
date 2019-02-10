2416cb992ca33fc1912c3e359d1381404c19085e
Americo <ame.zuz@gmail.com>
2019-02-09 17:44:47 -0800

    Lint

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,21 +6,21 @@ import UserEdit from '../containers/UserEdit'
 const Login = ({ location }) => (
   <div>
     {isClient() ? '' /*window.AuthService.login(location.query.nextUrl)*/ : ''}
-    <form action="/login-callback" method="POST">
-      <input type="hidden" name="nextUrl" value={location.query.nextUrl} />
+    <form action='/login-callback' method='POST'>
+      <input type='hidden' name='nextUrl' value={location.query.nextUrl} />
       <div>
         <label>email:</label>
-        <input type="text" name="email"/><br/>
+        <input type='text' name='email' /><br />
       </div>
       <div>
 	<label>Password:</label>
-	<input type="password" name="password"/>
+        <input type='password' name='password' />
       </div>
       <div>
-	<input type="submit" value="Submit"/>
+        <input type='submit' value='Submit' />
       </div>
     </form>
-    <UserEdit  />
+    <UserEdit />
   </div>
 )
 
