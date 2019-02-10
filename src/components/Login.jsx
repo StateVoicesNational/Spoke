e3fa349d8db61980896a003944acf76aecf261b3
Americo <ame.zuz@gmail.com>
2019-02-09 18:45:30 -0800

    Display auth0 login or local login based on env variable

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,35 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { isClient } from '../lib'
-import UserEdit from '../containers/UserEdit'
 
-const Login = ({ location }) => (
-  <div>
-    {isClient() ? '' /*window.AuthService.login(location.query.nextUrl)*/ : ''}
-    <form action='/login-callback' method='POST'>
-      <input type='hidden' name='nextUrl' value={location.query.nextUrl} />
-      <div>
-        <label>email:</label>
-        <input type='text' name='email' /><br />
-      </div>
-      <div>
-	<label>Password:</label>
-        <input type='password' name='password' />
-      </div>
-      <div>
-        <input type='submit' value='Submit' />
-      </div>
-    </form>
-    <UserEdit />
-  </div>
-)
+const Login = ({ location }) => {
+  const auth0Strategy = window.PASSPORT_STRATEGY === 'auth0' || window.PASSPORT_STRATEGY === ''
+  const auth0Login = isClient() && auth0Strategy
+  console.log('authstrategy', auth0Strategy,
+    'window.PASSPORT_STRATEGY === "auth0"', window.PASSPORT_STRATEGY === 'auth0',
+    'window.PASSPORT_STRATEGY === ""', window.PASSPORT_STRATEGY === ''
+  )
+  return (
+    <div>
+      {auth0Login ? window.AuthService.login(location.query.nextUrl) : ''}
+      {!auth0Login &&
+        <form action='/login-callback' method='POST'>
+          <input type='hidden' name='nextUrl' value={location.query.nextUrl} />
+          <div>
+            <label>email:</label>
+            <input type='text' name='email' /><br />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type='password' name='password' />
+          </div>
+          <div>
+            <input type='submit' value='Submit' />
+          </div>
+        </form>}
+    </div>
+  )
+}
 
 Login.propTypes = {
   location: PropTypes.object
