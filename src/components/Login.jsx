e28a51460adae7e87ac315488f849f331f62c91e
Americo <ame.zuz@gmail.com>
2019-02-24 13:46:51 -0800

    Style and only render sign up if nextUrl search query is present

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,50 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { withRouter } from 'react-router'
 
+import { StyleSheet, css } from 'aphrodite'
+import theme from '../styles/theme'
+
 import { isClient } from '../lib'
 import UserEdit from '../containers/UserEdit'
 
+const styles = StyleSheet.create({
+  authFields: {
+    display: 'flex',
+    'flex-direction': 'column'
+  },
+  fieldContainer: {
+    background: theme.colors.white,
+    padding: '15px'
+  },
+  authContainer: {
+    display: 'flex',
+    'justify-content': 'center',
+    'align-items': 'flex-start',
+    height: '100vh',
+    'padding-top': '10vh',
+    background: theme.colors.veryLightGray
+  },
+  button: {
+    border: 'none',
+    background: theme.colors.lightGray,
+    color: theme.colors.green,
+    padding: '16px 16px',
+    'font-size': '14px',
+    'text-transform': 'uppercase',
+    cursor: 'pointer',
+    width: '50%',
+    'transition-timing-function': 'linear',
+    'transition-duration': '0.05s',
+    ':disabled': {
+      background: theme.colors.white,
+      cursor: 'default',
+      color: theme.colors.darkGreen
+    }
+  }
+})
+
 class Login extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
@@ -26,37 +66,46 @@ class Login extends React.Component {
     const { location, router } = this.props
 
     return (
-      <div>
+      <div className={css(styles.authContainer)}>
         {/* Use auth0 */}
         {auth0Login && window.AuthService.login(location.query.nextUrl)}
 
         {/* Show UserEdit component configured for login / signup */}
-        <section>
-          <button
-            type='button'
-            name='login'
-            onClick={this.handleClick}
-            disabled={this.state.active === 'login'}
-          >
-            Log in
-          </button>
-          <button
-            type='button'
-            name='signup'
-            onClick={this.handleClick}
-            disabled={this.state.active === 'signup'}
-          >
-            Sign up
-          </button>
-        </section >
-
         {!auth0Login &&
-          <UserEdit
-            authType={this.state.active}
-            saveLabel={this.state.active === 'login' ? 'Log in' : 'Sign up'}
-            router={router}
-            location={location}
-          />
+          <div>
+            {/* Only display sign up option if there is a nextUrl */}
+            {location.search &&
+              <section>
+                <button
+                  className={css(styles.button)}
+                  type='button'
+                  name='login'
+                  onClick={this.handleClick}
+                  disabled={this.state.active === 'login'}
+                >
+                  Log In
+                </button>
+                <button
+                  className={css(styles.button)}
+                  type='button'
+                  name='signup'
+                  onClick={this.handleClick}
+                  disabled={this.state.active === 'signup'}
+                >
+                  Sign Up
+                </button>
+              </section >
+            }
+            <div className={css(styles.fieldContainer)}>
+              <UserEdit
+                authType={this.state.active}
+                saveLabel={this.state.active === 'login' ? 'Log in' : 'Sign up'}
+                router={router}
+                location={location}
+                style={css(styles.authFields)}
+              />
+            </div>
+          </div>
         }
       </div >
     )
