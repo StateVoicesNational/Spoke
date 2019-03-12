bb71ef99823962f496516cf9423b84307a73f08e
Americo <ame.zuz@gmail.com>
2019-03-12 07:21:44 -0700

    Load home on successful password reset

--- src/routes.jsx
+++ src/routes.jsx
@@ -109,7 +109,7 @@ export default function makeRoutes(requireAuth = () => { }) {
       </Route>
       <Route path='login' component={Login} />
       <Route path='terms' component={Terms} />
-      <Route path='reset/:resetHash' onEnter={requireAuth} />
+      <Route path='reset/:resetHash' component={Home} onEnter={requireAuth} />
       <Route path='invite/:inviteId' component={CreateOrganization} onEnter={requireAuth} />
       <Route path=':organizationUuid/join/:campaignId' component={JoinTeam} onEnter={requireAuth} />
       <Route path=':organizationUuid/join' component={JoinTeam} onEnter={requireAuth} />
