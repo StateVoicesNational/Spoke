48b911c691b018a2cfd6d5ccbd7885bf2159d832
Americo <ame.zuz@gmail.com>
2019-04-03 15:56:45 -0700

    Rename function to be more explicit and avoid merge conflicts down the line

--- src/server/auth-passport.js
+++ src/server/auth-passport.js
@@ -5,7 +5,7 @@ import { userLoggedIn } from './models/cacheable_queries'
 import { User } from './models'
 import localAuthHelpers from './local-auth-helpers'
 import wrap from './wrap'
-import { capitalize } from './api/lib/utils'
+import { capitalizeWord } from './api/lib/utils'
 
 
 export function setupAuth0Passport() {
@@ -53,9 +53,9 @@ export function setupAuth0Passport() {
           const userData = {
             auth0_id: auth0Id,
             // eslint-disable-next-line no-underscore-dangle
-            first_name: capitalize(userMetadata.given_name) || '',
+            first_name: capitalizeWord(userMetadata.given_name) || '',
             // eslint-disable-next-line no-underscore-dangle
-            last_name: capitalize(userMetadata.family_name) || '',
+            last_name: capitalizeWord(userMetadata.family_name) || '',
             cell: userMetadata.cell || '',
             // eslint-disable-next-line no-underscore-dangle
             email: req.user._json.email,
