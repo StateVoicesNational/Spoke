aa43e89f43af00e5ff2623dc78d137f98bab7658
Rebecca <rebecca@casecommons.org>
2019-06-18 20:10:37 -0400

    Removing a file that leaked from a different branch

--- __test__/lib/format-text.test.js
+++
@@ -1,8 +0,0 @@
-import {getScriptFieldValue} from '../../src/lib/scripts'
-
-describe('stripWhitespace', () => {
-  it('should remove leading and/or trailing whitespace', () => {
-    let test
-    test = applyScript()
-  })
-})
