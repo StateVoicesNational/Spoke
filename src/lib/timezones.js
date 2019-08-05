d6408b16744c04edf4e93f87c2ec8d844ac522e2
Ilona Brand <theilonabrand@gmail.com>
2019-08-05 16:37:28 -0400

    run prettier on all files

--- src/lib/timezones.js
+++ src/lib/timezones.js
@@ -1,7 +1,10 @@
-import moment from 'moment-timezone'
+import moment from "moment-timezone";
 
-import { getProcessEnvTz, getProcessEnvDstReferenceTimezone } from '../lib/tz-helpers'
-import { DstHelper } from './dst-helper'
+import {
+  getProcessEnvTz,
+  getProcessEnvDstReferenceTimezone
+} from "../lib/tz-helpers";
+import { DstHelper } from "./dst-helper";
 
 const TIMEZONE_CONFIG = {
   missingTimeZone: {
@@ -10,44 +13,63 @@ const TIMEZONE_CONFIG = {
     allowedStart: 12, // 12pm EST/9am PST
     allowedEnd: 21 // 9pm EST/6pm PST
   }
-}
+};
 
 export const getContactTimezone = (campaign, location) => {
-  const returnLocation = Object.assign({}, location)
+  const returnLocation = Object.assign({}, location);
   if (location.timezone == null || location.timezone.offset == null) {
-    let timezoneData = null
+    let timezoneData = null;
 
     if (campaign.overrideOrganizationTextingHours) {
-      const offset = DstHelper.getTimezoneOffsetHours(campaign.timezone)
-      const hasDST = DstHelper.timezoneHasDst(campaign.timezone)
-      timezoneData = { offset, hasDST }
+      const offset = DstHelper.getTimezoneOffsetHours(campaign.timezone);
+      const hasDST = DstHelper.timezoneHasDst(campaign.timezone);
+      timezoneData = { offset, hasDST };
     } else if (getProcessEnvTz()) {
-      const offset = DstHelper.getTimezoneOffsetHours(getProcessEnvTz())
-      const hasDST = DstHelper.timezoneHasDst(getProcessEnvTz())
-      timezoneData = { offset, hasDST }
+      const offset = DstHelper.getTimezoneOffsetHours(getProcessEnvTz());
+      const hasDST = DstHelper.timezoneHasDst(getProcessEnvTz());
+      timezoneData = { offset, hasDST };
     } else {
-      const offset = TIMEZONE_CONFIG.missingTimeZone.offset
-      const hasDST = TIMEZONE_CONFIG.missingTimeZone.hasDST
-      timezoneData = { offset, hasDST }
+      const offset = TIMEZONE_CONFIG.missingTimeZone.offset;
+      const hasDST = TIMEZONE_CONFIG.missingTimeZone.hasDST;
+      timezoneData = { offset, hasDST };
     }
-    returnLocation.timezone = timezoneData
+    returnLocation.timezone = timezoneData;
   }
-  return returnLocation
-}
+  return returnLocation;
+};
 
-export const getUtcFromOffsetAndHour = (offset, hasDst, hour, dstReferenceTimezone) => {
-  const isDst = moment().tz(dstReferenceTimezone).isDST()
-  return moment().utcOffset(offset + ((hasDst && isDst) ? 1 : 0)).hour(hour).startOf('hour').utc()
-}
+export const getUtcFromOffsetAndHour = (
+  offset,
+  hasDst,
+  hour,
+  dstReferenceTimezone
+) => {
+  const isDst = moment()
+    .tz(dstReferenceTimezone)
+    .isDST();
+  return moment()
+    .utcOffset(offset + (hasDst && isDst ? 1 : 0))
+    .hour(hour)
+    .startOf("hour")
+    .utc();
+};
 
 export const getUtcFromTimezoneAndHour = (timezone, hour) => {
-  return moment().tz(timezone).hour(hour).startOf('hour').utc()
-}
+  return moment()
+    .tz(timezone)
+    .hour(hour)
+    .startOf("hour")
+    .utc();
+};
 
-export const getSendBeforeTimeUtc = (contactTimezone, organization, campaign) => {
+export const getSendBeforeTimeUtc = (
+  contactTimezone,
+  organization,
+  campaign
+) => {
   if (campaign.overrideOrganizationTextingHours) {
     if (!campaign.textingHoursEnforced) {
-      return null
+      return null;
     }
 
     if (contactTimezone && contactTimezone.offset) {
@@ -56,24 +78,24 @@ export const getSendBeforeTimeUtc = (contactTimezone, organization, campaign) =>
         contactTimezone.hasDST,
         campaign.textingHoursEnd,
         campaign.timezone
-      )
+      );
     } else {
       return getUtcFromTimezoneAndHour(
         campaign.timezone,
         campaign.textingHoursEnd
-      )
+      );
     }
   }
 
   if (!organization.textingHoursEnforced) {
-    return null
+    return null;
   }
 
   if (getProcessEnvTz()) {
     return getUtcFromTimezoneAndHour(
       getProcessEnvTz(),
       organization.textingHoursEnd
-    )
+    );
   }
 
   if (contactTimezone && contactTimezone.offset) {
@@ -82,73 +104,90 @@ export const getSendBeforeTimeUtc = (contactTimezone, organization, campaign) =>
       contactTimezone.hasDST,
       organization.textingHoursEnd,
       getProcessEnvDstReferenceTimezone()
-    )
+    );
   } else {
     return getUtcFromOffsetAndHour(
       TIMEZONE_CONFIG.missingTimeZone.offset,
       TIMEZONE_CONFIG.missingTimeZone.hasDST,
       organization.textingHoursEnd,
       getProcessEnvDstReferenceTimezone()
-    )
+    );
   }
-}
+};
 
 export const getLocalTime = (offset, hasDST, dstReferenceTimezone) => {
-  return moment().utc().utcOffset(DstHelper.isDateDst(new Date(), dstReferenceTimezone) && hasDST ? offset + 1 : offset)
-}
+  return moment()
+    .utc()
+    .utcOffset(
+      DstHelper.isDateDst(new Date(), dstReferenceTimezone) && hasDST
+        ? offset + 1
+        : offset
+    );
+};
 
-const isOffsetBetweenTextingHours = (offsetData, textingHoursStart, textingHoursEnd, missingTimezoneConfig, dstReferenceTimezone) => {
-  let offset
-  let hasDST
-  let allowedStart
-  let allowedEnd
+const isOffsetBetweenTextingHours = (
+  offsetData,
+  textingHoursStart,
+  textingHoursEnd,
+  missingTimezoneConfig,
+  dstReferenceTimezone
+) => {
+  let offset;
+  let hasDST;
+  let allowedStart;
+  let allowedEnd;
   if (offsetData && offsetData.offset) {
-    allowedStart = textingHoursStart
-    allowedEnd = textingHoursEnd
-    offset = offsetData.offset
-    hasDST = offsetData.hasDST
+    allowedStart = textingHoursStart;
+    allowedEnd = textingHoursEnd;
+    offset = offsetData.offset;
+    hasDST = offsetData.hasDST;
   } else {
-    allowedStart = missingTimezoneConfig.allowedStart
-    allowedEnd = missingTimezoneConfig.allowedEnd
-    offset = missingTimezoneConfig.offset
-    hasDST = missingTimezoneConfig.hasDST
+    allowedStart = missingTimezoneConfig.allowedStart;
+    allowedEnd = missingTimezoneConfig.allowedEnd;
+    offset = missingTimezoneConfig.offset;
+    hasDST = missingTimezoneConfig.hasDST;
   }
 
-  const localTime = getLocalTime(offset, hasDST, dstReferenceTimezone)
-  return (localTime.hours() >= allowedStart && localTime.hours() < allowedEnd)
-}
+  const localTime = getLocalTime(offset, hasDST, dstReferenceTimezone);
+  return localTime.hours() >= allowedStart && localTime.hours() < allowedEnd;
+};
 
 export const isBetweenTextingHours = (offsetData, config) => {
   if (config.campaignTextingHours) {
     if (!config.campaignTextingHours.textingHoursEnforced) {
-      return true
+      return true;
     }
   } else if (!config.textingHoursEnforced) {
-    return true
+    return true;
   }
 
   if (config.campaignTextingHours) {
-    const { campaignTextingHours } = config
+    const { campaignTextingHours } = config;
     const missingTimezoneConfig = {
       allowedStart: campaignTextingHours.textingHoursStart,
       allowedEnd: campaignTextingHours.textingHoursEnd,
       offset: DstHelper.getTimezoneOffsetHours(campaignTextingHours.timezone),
       hasDST: DstHelper.timezoneHasDst(campaignTextingHours.timezone)
-    }
+    };
 
     return isOffsetBetweenTextingHours(
       offsetData,
       campaignTextingHours.textingHoursStart,
       campaignTextingHours.textingHoursEnd,
       missingTimezoneConfig,
-      campaignTextingHours.timezone)
+      campaignTextingHours.timezone
+    );
   }
 
   if (getProcessEnvTz()) {
-    const today = moment.tz(getProcessEnvTz()).format('YYYY-MM-DD')
-    const start = moment.tz(`${today}`, getProcessEnvTz()).add(config.textingHoursStart, 'hours')
-    const stop = moment.tz(`${today}`, getProcessEnvTz()).add(config.textingHoursEnd, 'hours')
-    return moment.tz(getProcessEnvTz()).isBetween(start, stop, null, '[]')
+    const today = moment.tz(getProcessEnvTz()).format("YYYY-MM-DD");
+    const start = moment
+      .tz(`${today}`, getProcessEnvTz())
+      .add(config.textingHoursStart, "hours");
+    const stop = moment
+      .tz(`${today}`, getProcessEnvTz())
+      .add(config.textingHoursEnd, "hours");
+    return moment.tz(getProcessEnvTz()).isBetween(start, stop, null, "[]");
   }
 
   return isOffsetBetweenTextingHours(
@@ -156,42 +195,42 @@ export const isBetweenTextingHours = (offsetData, config) => {
     config.textingHoursStart,
     config.textingHoursEnd,
     TIMEZONE_CONFIG.missingTimeZone,
-    getProcessEnvDstReferenceTimezone())
-}
-
+    getProcessEnvDstReferenceTimezone()
+  );
+};
 
 // Currently USA (-4 through -11) and Australia (10)
-const ALL_OFFSETS = [-4, -5, -6, -7, -8, -9, -10, -11, 10]
+const ALL_OFFSETS = [-4, -5, -6, -7, -8, -9, -10, -11, 10];
 
-export const defaultTimezoneIsBetweenTextingHours = (config) => isBetweenTextingHours(null, config)
+export const defaultTimezoneIsBetweenTextingHours = config =>
+  isBetweenTextingHours(null, config);
 
 export function convertOffsetsToStrings(offsetArray) {
-  const result = []
-  offsetArray.forEach((offset) => {
-    result.push((offset[0].toString() + '_' + (offset[1] === true ? '1' : '0')))
-  })
-  return result
+  const result = [];
+  offsetArray.forEach(offset => {
+    result.push(offset[0].toString() + "_" + (offset[1] === true ? "1" : "0"));
+  });
+  return result;
 }
 
-export const getOffsets = (config) => {
-  const offsets = ALL_OFFSETS.slice(0)
+export const getOffsets = config => {
+  const offsets = ALL_OFFSETS.slice(0);
 
-  const valid = []
-  const invalid = []
+  const valid = [];
+  const invalid = [];
 
-  const dst = [true, false]
-  dst.forEach((hasDST) => (
-    offsets.forEach((offset) => {
+  const dst = [true, false];
+  dst.forEach(hasDST =>
+    offsets.forEach(offset => {
       if (isBetweenTextingHours({ offset, hasDST }, config)) {
-        valid.push([offset, hasDST])
+        valid.push([offset, hasDST]);
       } else {
-        invalid.push([offset, hasDST])
+        invalid.push([offset, hasDST]);
       }
     })
+  );
 
-  ))
-
-  const convertedValid = convertOffsetsToStrings(valid)
-  const convertedInvalid = convertOffsetsToStrings(invalid)
-  return [convertedValid, convertedInvalid]
-}
+  const convertedValid = convertOffsetsToStrings(valid);
+  const convertedInvalid = convertOffsetsToStrings(invalid);
+  return [convertedValid, convertedInvalid];
+};
--- src/network/apollo-client-singleton.js
+++ src/network/apollo-client-singleton.js
@@ -1,37 +1,42 @@
-import ApolloClient, { addQueryMerging } from 'apollo-client'
-import ResponseMiddlewareNetworkInterface from './response-middleware-network-interface'
-import { log } from '../lib'
-import fetch from 'isomorphic-fetch'
-import { graphQLErrorParser } from './errors'
+import ApolloClient, { addQueryMerging } from "apollo-client";
+import ResponseMiddlewareNetworkInterface from "./response-middleware-network-interface";
+import { log } from "../lib";
+import fetch from "isomorphic-fetch";
+import { graphQLErrorParser } from "./errors";
 
 const responseMiddlewareNetworkInterface = new ResponseMiddlewareNetworkInterface(
-  process.env.GRAPHQL_URL || '/graphql', { credentials: 'same-origin' }
-)
+  process.env.GRAPHQL_URL || "/graphql",
+  { credentials: "same-origin" }
+);
 
 responseMiddlewareNetworkInterface.use({
   applyResponseMiddleware: (response, next) => {
-    const parsedError = graphQLErrorParser(response)
+    const parsedError = graphQLErrorParser(response);
     if (parsedError) {
-      log.debug(parsedError)
+      log.debug(parsedError);
       if (parsedError.status === 401) {
-        window.location = `/login?nextUrl=${window.location.pathname}`
+        window.location = `/login?nextUrl=${window.location.pathname}`;
       } else if (parsedError.status === 403) {
-        window.location = '/'
+        window.location = "/";
       } else if (parsedError.status === 404) {
-        window.location = '/404'
+        window.location = "/404";
       } else {
-        log.error(`GraphQL request resulted in error:\nRequest:${JSON.stringify(response.data)}\nError:${JSON.stringify(response.errors)}`)
+        log.error(
+          `GraphQL request resulted in error:\nRequest:${JSON.stringify(
+            response.data
+          )}\nError:${JSON.stringify(response.errors)}`
+        );
       }
     }
-    next()
+    next();
   }
-})
+});
 
-const networkInterface = addQueryMerging(responseMiddlewareNetworkInterface)
+const networkInterface = addQueryMerging(responseMiddlewareNetworkInterface);
 
 const ApolloClientSingleton = new ApolloClient({
   networkInterface,
   shouldBatch: true,
-  dataIdFromObject: (result) => result.id
-})
-export default ApolloClientSingleton
+  dataIdFromObject: result => result.id
+});
+export default ApolloClientSingleton;
