6ad2bfc3ea4ea158d65be7bb0184e0e60afabf5d
Zac <45137225+zhaluza@users.noreply.github.com>
2019-09-13 18:30:24 -0400

    Changed "canned responses" to "other responses"

--- src/containers/AssignmentTexterContact.jsx
+++ src/containers/AssignmentTexterContact.jsx
@@ -695,7 +695,7 @@ export class AssignmentTexterContact extends React.Component {
               />
               {this.renderNeedsResponseToggleButton(contact)}
               <RaisedButton
-                label='Canned responses'
+                label='Other responses'
                 onTouchTap={this.handleOpenPopover}
               />
               <RaisedButton
