ad32e2c5c9adda6cb704e0474fdd16df065c8875
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-11-22 23:52:42 -0500

    refactor to use tinycolor2

--- __test__/lib/color-contrast-helper.test.js
+++ __test__/lib/color-contrast-helper.test.js
@@ -1,119 +1,23 @@
-import {
-  parseRgbValues,
-  convertHexToRgb,
-  convertHslToRgb,
-  calcBrightness,
-  setContrastingColor
-} from "../../src/lib/color-contrast-helper";
+import { setContrastingColor } from "../../src/lib/color-contrast-helper";
 
 import theme from "../../src/styles/theme";
 
-describe("test parseRgbValues", () => {
-  it("handles rgba string correctly", () => {
-    expect(parseRgbValues("rgba(100, 100, 100, 1)")).toEqual({
-      r: 100,
-      g: 100,
-      b: 100,
-      alpha: 100
-    });
-  });
-
-  it("handles rgb string correctly", () => {
-    expect(parseRgbValues("rgb(100, 100, 100)")).toEqual({
-      r: 100,
-      g: 100,
-      b: 100,
-      alpha: 100
-    });
-  });
-});
-
-describe("test convertHexToRgb", () => {
-  it("handles 6-digit hex string correctly", () => {
-    expect(convertHexToRgb("#ffffff")).toEqual({ r: 255, g: 255, b: 255 });
-  });
-
-  it("handles 3-digit hex string correctly", () => {
-    expect(convertHexToRgb("#fff")).toEqual({ r: 255, g: 255, b: 255 });
-  });
-});
-
-describe("test convertHslToRgb", () => {
-  it("handles hsla string correctly", () => {
-    expect(convertHslToRgb("hsla(0, 0%, 100%, 1)")).toEqual({
-      r: 255,
-      g: 255,
-      b: 255,
-      alpha: 100
-    });
-  });
-
-  it("handles hsl string correctly", () => {
-    expect(convertHslToRgb("hsl(0, 0%, 100%)")).toEqual({
-      r: 255,
-      g: 255,
-      b: 255,
-      alpha: 100
-    });
-  });
-});
-
-describe("test calcBrightness", () => {
-  it("handles rgb string correctly", () => {
-    expect(calcBrightness("rgb(255, 255, 255)")).toEqual({
-      brightness: 255,
-      alpha: 100
-    });
-  });
-
-  it("handles rgba string correctly", () => {
-    expect(calcBrightness("rgb(255, 255, 255, 1)")).toEqual({
-      brightness: 255,
-      alpha: 100
-    });
-  });
-
-  it("handles hsl string correctly", () => {
-    expect(calcBrightness("hsla(0, 0%, 100%)")).toEqual({
-      brightness: 255,
-      alpha: 100
-    });
-  });
-
-  it("handles hsla string correctly", () => {
-    expect(calcBrightness("hsla(0, 0%, 100%, 1)")).toEqual({
-      brightness: 255,
-      alpha: 100
-    });
-  });
-
-  it("handles hex string correctly", () => {
-    expect(calcBrightness("#fff")).toEqual({
-      brightness: 255,
-      alpha: undefined
-    });
-  });
-
-  it("handles incorrectly formatted string correctly", () => {
-    expect(calcBrightness("incorrect")).toEqual({
-      brightness: undefined,
-      alpha: undefined
-    });
-  });
-});
-
 describe("test setContrastingColor", () => {
-  it("returns theme.colors.darkGray when a light primary color is passed in", () => {
+  it("returns darkGray when a light color is passed in", () => {
     expect(setContrastingColor("#fff")).toEqual(theme.colors.darkGray);
   });
 
-  it("returns theme.colors.white when a dark primary color is passed in", () => {
+  it("returns white when a dark color is passed in", () => {
     expect(setContrastingColor("#000")).toEqual(theme.colors.white);
   });
 
-  it("returns theme.colors.darkGray when a color is passed in with an opacity of less than .5", () => {
-    expect(setContrastingColor("rgba(0, 0, 0, .4)")).toEqual(
+  it("returns darkGray when a color is passed in with an opacity of less than .4", () => {
+    expect(setContrastingColor("rgba(0, 0, 0, .39)")).toEqual(
       theme.colors.darkGray
     );
+  });
+
+  it("returns darkGray when an invalid color is passed in", () => {
+    expect(setContrastingColor("incorrect")).toEqual(theme.colors.darkGray);
   });
 });
--- src/lib/color-contrast-helper.js
+++ src/lib/color-contrast-helper.js
@@ -1,116 +1,20 @@
 import theme from "../styles/theme";
 
-export const parseRgbValues = color => {
-  let rgbColorValuesArray = color
-    .slice(color.indexOf("(") + 1, color.indexOf(")"))
-    .split(",");
+const tinycolor = require("tinycolor2");
 
-  const r = parseInt(rgbColorValuesArray[0]);
-  const g = parseInt(rgbColorValuesArray[1]);
-  const b = parseInt(rgbColorValuesArray[2]);
-  const alpha = rgbColorValuesArray[3]
-    ? parseInt(rgbColorValuesArray[3] * 100)
-    : 100;
-  return { r, g, b, alpha };
-};
+export const setContrastingColor = color => {
+  const colorIsValid = tinycolor(color).isValid();
+  const brightness = tinycolor(color).getBrightness();
+  const alpha = tinycolor(color).getAlpha();
 
-export const convertHexToRgb = color => {
-  let hexcolor = color.replace("#", "");
-  if (hexcolor.length === 3) {
-    hexcolor = hexcolor.replace(/(.)/g, "$1$1");
-  }
-  const r = parseInt(hexcolor.substr(0, 2), 16);
-  const g = parseInt(hexcolor.substr(2, 2), 16);
-  const b = parseInt(hexcolor.substr(4, 2), 16);
-  return { r, g, b };
-};
+  const brightnessThreshhold = 160;
+  const alphaThreshhold = 0.4;
 
-export const convertHslToRgb = color => {
-  let hslColorValuesArray = color
-    .slice(color.indexOf("(") + 1, color.indexOf(")"))
-    .replace(/(%)/g, "")
-    .split(",");
-
-  const h = hslColorValuesArray[0];
-  const s = hslColorValuesArray[1] / 100;
-  const l = hslColorValuesArray[2] / 100;
-  const alpha = hslColorValuesArray[3]
-    ? parseInt(hslColorValuesArray[3] * 100)
-    : 100;
-
-  const val1 = (1 - Math.abs(2 * l - 1)) * s;
-  const val2 = val1 * (1 - Math.abs(((h / 60) % 2) - 1));
-  const val3 = l - val1 / 2;
-
-  let r, g, b;
-
-  if (h >= 0 && h < 60) {
-    r = val1;
-    g = val2;
-    b = 0;
-  } else if (h >= 60 && h < 120) {
-    r = val2;
-    g = val1;
-    b = 0;
-  } else if (h >= 120 && h < 180) {
-    r = 0;
-    g = val1;
-    b = val2;
-  } else if (h >= 180 && h < 240) {
-    r = 0;
-    g = val2;
-    b = val1;
-  } else if (h >= 240 && h < 300) {
-    r = val2;
-    g = 0;
-    b = val1;
-  } else if (h >= 300 && h < 360) {
-    r = val1;
-    g = 0;
-    b = val2;
-  }
-
-  r = Math.round((r + val3) * 255);
-  g = Math.round((g + val3) * 255);
-  b = Math.round((b + val3) * 255);
-  return { r, g, b, alpha };
-};
-
-export const calcBrightness = primaryColor => {
-  const W3CBrightnessFormula = (red, green, blue) =>
-    Math.floor((red * 299 + green * 587 + blue * 114) / 1000);
-
-  switch (primaryColor[0]) {
-    case "r": {
-      const { r, g, b, alpha } = parseRgbValues(primaryColor);
-      const brightness = W3CBrightnessFormula(r, g, b);
-      return { brightness, alpha };
-    }
-    case "#": {
-      const { r, g, b, alpha } = convertHexToRgb(primaryColor);
-      const brightness = W3CBrightnessFormula(r, g, b);
-      return { brightness, alpha };
-    }
-    case "h": {
-      const { r, g, b, alpha } = convertHslToRgb(primaryColor);
-      const brightness = W3CBrightnessFormula(r, g, b);
-      return { brightness, alpha };
-    }
-
-    default:
-      return { brightness: undefined, alpha: undefined };
-  }
-};
-
-export const setContrastingColor = primaryColor => {
-  const { brightness, alpha } = calcBrightness(primaryColor);
-  const threshhold = 160;
-  const color =
-    brightness > threshhold ||
-    (alpha && alpha < 50) ||
-    brightness === undefined ||
-    isNaN(brightness)
+  const contrastingColor =
+    brightness > brightnessThreshhold ||
+    alpha < alphaThreshhold ||
+    !colorIsValid
       ? theme.colors.darkGray
       : theme.colors.white;
-  return color;
+  return contrastingColor;
 };
