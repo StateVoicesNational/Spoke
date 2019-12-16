974f11835a0ee41f78aa8421224c75df40eb23c9
Larry Person <lp10011@gmail.com>
2019-12-16 17:22:55 -0500

    Added test for createOptOut, some minor refactoring and lint cleanup

--- __test__/containers/AssignmentTexterContact.test.js
+++ __test__/containers/AssignmentTexterContact.test.js
@@ -4,96 +4,131 @@
 import React from "react";
 import moment from "moment-timezone";
 import { mount } from "enzyme";
+import Store from "../../src/store";
+import { createMemoryHistory } from "react-router";
+import { ApolloProvider } from "react-apollo";
+import RaisedButton from "material-ui/RaisedButton";
+import ReactTestUtils from "react-dom/test-utils";
+
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { StyleSheetTestUtils } from "aphrodite";
-import { AssignmentTexterContact } from "../../src/containers/AssignmentTexterContact";
+import AssignmentTexterContact, {
+  InnerAssignmentTexterContact
+} from "../../src/containers/AssignmentTexterContact";
+import ApolloClientSingleton from "../../src/network/apollo-client-singleton";
 
-var MockDate = require("mockdate");
+import { r } from "../../src/server/models";
+
+import {
+  setupTest,
+  cleanupTest,
+  createUser,
+  createInvite,
+  createOrganization,
+  createCampaign,
+  createContacts,
+  createTexter,
+  assignTexter,
+  startCampaign,
+  getCampaignContact,
+  sendMessage,
+  getConversations
+} from "../test_helpers";
+
+const MockDate = require("mockdate");
 
 jest.mock("../../src/lib/timezones");
 jest.unmock("../../src/lib/tz-helpers");
-jest.useFakeTimers();
 
-var timezones = require("../../src/lib/timezones");
+const timezones = require("../../src/lib/timezones");
 
-const campaign = {
-  id: 9,
-  isArchived: false,
-  useDynamicAssignment: null,
-  organization: {
-    id: 2,
-    textingHoursEnforced: true,
-    textingHoursStart: 8,
-    textingHoursEnd: 21,
-    threeClickEnabled: false
-  },
-  customFields: [],
-  interactionSteps: [
-    {
-      id: 11,
-      question: {
-        text: "",
-        answerOptions: []
+const getProps = (messageStatus = "needsMessage") => {
+  const campaign = {
+    id: 9,
+    isArchived: false,
+    useDynamicAssignment: null,
+    organization: {
+      id: 2,
+      textingHoursEnforced: true,
+      textingHoursStart: 8,
+      textingHoursEnd: 21,
+      threeClickEnabled: false
+    },
+    customFields: [],
+    interactionSteps: [
+      {
+        id: 11,
+        question: {
+          text: "",
+          answerOptions: []
+        }
+      }
+    ]
+  };
+
+  return {
+    ...{
+      texter: {
+        id: 2,
+        firstName: "larry",
+        lastName: "person",
+        assignedCell: null
+      },
+      campaign,
+      assignment: {
+        id: 9,
+        userCannedResponses: [],
+        campaignCannedResponses: [],
+        texter: {
+          id: 2,
+          firstName: "larry",
+          lastName: "person",
+          assignedCell: null
+        },
+        campaign,
+        contacts: [
+          {
+            id: 19
+          },
+          {
+            id: 20
+          }
+        ],
+        allContactsCount: 2
+      },
+      refreshData: jest.fn(),
+      contact: {
+        id: 19,
+        assignmentId: 9,
+        firstName: "larry",
+        lastName: "person",
+        cell: "+19734779697",
+        zip: "10025",
+        customFields: "{}",
+        optOut: null,
+        currentInteractionStepScript: "{firstName}",
+        questionResponseValues: [],
+        location: {
+          city: "New York",
+          state: "NY",
+          timezone: {
+            offset: -5,
+            hasDST: true
+          }
+        },
+        messageStatus,
+        messages: []
       }
     }
-  ]
-};
-
-const propsWithEnforcedTextingHoursCampaign = {
-  texter: {
-    id: 2,
-    firstName: "larry",
-    lastName: "person",
-    assignedCell: null
-  },
-  campaign: campaign,
-  assignment: {
-    id: 9,
-    userCannedResponses: [],
-    campaignCannedResponses: [],
-    texter: {
-      id: 2,
-      firstName: "larry",
-      lastName: "person",
-      assignedCell: null
-    },
-    campaign: campaign,
-    contacts: [
-      {
-        id: 19
-      },
-      {
-        id: 20
-      }
-    ],
-    allContactsCount: 2
-  },
-  refreshData: jest.fn(),
-  contact: {
-    id: 19,
-    assignmentId: 9,
-    firstName: "larry",
-    lastName: "person",
-    cell: "+19734779697",
-    zip: "10025",
-    customFields: "{}",
-    optOut: null,
-    currentInteractionStepScript: "{firstName}",
-    questionResponseValues: [],
-    location: {
-      city: "New York",
-      state: "NY",
-      timezone: {
-        offset: -5,
-        hasDST: true
-      }
-    },
-    messageStatus: "needsMessage",
-    messages: []
-  }
+  };
 };
 
 describe("when contact is not within texting hours...", () => {
+  let propsWithEnforcedTextingHoursCampaign;
+  beforeEach(() => {
+    jest.useFakeTimers();
+    propsWithEnforcedTextingHoursCampaign = getProps();
+  });
   afterEach(() => {
     propsWithEnforcedTextingHoursCampaign.refreshData.mockReset();
   });
@@ -106,11 +141,11 @@ describe("when contact is not within texting hours...", () => {
         .utcOffset(-5)
     );
     StyleSheetTestUtils.suppressStyleInjection();
-    let component = mount(
+    mount(
       <MuiThemeProvider>
-        <AssignmentTexterContact
+        <InnerAssignmentTexterContact
           texter={propsWithEnforcedTextingHoursCampaign.texter}
-          campaign={campaign}
+          campaign={propsWithEnforcedTextingHoursCampaign.campaign}
           assignment={propsWithEnforcedTextingHoursCampaign.assignment}
           refreshData={propsWithEnforcedTextingHoursCampaign.refreshData}
           contact={propsWithEnforcedTextingHoursCampaign.contact}
@@ -125,7 +160,11 @@ describe("when contact is not within texting hours...", () => {
 });
 
 describe("when contact is within texting hours...", () => {
-  var component;
+  let propsWithEnforcedTextingHoursCampaign;
+  beforeEach(() => {
+    jest.useFakeTimers();
+    propsWithEnforcedTextingHoursCampaign = getProps();
+  });
   beforeEach(() => {
     timezones.isBetweenTextingHours.mockReturnValue(true);
     timezones.getLocalTime.mockReturnValue(
@@ -134,11 +173,11 @@ describe("when contact is within texting hours...", () => {
         .utcOffset(-5)
     );
     StyleSheetTestUtils.suppressStyleInjection();
-    component = mount(
+    mount(
       <MuiThemeProvider>
-        <AssignmentTexterContact
+        <InnerAssignmentTexterContact
           texter={propsWithEnforcedTextingHoursCampaign.texter}
-          campaign={campaign}
+          campaign={propsWithEnforcedTextingHoursCampaign.campaign}
           assignment={propsWithEnforcedTextingHoursCampaign.assignment}
           refreshData={propsWithEnforcedTextingHoursCampaign.refreshData}
           contact={propsWithEnforcedTextingHoursCampaign.contact}
@@ -157,10 +196,137 @@ describe("when contact is within texting hours...", () => {
   });
 });
 
+describe("when the texter clicks the opt-out button", () => {
+  let component;
+  let organizationId;
+  let optOutContact;
+  let assignmentId;
+
+  beforeAll(async () => {
+    await setupTest();
+    const testAdminUser = await createUser();
+    const testInvite = await createInvite();
+    const testOrganization = await createOrganization(
+      testAdminUser,
+      testInvite
+    );
+    const testCampaign = await createCampaign(testAdminUser, testOrganization);
+    const testContacts = await createContacts(testCampaign, 1);
+    const testTexterUser = await createTexter(testOrganization);
+    await startCampaign(testAdminUser, testCampaign);
+
+    const testAssignment = (await assignTexter(
+      testAdminUser,
+      testTexterUser,
+      testCampaign
+    )).data.editCampaign;
+
+    const dbCampaignContact = await getCampaignContact(testContacts[0].id);
+    assignmentId = dbCampaignContact.assignment_id;
+    organizationId = testOrganization.data.createOrganization.id;
+
+    optOutContact = testContacts[0];
+
+    optOutContact.questionResponseValues = [];
+    optOutContact.messages = [];
+    testAssignment.userCannedResponses = [];
+    testAssignment.campaignCannedResponses = [];
+    testCampaign.interactionSteps = [
+      {
+        id: 11,
+        question: {
+          text: "",
+          answerOptions: []
+        }
+      }
+    ];
+    testCampaign.organization = {
+      optOutMessage: "don't let the door hit you on the way out"
+    };
+
+    const message = {
+      assignmentId,
+      contactNumber: optOutContact.cell,
+      text: "hey now"
+    };
+
+    await sendMessage(optOutContact.id, testTexterUser, message);
+
+    StyleSheetTestUtils.suppressStyleInjection();
+    const store = new Store(createMemoryHistory("/")).data;
+
+    const root = document.createElement("div");
+    document.body.appendChild(root);
+
+    // the destructuring below is a hack to avoid "hasOwnProperty is not a function" errors
+    component = mount(
+      <ApolloProvider store={store} client={ApolloClientSingleton}>
+        <MuiThemeProvider>
+          <AssignmentTexterContact
+            organizationId={organizationId}
+            texter={{ ...testTexterUser }}
+            campaign={{ ...testCampaign }}
+            assignment={{ ...testAssignment }}
+            refreshData={() => {}}
+            contact={{ ...testContacts[0] }}
+            onFinishContact={() => {}}
+          />
+        </MuiThemeProvider>
+      </ApolloProvider>,
+      {
+        attachTo: root
+      }
+    );
+  });
+
+  afterAll(async () => {
+    await cleanupTest();
+    if (r.redis) r.redis.flushdb();
+  });
+
+  it("calls the createOptOut mutation", async () => {
+    const wrappedComponent = component.find(InnerAssignmentTexterContact);
+    const wrappedComponentState = wrappedComponent.instance().state;
+    const updatedComponent = wrappedComponent.setState({
+      ...wrappedComponentState,
+      optOutDialogOpen: true,
+      disabled: false
+    });
+
+    const createOptOutMock = jest.fn((_optOut, _campaignContactId) =>
+      Promise.resolve({ campaignContactId: _campaignContactId })
+    );
+
+    updatedComponent.instance().props.mutations.createOptOut = createOptOutMock;
+    updatedComponent.instance().handleSubmitSurveys = () => {};
+    updatedComponent.instance().props.mutations.sendMessage = () => {};
+
+    await updatedComponent.instance().handleOptOut();
+
+    const optOut = {
+      cell: optOutContact.cell,
+      assignmentId: assignmentId.toString()
+    };
+
+    expect(createOptOutMock).toBeCalledTimes(1);
+    expect(createOptOutMock).toBeCalledWith(
+      organizationId,
+      optOut,
+      optOutContact.id
+    );
+  });
+});
+
 describe("AssignmentTextContact has the proper enabled/disabled state when created", () => {
+  let propsWithEnforcedTextingHoursCampaign;
+  beforeEach(() => {
+    jest.useFakeTimers();
+    propsWithEnforcedTextingHoursCampaign = getProps();
+  });
+
   it("is enabled if the contact is inside texting hours", () => {
     timezones.isBetweenTextingHours.mockReturnValueOnce(true);
-    var assignmentTexterContact = new AssignmentTexterContact(
+    const assignmentTexterContact = new InnerAssignmentTexterContact(
       propsWithEnforcedTextingHoursCampaign
     );
     expect(assignmentTexterContact.state.disabled).toBeFalsy();
@@ -169,7 +335,7 @@ describe("AssignmentTextContact has the proper enabled/disabled state when creat
 
   it("is disabled if the contact is inside texting hours", () => {
     timezones.isBetweenTextingHours.mockReturnValueOnce(false);
-    var assignmentTexterContact = new AssignmentTexterContact(
+    const assignmentTexterContact = new InnerAssignmentTexterContact(
       propsWithEnforcedTextingHoursCampaign
     );
     expect(assignmentTexterContact.state.disabled).toBeTruthy();
@@ -180,13 +346,16 @@ describe("AssignmentTextContact has the proper enabled/disabled state when creat
 });
 
 describe("test isContactBetweenTextingHours", () => {
-  var assignmentTexterContact;
+  let assignmentTexterContact;
+  let propsWithEnforcedTextingHoursCampaign;
 
   beforeAll(() => {
-    assignmentTexterContact = new AssignmentTexterContact(
+    jest.useFakeTimers();
+    propsWithEnforcedTextingHoursCampaign = getProps();
+    assignmentTexterContact = new InnerAssignmentTexterContact(
       propsWithEnforcedTextingHoursCampaign
     );
-    timezones.isBetweenTextingHours.mockImplementation((o, c) => false);
+    timezones.isBetweenTextingHours.mockImplementation(() => false);
     MockDate.set("2018-02-01T15:00:00.000Z");
     timezones.getLocalTime.mockReturnValue(
       moment()
@@ -204,7 +373,7 @@ describe("test isContactBetweenTextingHours", () => {
   });
 
   it("works when the contact has location data with empty timezone", () => {
-    let contact = {
+    const contact = {
       location: {
         city: "New York",
         state: "NY",
@@ -220,7 +389,7 @@ describe("test isContactBetweenTextingHours", () => {
     ).toBeFalsy();
     expect(timezones.isBetweenTextingHours.mock.calls).toHaveLength(1);
 
-    let theCall = timezones.isBetweenTextingHours.mock.calls[0];
+    const theCall = timezones.isBetweenTextingHours.mock.calls[0];
     expect(theCall[0]).toBeFalsy();
     expect(theCall[1]).toEqual({
       textingHoursStart: 8,
@@ -230,7 +399,7 @@ describe("test isContactBetweenTextingHours", () => {
   });
 
   it("works when the contact has location data", () => {
-    let contact = {
+    const contact = {
       location: {
         city: "New York",
         state: "NY",
@@ -246,7 +415,7 @@ describe("test isContactBetweenTextingHours", () => {
     ).toBeFalsy();
     expect(timezones.isBetweenTextingHours.mock.calls).toHaveLength(1);
 
-    let theCall = timezones.isBetweenTextingHours.mock.calls[0];
+    const theCall = timezones.isBetweenTextingHours.mock.calls[0];
     expect(theCall[0]).toEqual({ hasDST: true, offset: -5 });
     expect(theCall[1]).toEqual({
       textingHoursStart: 8,
@@ -256,14 +425,14 @@ describe("test isContactBetweenTextingHours", () => {
   });
 
   it("works when the contact does not have location data", () => {
-    let contact = {};
+    const contact = {};
 
     expect(
       assignmentTexterContact.isContactBetweenTextingHours(contact)
     ).toBeFalsy();
     expect(timezones.isBetweenTextingHours.mock.calls).toHaveLength(1);
 
-    let theCall = timezones.isBetweenTextingHours.mock.calls[0];
+    const theCall = timezones.isBetweenTextingHours.mock.calls[0];
     expect(theCall[0]).toBeNull();
     expect(theCall[1]).toEqual({
       textingHoursStart: 8,
