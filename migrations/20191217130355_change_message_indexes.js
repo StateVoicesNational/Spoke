96024bab62da81a672af9e107c20076a574843af
Schuyler Duveen <sky@skyb.us>
2019-12-24 12:32:50 -0500

    simplify migration -- concurrent will have to be manual

--- migrations/20191217130355_change_message_indexes.js
+++ migrations/20191217130355_change_message_indexes.js
@@ -1,50 +1,20 @@
-exports.config = {
-  // in order for the raw CONCURRENTLY commands, we need to disable transasctions for this migration
-  transaction: false
-};
-
 exports.up = function(knex, Promise) {
   const isPostgres = knex.client.config.client === "pg";
-  return knex.schema
-    .alterTable("message", table => {
-      // NOTE: this could be an expensive migration which locks tables for some time, if you have millions of message rows
-      // In that case, we recommend performing this migration manually during planned downtime
+  return knex.schema.alterTable("message", table => {
+    // NOTE: this could be an expensive migration which locks tables for some time, if you have millions of message rows
+    // In that case, we recommend performing this migration manually during planned downtime
 
-      // 1.5: backfill messageservice_sid for running campaigns
-      table.dropIndex("contact_number"); // Will be recreated with messageservice_sid
-    })
-    .then(() => {
-      if (isPostgres) {
-        return knex.schema.raw(
-          `CREATE INDEX CONCURRENTLY cell_messageservice_sid_idx ON message (contact_number, messageservice_sid)`
-        );
-      } else {
-        return knex.schema.alterTable("message", table => {
-          table.index(
-            ["contact_number", "messageservice_sid"],
-            "cell_messageservice_sid_idx"
-          );
-        });
-      }
-
-      // 2. create index concurrently (if postgres)
-      //table.index("campaign_contact_id");
-      // 3. drop Index on assignment_id
-    })
-    .then(() => {
-      if (isPostgres) {
-        return knex.schema.raw(
-          `CREATE INDEX CONCURRENTLY message_campaign_contact_id_index ON message (campaign_contact_id)`
-        );
-      } else {
-        return knex.schema.alterTable("message", table => {
-          table.index(
-            "campaign_contact_id",
-            "message_campaign_contact_id_index"
-          );
-        });
-      }
-    });
+    table.dropIndex("contact_number"); // Will be recreated with messageservice_sid
+    // For Postgres, consider concurrent creation with manual command:
+    // CREATE INDEX CONCURRENTLY cell_messageservice_sid_idx ON message (contact_number, messageservice_sid)
+    table.index(
+      ["contact_number", "messageservice_sid"],
+      "cell_messageservice_sid_idx"
+    );
+    // For Postgres, consider concurrent creation with manual command:
+    // CREATE INDEX CONCURRENTLY message_campaign_contact_id_index ON message (campaign_contact_id)
+    table.index("campaign_contact_id", "message_campaign_contact_id_index");
+  });
 };
 
 exports.down = function(knex, Promise) {
