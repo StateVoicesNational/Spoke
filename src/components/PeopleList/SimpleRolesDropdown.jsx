e39d57ed6ed0673d20356de976b8d9cefa4394ae
Larry Person <lp10011@gmail.com>
2019-12-28 19:58:25 -0500

    Manual merge WFP PR 105

---
+++ src/components/PeopleList/SimpleRolesDropdown.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import type from "prop-types";
+
+import DropDownMenu from "material-ui/DropDownMenu";
+import MenuItem from "material-ui/MenuItem";
+import { ROLE_HIERARCHY } from "../../lib";
+
+export const ALL_ROLES = "ALL ROLES";
+
+const SimpleRolesDropdown = props => (
+  <DropDownMenu
+    value={props.selectedRole}
+    onChange={(event, index, value) => props.onChange(value)}
+  >
+    {[ALL_ROLES].concat(ROLE_HIERARCHY).map(option => (
+      <MenuItem
+        key={option}
+        value={option}
+        primaryText={`${option.charAt(0).toUpperCase()}${option
+          .substring(1)
+          .toLowerCase()}`}
+      />
+    ))}
+  </DropDownMenu>
+);
+
+SimpleRolesDropdown.propTypes = {
+  selectedRole: type.object,
+  onChange: type.func
+};
+
+export default SimpleRolesDropdown;
