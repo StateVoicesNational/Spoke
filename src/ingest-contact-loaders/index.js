6498dac1536df4433923564ff51cfc9297c5282f
Schuyler Duveen <sky@skyb.us>
2020-01-15 22:01:54 -0500

    fix ingestMethod selection

--- src/ingest-contact-loaders/index.js
+++ src/ingest-contact-loaders/index.js
@@ -2,25 +2,26 @@ import { getConfig } from "../server/api/lib/config";
 import { r } from "../server/models";
 
 const availabilityCacheKey = (name, organizationId, userId) =>
-      `${process.env.CACHE_PREFIX || ""}ingestavail-${name}-${organizationId}-${userId}`;
-const choiceDataCacheKey = (suffix) => `${process.env.CACHE_PREFIX || ""}ingestchoices-${suffix}`;
-
+  `${process.env.CACHE_PREFIX ||
+    ""}ingestavail-${name}-${organizationId}-${userId}`;
+const choiceDataCacheKey = suffix =>
+  `${process.env.CACHE_PREFIX || ""}ingestchoices-${suffix}`;
 
 function getIngestMethods() {
-  // TODO: default will be csv-upload for real version
-  const enabledIngestMethods = (getConfig("CONTACT_LOADERS") || "test-fakedata").split(",");
-  const ingestMethods = {}
+  const enabledIngestMethods = (
+    getConfig("CONTACT_LOADERS") || "csv-upload,datawarehouse"
+  ).split(",");
+  const ingestMethods = {};
   enabledIngestMethods.forEach(name => {
     try {
       const c = require(`./${name}/index.js`);
       ingestMethods[name] = c;
-    } catch(err) {
-      console.error(
-        "CONTACT_LOADERS failed to load ingestMethod", name
-      );
+      // console.log('LOADING INGEST METHOD', name, c);
+    } catch (err) {
+      console.error("CONTACT_LOADERS failed to load ingestMethod", name);
     }
   });
-  return ingestMethods
+  return ingestMethods;
 }
 
 const CONFIGURED_INGEST_METHODS = getIngestMethods();
@@ -40,9 +41,10 @@ async function getSetCacheableResult(cacheKey, fallbackFunc) {
 }
 
 async function getIngestAvailability(name, ingestMethod, organization, user) {
-  return await getSetCacheableResult(
+  return (await getSetCacheableResult(
     availabilityCacheKey(name, organization.id, user.id),
-    async () => ingestMethod.available(organization, user));
+    async () => ingestMethod.available(organization, user)
+  )).result;
 }
 
 export async function getIngestMethod(name, organization, user) {
@@ -51,23 +53,38 @@ export async function getIngestMethod(name, organization, user) {
       name,
       CONFIGURED_INGEST_METHODS[name],
       organization,
-      user);
-    if (isAvail.result) {
+      user
+    );
+    // console.log('getIngestMethod', name, organization, user, 'isavail', isAvail);
+    if (isAvail) {
       return CONFIGURED_INGEST_METHODS[name];
     }
   }
 }
 
 export async function getAvailableIngestMethods(organization, user) {
-  return Promise.all(
-    Object.keys(CONFIGURED_INGEST_METHODS)
-      .map(name => getIngestMethod(name, organization, user)));
+  const ingestMethods = await Promise.all(
+    Object.keys(CONFIGURED_INGEST_METHODS).map(name =>
+      getIngestMethod(name, organization, user)
+    )
+  );
+  // console.log('availableIngestMethods', ingestMethods);
+  return ingestMethods.filter(x => x);
 }
 
-export async function getMethodChoiceData(ingestMethod, organization, campaign, user, loaders) {
-  const cacheFunc = ingestMethod.clientChoiceDataCacheKey || ((org) => `${org.id}`);
-  const cacheKey = choiceDataCacheKey(cacheFunc(organization, campaign, user, loaders));
-  return (await getSetCacheableResult(cacheKey, async () => (
+export async function getMethodChoiceData(
+  ingestMethod,
+  organization,
+  campaign,
+  user,
+  loaders
+) {
+  const cacheFunc =
+    ingestMethod.clientChoiceDataCacheKey || (org => `${org.id}`);
+  const cacheKey = choiceDataCacheKey(
+    cacheFunc(organization, campaign, user, loaders)
+  );
+  return (await getSetCacheableResult(cacheKey, async () =>
     ingestMethod.getClientChoiceData(organization, campaign, user, loaders)
-  ))).data;
+  )).data;
 }
