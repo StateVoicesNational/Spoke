cbbc5acf55cb26b773d1ff8b585f6fb0b6c94469
Ilona Brand <theilonabrand@gmail.com>
2020-01-28 15:47:02 -0500

    foundation for actual test

--- __test__/containers/UserMenu.test.js
+++ __test__/containers/UserMenu.test.js
@@ -2,61 +2,49 @@
  * @jest-environment jsdom
  */
 import React from "react";
-import { shallow, mount } from "enzyme";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import Store from "../../src/store";
-import { createMemoryHistory } from "react-router";
-import ApolloClientSingleton from "../../src/network/apollo-client-singleton";
+import { mount } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+
 import { ApolloProvider } from "react-apollo";
-import UserMenu from "../../src/containers/UserMenu";
-import { r } from "../../src/server/models/";
-import { createUser, setupTest, cleanupTest, runGql } from "../test_helpers";
-import { assignmentId, testTexterUser } from "../server/texter.test/common";
-import gql from "graphql-tag";
+import { UserMenu } from "../../src/containers/UserMenu";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import Avatar from "material-ui/Avatar";
 
 describe("UserMenu", () => {
-  let store;
-  let testUser;
-
-  beforeEach(async () => {
-    await setupTest();
-    testUser = await createUser();
-  }, global.DATABASE_SETUP_TEARDOWN_TIMEOUT);
-
-  store = new Store(createMemoryHistory("/")).data;
   it("becomes active when the user icon is clicked", async () => {
-    const getCurrentUserForMenuQuery = gql`
-      query getCurrentUserForMenu {
-        currentUser {
-          id
-          displayName
-          email
-          organizations {
-            id
-            name
+    StyleSheetTestUtils.suppressStyleInjection();
+
+    const data = {
+      currentUser: {
+        id: 1,
+        displayName: "Ilona",
+        email: "test@test.com",
+        organizations: [
+          {
+            id: 2,
+            name: "testOrg"
           }
-        }
+        ]
       }
-    `;
-
-    const userResult = await runGql(getCurrentUserForMenuQuery, {}, testUser);
-
-    console.log("userResult", userResult);
+    };
 
     const wrapper = mount(
-      <ApolloProvider store={store} client={ApolloClientSingleton}>
-        <MuiThemeProvider>
-          <UserMenu />
-        </MuiThemeProvider>
-      </ApolloProvider>
+      <MuiThemeProvider>
+        <UserMenu data={data} />
+      </MuiThemeProvider>
+    ).find(UserMenu);
+    const component = wrapper.instance();
+    const avatar = wrapper.find("Avatar");
+    console.log(
+      "AVATAR ",
+      avatar,
+      component,
+      "XXX ",
+      avatar.props().children,
+      " XXX"
     );
-    // const userMenuButton = document.querySelector(
-    //   "[data-test=userMenuButton]"
-    // );
+    const nameMenuItem = wrapper.find("MenuItem");
+    console.log("MENU ITEM ", nameMenuItem);
   });
-
-  afterEach(async () => {
-    await cleanupTest();
-    if (r.redis) r.redis.flushdb();
-  }, global.DATABASE_SETUP_TEARDOWN_TIMEOUT);
 });
