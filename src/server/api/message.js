aa8e56816f8592b634bf03e9421c54146a458667
Schuyler Duveen <sky@skyb.us>
2020-02-10 12:25:00 -0500

    msg.id not in message cache

--- src/server/api/message.js
+++ src/server/api/message.js
@@ -4,15 +4,10 @@ import { Message } from "../models";
 export const resolvers = {
   Message: {
     ...mapFieldsToModel(
-      [
-        "id",
-        "text",
-        "userNumber",
-        "contactNumber",
-        "createdAt",
-        "isFromContact"
-      ],
+      ["text", "userNumber", "contactNumber", "createdAt", "isFromContact"],
       Message
-    )
+    ),
+    // cached messages don't have message.id -- why bother
+    id: msg => msg.id || `fake${Math.random()}`
   }
 };
