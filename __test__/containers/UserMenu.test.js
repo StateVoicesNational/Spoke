dd509a14e385f130093f30a92daa37c7ede790f6
Ilona Brand <theilonabrand@gmail.com>
2020-02-12 13:46:17 -0500

    a couple functioning test cases

--- __test__/containers/UserMenu.test.js
+++ __test__/containers/UserMenu.test.js
@@ -5,20 +5,18 @@ import React from "react";
 import { mount } from "enzyme";
 import { StyleSheetTestUtils } from "aphrodite";
 
-import { ApolloProvider } from "react-apollo";
 import { UserMenu } from "../../src/containers/UserMenu";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-
-import Avatar from "material-ui/Avatar";
+import Menu from "material-ui/Menu";
 
 describe("UserMenu", () => {
-  it("becomes active when the user icon is clicked", async () => {
+  it("renders the correct user Avatar icon", async () => {
     StyleSheetTestUtils.suppressStyleInjection();
 
     const data = {
       currentUser: {
         id: 1,
-        displayName: "Ilona",
+        displayName: "TestName",
         email: "test@test.com",
         organizations: [
           {
@@ -34,17 +32,47 @@ describe("UserMenu", () => {
         <UserMenu data={data} />
       </MuiThemeProvider>
     ).find(UserMenu);
-    const component = wrapper.instance();
+
     const avatar = wrapper.find("Avatar");
-    console.log(
-      "AVATAR ",
-      avatar,
-      component,
-      "XXX ",
-      avatar.props().children,
-      " XXX"
+    expect(avatar.props().children).toBe(
+      data.currentUser.displayName.charAt(0)
     );
-    const nameMenuItem = wrapper.find("MenuItem");
-    console.log("MENU ITEM ", nameMenuItem);
+  });
+
+  it("renders the full popover menu", async () => {
+    StyleSheetTestUtils.suppressStyleInjection();
+
+    const data = {
+      currentUser: {
+        id: 1,
+        displayName: "TestName",
+        email: "test@test.com",
+        organizations: [
+          {
+            id: 2,
+            name: "testOrg"
+          }
+        ]
+      }
+    };
+
+    const wrapper = mount(
+      <MuiThemeProvider>
+        <UserMenu data={data} />
+      </MuiThemeProvider>
+    ).find(UserMenu);
+
+    // const nameMenuItem = wrapper.find("MenuItem");
+    // The popover loads as an object
+    const menuPopover = wrapper.find("Popover");
+    expect(menuPopover.length).toBeGreaterThan(0);
+
+    const menuItemArray = menuPopover.props().children.props.children;
+
+    // First MenuItem should always be the user's display name
+    expect(menuItemArray[0].props["data-test"]).toBe("userMenuDisplayName");
+
+    // Second MenuItem will be a divider
+    console.log("MENU ITEM ", menuItemArray[1]);
   });
 });
