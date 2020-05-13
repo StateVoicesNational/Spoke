9f2c765f5d07459b621d984af9d6b3daf0b9a155
Larry Person <lp10011@gmail.com>
2020-05-13 09:39:03 -0400

    Refactored updateQuestionResponses; work on tests

--- __test__/server/api/campaign/updateQuestionResponses.test.js
+++ __test__/server/api/campaign/updateQuestionResponses.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import {
   cleanupTest,
   createScript,
@@ -8,7 +11,20 @@ import {
   sleep
 } from "../../../test_helpers";
 
-import { r } from "../../../../src/server/models";
+const UpdateQuestionResponses = require("../../../../src/server/api/mutations/updateQuestionResponses");
+
+import { r, loaders } from "../../../../src/server/models";
+
+// for testing with AssignmentTexterContact
+import React from "react";
+import { mount } from "enzyme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { StyleSheetTestUtils } from "aphrodite";
+import { AssignmentTexterContact } from "../../../../src/containers/AssignmentTexterContact";
+import {
+  dataQueryString as assignmentQueryString,
+  contactDataFragment
+} from "../../../../src/containers/TexterTodo";
 
 describe("mutations.updateQuestionResponses", () => {
   let adminUser;
@@ -21,6 +37,8 @@ describe("mutations.updateQuestionResponses", () => {
   let colorInteractionSteps;
   let redInteractionStep;
   let shadesOfRedInteractionSteps;
+  let questionResponses;
+  let questionResponseValuesDatabaseSql;
 
   beforeEach(async () => {
     await setupTest();
@@ -32,6 +50,30 @@ describe("mutations.updateQuestionResponses", () => {
   }, global.DATABASE_SETUP_TEARDOWN_TIMEOUT);
 
   beforeEach(async () => {
+    jest.spyOn(UpdateQuestionResponses, "updateQuestionResponses");
+
+    questionResponseValuesDatabaseSql = `
+      SELECT
+        child.answer_option,
+        child.id as child_id,
+        child.parent_interaction_id,
+        interaction_step.campaign_id,
+        interaction_step.question,
+        interaction_step.script,
+        child.answer_actions,
+        question_response.value
+      FROM
+        question_response
+      JOIN
+        interaction_step ON interaction_step.id = question_response.interaction_step_id
+      JOIN
+        interaction_step as child ON child.parent_interaction_id = interaction_step.id and question_response.value = child.answer_option
+      WHERE
+        question_response.campaign_contact_id = ?
+      ORDER BY
+        question_response.id;
+      `;
+
     const startedCampaign = await createStartedCampaign();
 
     ({
@@ -133,9 +175,7 @@ describe("mutations.updateQuestionResponses", () => {
       interactionStep =>
         interactionStep.parentInteractionId === redInteractionStep.id
     );
-  });
 
-  it("records answers for a contact", async () => {
     const promises = contacts.slice(0, 2).map(contact => {
       return sendMessage(contact.id, texterUser, {
         text: returnedInteractionSteps[0].script,
@@ -145,11 +185,27 @@ describe("mutations.updateQuestionResponses", () => {
       });
     });
 
-    const sendResults = await Promise.all(promises);
+    await Promise.all(promises);
+  });
 
-    expect(sendResults).toHaveLength(2);
-    expect(sendResults[0].data.sendMessage.messageStatus).toEqual("messaged");
-    expect(sendResults[1].data.sendMessage.messageStatus).toEqual("messaged");
+  it("records answers for a contact", async () => {
+    // verify that contacts have messageStatus === 'messaged'
+    const results = await r
+      .knex("campaign_contact")
+      .select("message_status")
+      .whereIn(
+        "id",
+        contacts.slice(0, 2).map(contact => contact.id)
+      );
+
+    expect(results).toEqual([
+      {
+        message_status: "messaged"
+      },
+      {
+        message_status: "messaged"
+      }
+    ]);
 
     const updateQuestionResponseGql = `
       mutation updateQuestionResponses($qr: [QuestionResponseInput], $ccid: String!) {
@@ -164,20 +220,22 @@ describe("mutations.updateQuestionResponses", () => {
       }
     `;
 
+    questionResponses = [
+      {
+        campaignContactId: contacts[0].id,
+        interactionStepId: returnedInteractionSteps[0].id,
+        value: colorInteractionSteps[0].answerOption
+      },
+      {
+        campaignContactId: contacts[0].id.toString(),
+        interactionStepId: redInteractionStep.id,
+        value: shadesOfRedInteractionSteps[0].answerOption
+      }
+    ];
+
     const variables = {
       ccid: contacts[0].id,
-      qr: [
-        {
-          campaignContactId: contacts[0].id,
-          interactionStepId: returnedInteractionSteps[0].id,
-          value: colorInteractionSteps[0].answerOption
-        },
-        {
-          campaignContactId: contacts[0].id.toString(),
-          interactionStepId: redInteractionStep.id,
-          value: shadesOfRedInteractionSteps[0].answerOption
-        }
-      ]
+      qr: questionResponses
     };
 
     // this returns immediately before doing any updates
@@ -186,6 +244,26 @@ describe("mutations.updateQuestionResponses", () => {
       variables,
       texterUser
     );
+
+    expect(UpdateQuestionResponses.updateQuestionResponses.mock.calls).toEqual([
+      [
+        [
+          {
+            campaignContactId: contacts[0].id.toString(),
+            interactionStepId: returnedInteractionSteps[0].id.toString(),
+            value: "Red"
+          },
+          {
+            campaignContactId: contacts[0].id.toString(),
+            interactionStepId: "2",
+            value: "Crimson"
+          }
+        ],
+        contacts[0].id.toString(),
+        loaders,
+        texterUser
+      ]
+    ]);
 
     expect(updateQuestionResponseResult.data.updateQuestionResponses).toEqual({
       id: contacts[0].id.toString(),
@@ -197,31 +275,9 @@ describe("mutations.updateQuestionResponses", () => {
     // the sleep gives it a chance to finish before we start expecting outcomes
     await sleep(1000);
 
-    const databaseQuery = `
-      SELECT
-        child.answer_option,
-        child.id as child_id,
-        child.parent_interaction_id,
-        interaction_step.campaign_id,
-        interaction_step.question,
-        interaction_step.script,
-        child.answer_actions,
-        question_response.value
-      FROM
-        question_response
-      JOIN
-        interaction_step ON interaction_step.id = question_response.interaction_step_id
-      JOIN
-        interaction_step as child ON child.parent_interaction_id = interaction_step.id and question_response.value = child.answer_option
-      WHERE
-        question_response.campaign_contact_id = ?
-      ORDER BY
-        question_response.id;
-      `;
-
-    const { rows: databaseQueryResults } = await r.knex.raw(databaseQuery, [
-      contacts[0].id
-    ]);
+    const {
+      rows: databaseQueryResults
+    } = await r.knex.raw(questionResponseValuesDatabaseSql, [contacts[0].id]);
 
     expect(databaseQueryResults).toEqual([
       {
@@ -245,5 +301,85 @@ describe("mutations.updateQuestionResponses", () => {
         value: "Crimson"
       }
     ]);
+  });
+
+  describe("when updated from the UI", () => {
+    let updatedCampaign;
+    let updatedContacts;
+    let updatedAssignment;
+
+    beforeEach(async () => {
+      const variables = {
+        contactsFilter: {
+          messageStatus: "messaged"
+        },
+        assignmentId: assignment.id.toString()
+      };
+
+      const retrievedAssignment = await runGql(
+        assignmentQueryString,
+        variables,
+        adminUser
+      );
+
+      ({
+        data: { assignment: updatedAssignment }
+      } = retrievedAssignment);
+
+      updatedCampaign = updatedAssignment.campaign;
+
+      const getAssignmentContactsGql = `
+        mutation getAssignmentContacts($assignmentId: String!, $contactIds: [String]!, $findNew: Boolean) {
+          getAssignmentContacts(assignmentId: $assignmentId, contactIds: $contactIds, findNew: $findNew) {
+            ${contactDataFragment}
+          }
+        }
+      `;
+
+      const getAssignmentVariables = {
+        assignmentId: assignment.id,
+        contactIds: contacts[0].id,
+        findNew: false
+      };
+
+      const retrievedContacts = await runGql(
+        getAssignmentContactsGql,
+        getAssignmentVariables,
+        adminUser
+      );
+
+      ({
+        data: { getAssignmentContacts: updatedContacts }
+      } = retrievedContacts);
+    });
+
+    it("calls the resolver", async () => {
+      const navigationToolbarChildren = {
+        onNext: jest.fn(),
+        onPrevious: jest.fn(),
+        title: "1 of 2",
+        total: 2,
+        currentIndex: 1
+      };
+      StyleSheetTestUtils.suppressStyleInjection();
+      const component = mount(
+        <MuiThemeProvider>
+          <AssignmentTexterContact
+            texter={texterUser}
+            campaign={updatedCampaign}
+            assignment={updatedAssignment}
+            refreshData={jest.fn()}
+            contact={updatedContacts[0]}
+            navigationToolbarChildren={navigationToolbarChildren}
+          />
+        </MuiThemeProvider>
+      );
+    });
+  });
+
+  describe("#updateQuestionResponses", () => {
+    it("does a thing", async () => {
+      expect(true).toBeTruthy();
+    });
   });
 });
