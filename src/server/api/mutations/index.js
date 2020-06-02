f623f118884bf0cdcc9ab58fa92a1eddb2b168b6
Schuyler Duveen <sky@skyb.us>
2020-06-02 12:48:09 -0400

    move joinOrganization to mutation

--- src/server/api/mutations/index.js
+++ src/server/api/mutations/index.js
@@ -3,11 +3,13 @@ import { bulkSendMessages } from "./bulkSendMessages";
 import { findNewCampaignContact } from "./findNewCampaignContact";
 import { updateQuestionResponses } from "./updateQuestionResponses";
 import { buyPhoneNumbers } from "./buyPhoneNumbers";
+import { joinOrganization } from "./joinOrganization";
 
 export {
   sendMessage,
   bulkSendMessages,
   findNewCampaignContact,
+  joinOrganization,
   updateQuestionResponses,
   buyPhoneNumbers
 };
