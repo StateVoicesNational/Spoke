180e0cde88520da4126005ae4202d3f0f1a5fae9
Schuyler Duveen <sky@skyb.us>
2020-06-12 20:27:38 -0400

    sideboxes: settingsData in sidebox properties

--- src/containers/TexterTodo.jsx
+++ src/containers/TexterTodo.jsx
@@ -170,6 +170,7 @@ export class TexterTodo extends React.Component {
         assignment.contacts.map(c => c.id)
       );
       this.loadingNewContacts = true;
+      // TODO: don't run this ever
       const didAddContacts = (
         await this.props.mutations.findNewCampaignContact(assignment.id)
       ).data.findNewCampaignContact.found;
