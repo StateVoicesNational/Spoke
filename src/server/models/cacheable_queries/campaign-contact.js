23ca31163690512b0291feb4a4d92e3def4799c4
Schuyler Duveen <sky@skyb.us>
2020-06-16 16:34:03 -0400

    merge profanity-filter into stage-main-71

--- src/server/models/cacheable_queries/campaign-contact.js
+++ src/server/models/cacheable_queries/campaign-contact.js
@@ -347,13 +347,7 @@ const campaignContactCache = {
       const cellData = await r.redis.getAsync(
         cellTargetKey(cell, messageServiceSid)
       );
-      console.log(
-        "lookupByCell cache",
-        cell,
-        service,
-        messageServiceSid,
-        cellData
-      );
+      // console.log('lookupByCell cache', cell, service, messageServiceSid, cellData)
       if (cellData) {
         // eslint-disable-next-line camelcase
         const [
