c9e1d932d23c68e119a2b3386706397ea10cb740
Jeff Mann <jmann@aflcio.org>
2020-07-06 13:44:07 -0400

    Fix failing tests

--- src/server/models/cacheable_queries/user.js
+++ src/server/models/cacheable_queries/user.js
@@ -119,7 +119,7 @@ const dbLoadUserAuth = async (field, val) => {
     .where(field, val)
     .select("*")
     .first();
-  if (userAuth.extra && typeof userAuth.extra === "string") {
+  if (userAuth && typeof userAuth.extra === "string") {
     userAuth.extra = JSON.parse(userAuth.extra);
   }
   if (r.redis && userAuth) {
