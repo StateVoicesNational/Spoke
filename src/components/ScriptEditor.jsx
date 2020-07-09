5906f434fb5833b19249287057caca13c6924ae6
Hiemanshu Sharma <hiemanshu@gmail.com>
2020-07-09 16:41:22 -0400

    Add character/segment counts to ScriptEditor

--- src/components/ScriptEditor.jsx
+++ src/components/ScriptEditor.jsx
@@ -10,6 +10,7 @@ import {
 import { delimit } from "../lib/scripts";
 import Chip from "./Chip";
 import { red400, green500, green600, grey100 } from "material-ui/styles/colors";
+import { getCharCount } from "@trt2/gsm-charset-utils";
 
 const styles = {
   editor: {
@@ -44,6 +45,21 @@ const styles = {
     padding: 5
   }
 };
+
+const gsmReplacements = [
+  ["‘", "'"],
+  ["’", "'"],
+  ["”", '"'],
+  ["”", '"'],
+  ["“", '"'],
+  ["–", "-"]
+];
+
+const replaceEasyGsmWins = text =>
+  gsmReplacements.reduce(
+    (acc, replacement) => acc.replace(replacement[0], replacement[1]),
+    text
+  );
 
 function findWithRegex(regex, contentBlock, callback) {
   const text = contentBlock.getText();
@@ -195,9 +211,16 @@ class ScriptEditor extends React.Component {
 
   render() {
     const { name } = this.props;
-
+    const text = this.state.editorState.getCurrentContent().getPlainText();
+    const segmentInfo = getCharCount(replaceEasyGsmWins(text));
     return (
       <div>
+        <div style={segmentInfo.charCount > 1600 ? { color: "red" } : {}}>
+          Total characters: {segmentInfo.charCount}
+          {segmentInfo.charCount > 1600 ? (
+            <span> Exceeded SMS maximum </span>
+          ) : null}
+        </div>
         <div style={styles.editor} onClick={this.focus}>
           <Editor
             name={name}
@@ -208,6 +231,14 @@ class ScriptEditor extends React.Component {
           />
         </div>
         {this.renderCustomFields()}
+        <div>
+          Estimated Segments: {segmentInfo.msgCount}
+          <br />
+          Characters left in current segment:{" "}
+          {segmentInfo.msgCount * segmentInfo.charsPerSegment -
+            segmentInfo.charCount}
+          <br />
+        </div>
       </div>
     );
   }
