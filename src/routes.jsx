c0e62daa18702e06ce93dc7f0a56ef8b2b1b10b9
Barbara Atkins <bdatkins@gmail.com>
2020-08-02 13:27:38 -0400

    1591: Remove Opt outs menu

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,7 +4,6 @@ import AdminDashboard from "./components/AdminDashboard";
 import AdminCampaignList from "./containers/AdminCampaignList";
 import AdminCampaignStats from "./containers/AdminCampaignStats";
 import AdminPersonList from "./containers/AdminPersonList";
-import AdminOptOutList from "./containers/AdminOptOutList";
 import AdminIncomingMessageList from "./containers/AdminIncomingMessageList";
 import AdminCampaignEdit from "./containers/AdminCampaignEdit";
 import AdminReplySender from "./containers/AdminReplySender";
@@ -53,7 +52,6 @@ export default function makeRoutes(requireAuth = () => {}) {
             </Route>
           </Route>
           <Route path="people" component={AdminPersonList} />
-          <Route path="optouts" component={AdminOptOutList} />
           <Route path="incoming" component={AdminIncomingMessageList} />
           <Route path="tags" component={Tags} />
           <Route path="settings" component={Settings} />
