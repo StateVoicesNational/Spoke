05da8254b650d69deaea9466b658a79025509b47
Larry Person <lp10011@gmail.com>
2020-08-18 11:23:47 -0400

    Make VAN database mode configurable. Finally.

--- src/extensions/contact-loaders/ngpvan/util.js
+++ src/extensions/contact-loaders/ngpvan/util.js
@@ -1,11 +1,13 @@
 import { getConfig } from "../../../server/api/lib/config";
 
 export const DEFAULT_NGP_VAN_API_BASE_URL = "https://api.securevan.com";
+export const DEFAULT_NGP_VAN_DATABASE_MODE = 0;
 
 export default class Van {
   static getAuth = organization => {
     const appName = getConfig("NGP_VAN_APP_NAME", organization);
     const apiKey = getConfig("NGP_VAN_API_KEY", organization);
+    const databaseMode = getConfig("NGP_VAN_DATABASE_MODE", organization);
 
     if (!appName || !apiKey) {
       throw new Error(
@@ -13,7 +15,9 @@ export default class Van {
       );
     }
 
-    const buffer = Buffer.from(`${appName}:${apiKey}|0`);
+    const buffer = Buffer.from(
+      `${appName}:${apiKey}|${databaseMode || DEFAULT_NGP_VAN_DATABASE_MODE}`
+    );
     return `Basic ${buffer.toString("base64")}`;
   };
 
