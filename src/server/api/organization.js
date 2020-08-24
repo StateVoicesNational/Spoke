4c1d81ae18abb4a75919101f4c0aad2b6e9c9ab1
Adam Greenspan <agreenspan24@gmail.com>
2020-08-24 16:57:05 -0500

    Fully configured

--- src/server/api/organization.js
+++ src/server/api/organization.js
@@ -253,7 +253,10 @@ export const resolvers = {
         let messagingServiceConfigured;
         if (
           manualMessagingServicesEnabled(organization) ||
-          campaignNumbersEnabled(organization)
+          campaignNumbersEnabled(organization) ||
+          getConfig("SKIP_TWILIO_MESSAGING_SERVICE", organization, {
+            truthy: true
+          })
         ) {
           messagingServiceConfigured = true;
         } else {
