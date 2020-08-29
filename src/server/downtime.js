ecae4a510cce9e4c34c2a3dab2a7d544ccdfad18
Schuyler Duveen <sky@skyb.us>
2020-08-28 20:52:45 -0400

    fix downtime in production assetMap conveince

--- src/server/downtime.js
+++ src/server/downtime.js
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import express from "express";
 import { log } from "../lib";
 import renderIndex from "./middleware/render-index";
+import fs from "fs";
+import { existsSync } from "fs";
+import path from "path";
 
 // This server is for when it is in downtime mode and we just statically
 // serve the client app
@@ -17,15 +20,47 @@ app.enable("trust proxy");
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static assets
+if (existsSync(process.env.ASSETS_DIR)) {
+  app.use(
+    "/assets",
+    express.static(process.env.ASSETS_DIR, {
+      maxAge: "180 days"
+    })
+  );
+}
+
+let assetMap = {
+  "bundle.js": "/assets/bundle.js"
+};
+if (process.env.NODE_ENV === "production") {
+  const assetMapData = JSON.parse(
+    fs.readFileSync(
+      // this is a bit overly complicated for the use case
+      // of it being run from the build directory BY claudia.js
+      // we need to make it REALLY relative, but not by the
+      // starting process or the 'local' directory (which are both wrong then)
+      (process.env.ASSETS_DIR || "").startsWith(".")
+        ? path.join(
+            __dirname,
+            "../../../",
+            process.env.ASSETS_DIR,
+            process.env.ASSETS_MAP_FILE
+          )
+        : path.join(process.env.ASSETS_DIR, process.env.ASSETS_MAP_FILE)
+    )
+  );
+  const staticBase = process.env.STATIC_BASE_URL || "/assets/";
+  for (var a in assetMapData) {
+    assetMap[a] = staticBase + assetMapData[a];
+  }
+}
+
 app.use((req, res, next) => {
   if (req.path !== "/downtime") {
     res.redirect(302, "/downtime");
   } else {
-    res.send(
-      renderIndex("", "", {
-        "bundle.js": "/assets/bundle.js"
-      })
-    );
+    res.send(renderIndex("", "", assetMap));
   }
   next();
 });
