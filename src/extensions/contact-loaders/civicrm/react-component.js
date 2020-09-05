3065725a13e0df00b39fb9086eef00b2c6937974
Elliana <me@mause.me>
2020-09-05 10:18:41 +0800

    Cleanup react-component

--- src/extensions/contact-loaders/civicrm/react-component.js
+++ src/extensions/contact-loaders/civicrm/react-component.js
@@ -1,22 +1,10 @@
 import type from "prop-types";
 import React from "react";
-import RaisedButton from "material-ui/RaisedButton";
 import GSForm from "../../../components/forms/GSForm";
 import Form from "react-formal";
-import Subheader from "material-ui/Subheader";
 import { ListItem, List } from "material-ui/List";
-import SelectField from "material-ui/SelectField";
-import MenuItem from "material-ui/MenuItem";
-import CampaignFormSectionHeading from "../../../components/CampaignFormSectionHeading";
-import CheckIcon from "material-ui/svg-icons/action/check-circle";
-import WarningIcon from "material-ui/svg-icons/alert/warning";
-import ErrorIcon from "material-ui/svg-icons/alert/error";
-import { StyleSheet, css } from "aphrodite";
 import yup from "yup";
 import AutoComplete from "material-ui/AutoComplete";
-import FontIcon from "material-ui/FontIcon";
-import ActionFlightTakeoff from "material-ui/svg-icons/action/flight-takeoff";
-
 import LoadingIndicator from "../../../components/LoadingIndicator";
 
 export class CampaignContactsForm extends React.Component {
@@ -40,7 +28,7 @@ export class CampaignContactsForm extends React.Component {
   }
 
   render() {
-    const { clientChoiceData, lastResult } = this.props,
+    const { lastResult } = this.props,
       props = this.props;
     let resultMessage = "";
     if (lastResult && lastResult.result) {
