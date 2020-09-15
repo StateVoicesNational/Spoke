ae52f7f09838baca41c3ea96290cb39814189ccc
Jeff Mann <jmann@aflcio.org>
2020-09-15 17:58:06 -0400

    Only run erroredMessageSender once

--- src/heroku/heroku-dispatch.js
+++ src/heroku/heroku-dispatch.js
@@ -1,6 +1,16 @@
 import { dispatchProcesses } from "../workers/job-processes";
 
-const event = {};
-dispatchProcesses(event).catch(err => {
-  console.log(err);
-});
+const runDispatch = async () => {
+  const event = {
+    maxCount: 2 // maxCount is 2 so erroredMessageSender runs once
+  };
+  dispatchProcesses(event)
+    .catch(err => {
+      console.log(err);
+    })
+    .then(results => {
+      console.log('dispatch results', results);
+      process.exit(0)
+    })
+};
+runDispatch();
