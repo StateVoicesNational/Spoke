4834c2e4a6646b44402cfae8c0f9ffab9d1d9382
Schuyler Duveen <sky@skyb.us>
2020-10-01 10:37:08 -0400

    telemetry: Assignment Dynamic Request

--- src/server/api/mutations/findNewCampaignContact.js
+++ src/server/api/mutations/findNewCampaignContact.js
@@ -28,6 +28,10 @@ export const findNewCampaignContact = async (
     return falseRetVal;
   }
   const campaign = await loaders.campaign.load(assignment.campaign_id);
+  await telemetry.reportEvent("Assignment Dynamic Request", {
+    count: 1,
+    organizationId: campaign.organization_id
+  });
 
   await assignmentRequiredOrAdminRole(
     user,
