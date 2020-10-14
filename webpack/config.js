b9bf877516006939f0f886756570fd99ccdd9cf9
Alon Salant <alon@salant.org>
2020-10-14 09:09:28 -0700

    CSV file upload working in cypress tests

--- webpack/config.js
+++ webpack/config.js
@@ -7,7 +7,8 @@ const DEBUG = process.env.NODE_ENV === "development";
 const plugins = [
   new webpack.DefinePlugin({
     "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
-    "process.env.PHONE_NUMBER_COUNTRY": `"${process.env.PHONE_NUMBER_COUNTRY}"`
+    "process.env.PHONE_NUMBER_COUNTRY": `"${process.env.PHONE_NUMBER_COUNTRY ||
+      "US"}"`
   }),
   new webpack.ContextReplacementPlugin(
     /[\/\\]node_modules[\/\\]timezonecomplete[\/\\]/,
