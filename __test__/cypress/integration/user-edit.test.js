aeee3443654eb10764dcfac2eb42006aa46d125d
Alon Salant <alon@salant.org>
2020-11-13 23:00:49 -0800

    Replace Cypress fixture data file with use of unit data factory methods

--- __test__/cypress/integration/user-edit.test.js
+++ __test__/cypress/integration/user-edit.test.js
@@ -1,12 +1,11 @@
-import TestData from "../fixtures/test-data";
-
 describe("The user edit screen", () => {
+  const adminInfo = { email: "admin@example.com", password: "Admin1!" };
   let admin = null;
 
   beforeEach(() => {
     cy.task("createOrganization").then(org => {
       cy.task("createUser", {
-        userInfo: TestData.users.admin1,
+        userInfo: adminInfo,
         org,
         role: "OWNER"
       }).then(user => (admin = user));
@@ -14,23 +13,16 @@ describe("The user edit screen", () => {
   });
 
   it("displays the current user's and allows them to edit it", () => {
-    cy.login("admin1");
+    cy.login(admin);
     cy.visit("/");
 
-    const userDetails = TestData.users.admin1;
     cy.get("[data-test=userMenuButton]").click();
     cy.get("[data-test=userMenuDisplayName]").click();
-    cy.get("input[data-test=email]").should("have.value", userDetails.email);
-    cy.get("input[data-test=firstName]").should(
-      "have.value",
-      userDetails.first_name
-    );
-    cy.get("input[data-test=lastName]").should(
-      "have.value",
-      userDetails.last_name
-    );
+    cy.get("input[data-test=email]").should("have.value", admin.email);
+    cy.get("input[data-test=firstName]").should("have.value", admin.first_name);
+    cy.get("input[data-test=lastName]").should("have.value", admin.last_name);
     cy.get("input[data-test=alias]").should("have.value", "");
-    cy.get("input[data-test=cell]").should("have.value", userDetails.cell);
+    cy.get("input[data-test=cell]").should("have.value", admin.cell);
 
     cy.get("input[data-test=firstName]").type("NewAdminFirstName");
     cy.get("input[data-test=lastName]").type("NewAdminLastName");
@@ -47,12 +39,5 @@ describe("The user edit screen", () => {
       "NewAdminLastName"
     );
     cy.get("input[data-test=alias]").should("have.value", "NewAlias");
-
-    // revert to previous values so this doesn't interfere with other tests
-    // TODO: it would be better to create a new user for this
-    cy.get("input[data-test=firstName]").type(userDetails.first_name);
-    cy.get("input[data-test=lastName]").type(userDetails.last_name);
-    cy.get("input[data-test=alias]").type("{backspace}");
-    cy.get("[data-test=userEditForm]").submit();
   });
 });
