72152e392cec66513ebd2a06e73ff821dd90ad2d
Stefan Hayden <alt255@gmail.com>
2021-04-10 00:25:38 -0400

    wip

--- src/components/DualNavDataTables/index.jsx
+++
@@ -1,9 +0,0 @@
-import DataTables from "material-ui-datatables";
-import React from "react";
-import withPagination from "../Paginated/withPagination";
-
-const DualNavDataTables = props => (
-  <DataTables {...props} showFooterToolbar={false} />
-);
-
-export default withPagination(DualNavDataTables, true, true);
