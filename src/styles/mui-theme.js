d24186fc14be97aed4990ca354aaf0449cb025b1
Stefan Hayden <alt255@gmail.com>
2021-05-01 15:04:52 -0400

    all old material-ui updated

--- src/styles/mui-theme.js
+++ src/styles/mui-theme.js
@@ -1,10 +1,7 @@
-import getMuiTheme from "material-ui/styles/getMuiTheme";
 import theme from "./theme";
-import { grey400, grey500, darkBlack } from "material-ui/styles/colors";
-import { fade } from "material-ui/utils/colorManipulator";
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const newtheme = createMuiTheme({
+export default createMuiTheme({
   palette: {
     primary: {
       main: theme.colors.green,
@@ -16,28 +13,3 @@ const newtheme = createMuiTheme({
     }
   }
 });
-
-export { newtheme };
-
-const muiTheme = getMuiTheme(
-  {
-    fontFamily: "Poppins",
-    palette: {
-      primary1Color: theme.colors.green,
-      textColor: theme.text.body.color,
-      primary2Color: theme.colors.orange,
-      primary3Color: grey400,
-      accent1Color: theme.colors.orange,
-      accent2Color: theme.colors.lightGray,
-      accent3Color: grey500,
-      alternateTextColor: theme.colors.white,
-      canvasColor: theme.colors.white,
-      borderColor: theme.colors.lightGray,
-      // Used both for placeholder text (against white) and sometimes disabled buttons (often against grey)
-      disabledColor: fade(darkBlack, 0.3)
-    }
-  },
-  { userAgent: "all" }
-);
-
-export default muiTheme;
