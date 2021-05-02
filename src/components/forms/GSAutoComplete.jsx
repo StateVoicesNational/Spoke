70d2a0557a0d32450c82696d70a92821cee4deb3
Stefan Hayden <alt255@gmail.com>
2021-05-01 22:17:11 -0400

    TESTS\!

--- src/components/forms/GSAutoComplete.jsx
+++ src/components/forms/GSAutoComplete.jsx
@@ -1,54 +1,36 @@
 import React from "react";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
-import { dataSourceItem } from "../../components/utils";
 
 import GSFormField from "./GSFormField";
 
 export default class GSAutoComplete extends GSFormField {
   constructor(props) {
     super(props);
-    const propsValue = props.value || {};
-
-    const autoCompleteProps = {
-      ...props
-    };
-    delete autoCompleteProps.choices;
-
-    this.state = {
-      options: this.createMenuItems(props.options),
-      value: propsValue,
-      name: propsValue.label,
-      autoCompleteProps
-    };
   }
-
-  createMenuItems = choices =>
-    choices.map(({ value, label }) => dataSourceItem(label, value));
-
-  findMatchingChoices = valueToLookFor => {
-    const regex = new RegExp(`.*${valueToLookFor}.*`, "i");
-    return this.state.options.filter(item => regex.test(item.text));
-  };
 
   render = () => {
     const {
       label,
       fullWidth,
       placeholder,
-      name,
       value,
       onChange,
-      getOptionLabel
+      getOptionLabel,
+      options
     } = this.props;
     const dataTest = { "data-test": this.props["data-test"] };
     return (
       <Autocomplete
         {...dataTest}
-        name={name}
-        getOptionLabel={getOptionLabel || (option => option.label)}
-        value={this.state.name}
-        options={this.state.options}
+        getOptionLabel={
+          getOptionLabel ||
+          (option => {
+            return option.label;
+          })
+        }
+        value={value}
+        options={options}
         renderInput={params => {
           return (
             <TextField
@@ -60,31 +42,7 @@ export default class GSAutoComplete extends GSFormField {
           );
         }}
         onChange={(event, value) => {
-          // If you're searching but get no match, value is a string
-          // representing your search term, but we only want to handle matches
-          if (typeof value === "object" && value !== null) {
-            const data = value.rawValue;
-            this.setState({ data, name: value.text });
-            onChange({
-              value: data,
-              label: value.text
-            });
-          } else {
-            // if it matches one item, that's their selection
-            const matches = this.findMatchingChoices(value);
-            if (matches.length === 1) {
-              const data = matches[0].rawValue;
-              const searchText = matches[0].text;
-              this.setState({ name: searchText, data });
-              this.props.onChange({
-                value: data,
-                label: searchText
-              });
-            } else {
-              this.setState({ data: undefined });
-              this.props.onChange(undefined);
-            }
-          }
+          onChange(value);
         }}
       />
     );
