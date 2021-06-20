b7c25f638e3753b49d2f457cce48f8fb528ea2a2
Preston Maness <aggroskater@gmail.com>
2021-06-19 23:21:19 -0500

    Remove debug logging lines and old switch statement implementation

--- src/workers/tasks.js
+++ src/workers/tasks.js
@@ -1,7 +1,6 @@
 // Tasks are lightweight, fire-and-forget functions run in the background.
 // Unlike Jobs, tasks are not tracked in the database.
 // See src/extensions/job-runners/README.md for more details
-import { log } from "../lib";
 import serviceMap from "../server/api/lib/services";
 import * as ActionHandlers from "../extensions/action-handlers";
 import { cacheableData } from "../server/models";
@@ -40,16 +39,6 @@ const questionResponseActionHandler = async ({
   previousValue
 }) => {
   const handler = await ActionHandlers.rawActionHandler(name);
-
-  //log.info(`qrah name: ${name}`);
-  //log.info(`qrah organization: ${organization}`);
-  //log.info(`qrah user: ${user}`);
-  //log.info(`qrah questionResponse: ${questionResponse}`);
-  //log.info(`qrah interactionStep: ${interactionStep}`);
-  //log.info(`qrah campaign: ${campaign}`);
-  //log.info(`qrah contact: ${contact}`);
-  //log.info(`qrah wasDeleted: ${wasDeleted}`);
-  //log.info(`qrah previousValue: ${previousValue}`);
 
   if (!wasDeleted) {
     // TODO: clean up processAction interface
