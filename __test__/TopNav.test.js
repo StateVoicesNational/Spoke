c40d62aff30045dbcd600a90d98b4e3857088a6b
Stefan Hayden <alt255@gmail.com>
2021-10-11 14:08:28 -0400

    UTs

--- __test__/TopNav.test.js
+++ __test__/TopNav.test.js
@@ -3,19 +3,25 @@ import { shallow } from "enzyme";
 import { StyleSheetTestUtils } from "aphrodite";
 import IconButton from "@material-ui/core/IconButton";
 import { TopNavBase as TopNav } from "../src/components/TopNav";
+import UserMenu from "../src/containers/UserMenu";
+import { muiTheme } from "./test_helpers";
 
 describe("TopNav", () => {
   it("can render only title", () => {
-    const nav = shallow(<TopNav title="Welcome to my website" />);
-    expect(nav.text()).toEqual(
-      "Welcome to my website<Apollo(withRouter(UserMenu)) />"
+    const nav = shallow(
+      <TopNav muiTheme={muiTheme} title="Welcome to my website" />
     );
+    expect(nav.text()).toContain("Welcome to my website");
     expect(nav.find("Link").length).toBe(0);
   });
 
   it("can render Link to go back", () => {
     const link = shallow(
-      <TopNav title="Welcome" backToURL="/admin/1/campaigns" />
+      <TopNav
+        title="Welcome"
+        muiTheme={muiTheme}
+        backToURL="/admin/1/campaigns"
+      />
     ).find("Link");
     expect(link.length).toBe(1);
     expect(link.prop("to")).toBe("/admin/1/campaigns");
@@ -23,8 +29,10 @@ describe("TopNav", () => {
   });
 
   it("renders UserMenu", () => {
-    const nav = shallow(<TopNav title="Welcome to my website" />);
-    expect(nav.find("Apollo(withRouter(UserMenu))").length).toBe(1);
+    const nav = shallow(
+      <TopNav muiTheme={muiTheme} title="Welcome to my website" />
+    );
+    expect(nav.find(UserMenu).length).toBe(1);
   });
 });
 
