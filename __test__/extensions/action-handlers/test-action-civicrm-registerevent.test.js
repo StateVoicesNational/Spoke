3dede622b9c6558058811e5d9b69f6086664d02d
Peter Murphy <peterkmurphy@gmail.com>
2021-10-26 11:29:40 +1000

    [COVID-24] Intial tests for action handlers.

---
+++ __test__/extensions/action-handlers/test-action-civicrm-registerevent.test.js
@@ -0,0 +1,15 @@
+import {
+  validateActionHandler,
+  validateActionHandlerWithClientChoices
+} from "../../../src/extensions/action-handlers";
+
+import * as HandlerToTest from "../../../src/extensions/action-handlers/civicrm-registerevent";
+
+describe("test-action-civicrm-registerevent", () => {
+  it("passes validation", async () => {
+    expect(() => validateActionHandler(HandlerToTest)).not.toThrowError();
+    expect(() =>
+      validateActionHandlerWithClientChoices(HandlerToTest)
+    ).not.toThrowError();
+  });
+});
