3f9f199df8606c2ab6990a28e0ab95b80ce77c4f
John Twyman <john.twyman@greens.org.au>
2022-01-13 09:02:17 +1100

    (WIP) Use env vars for cost inputs

--- src/extensions/service-managers/campaign-cost-calculator/react-component.js
+++ src/extensions/service-managers/campaign-cost-calculator/react-component.js
@@ -3,81 +3,31 @@ import { css } from "aphrodite";
 import PropTypes from "prop-types";
 import React from "react";
 import Form from "react-formal";
-import * as yup from "yup";
-import DisplayLink from "../../../components/DisplayLink";
-import GSForm from "../../../components/forms/GSForm";
-import GSTextField from "../../../components/forms/GSTextField";
-import GSSubmitButton from "../../../components/forms/GSSubmitButton";
 
-export class OrgConfig extends React.Component {
+export class CampaignStats extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    console.log("campaign-cost-calculator OrgConfig", this.props);
-    const formSchema = yup.object({
-      outboundCost: yup
-        .number()
-        .nullable()
-        .positive(),
-      inboundCost: yup
-        .number()
-        .nullable()
-        .positive(),
-      currency: yup
-        .string()
-        .max(3)
-        .nullable()
-        .uppercase()
-    });
+    console.log("campaign-cost-calculator CampaignStats", this.props);
+    const { outboundCost, inboundCost, totalCost } =
+      this.props.serviceManagerInfo && this.props.serviceManagerInfo.data;
     return (
       <div>
-        Set the currency and cost per inbound and outbound SMS message segment
-        here to show SMS costs in the campaign stats section
-        <GSForm
-          schema={formSchema}
-          defaultValue={this.props.serviceManagerInfo.data}
-          onSubmit={x => {
-            console.log("onSubmit", x);
-            this.props.onSubmit(x);
-          }}
-        >
-          <Form.Field
-            as={GSTextField}
-            label="Cost per outbound SMS segment (eg. 0.0075)"
-            name="outboundCost"
-            fullWidth
-          />
-          <Form.Field
-            as={GSTextField}
-            label="Cost per inbound SMS segment (eg. 0.00049)"
-            name="inboundCost"
-            fullWidth
-          />
-          <Form.Field
-            as={GSTextField}
-            label="Currency (eg. AUD, EUR, USD, etc.)"
-            name="currency"
-            fullWidth
-          />
-          <Form.Submit
-            as={GSSubmitButton}
-            label="Save"
-            style={this.props.inlineStyles.dialogButton}
-          />
-        </GSForm>
+        Outbound cost: {outboundCost} <br />
+        Inbound cost: {inboundCost} <br />
+        Total cost: {totalCost}
       </div>
     );
   }
 }
 
-OrgConfig.propTypes = {
-  organizationId: PropTypes.string,
+CampaignStats.propTypes = {
+  user: PropTypes.object,
+  campaign: PropTypes.object,
   serviceManagerInfo: PropTypes.object,
-  inlineStyles: PropTypes.object,
-  styles: PropTypes.object,
   saveLabel: PropTypes.string,
   onSubmit: PropTypes.func
 };
