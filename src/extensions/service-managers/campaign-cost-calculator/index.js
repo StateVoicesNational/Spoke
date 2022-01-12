3c6bac1b898185e0c871896b1049143df1538e28
John Twyman <john.twyman@greens.org.au>
2022-01-13 09:02:17 +1100

    (WIP) Use env vars for cost inputs

--- src/extensions/service-managers/campaign-cost-calculator/index.js
+++ src/extensions/service-managers/campaign-cost-calculator/index.js
@@ -21,42 +21,23 @@ export async function getCampaignData({
   fromCampaignStatsPage
 }) {
   // called both from edit and stats contexts: editMode==true for edit page
-  //  if (fromCampaignStatsPage) {
-  //    const costs = await r.knex.raw("SELECT SMSCampaignCost(?)", [campaign]);
-  //
-  //    return {
-  //      data: {
-  //        costs: costs
-  //      }
-  //    };
-  //  }
-}
-
-export async function getOrganizationData({ organization, user, loaders }) {
-  const features = getFeatures(organization);
-  const {
-    calcCampaignOutboundCost = null,
-    calcCampaignInboundCost = null,
-    calcCampaignCurrency = null
-  } = features;
-
-  return {
-    data: {
-      inboundCost: organization.features.inboundCost,
-      outboundCost: organization.features.outboundCost,
-      currency: organization.features.currency
-    },
-    fullyConfigured: null
-  };
-}
-
-export async function onOrganizationUpdateSignal({
-  organization,
-  user,
-  updateData
-}) {
-  return {
-    data: updateData,
-    fullyConfigured: true
-  };
+  if (fromCampaignStatsPage) {
+    const outboundUnitCost = getConfig("CAMPAIGN_COST_OUTBOUND");
+    const inboundUnitCost = getConfig("CAMPAIGN_COST_INBOUND");
+    const currency = getConfig("CAMPAIGN_COST_CURRENCY");
+    const costs = await r.knex.raw("SELECT SMSCampaignCost(?, ?, ?)", [
+      campaign,
+      outboundUnitCost,
+      inboundUnitCost
+    ]);
+    return {
+      data: {
+        costs: {
+          outboundCost: "$" + costs.outboundCost + " USD",
+          inboundCost: "$" + costs.inboundCost + " USD",
+          totalCost: "$" + costs.totalCost + " USD"
+        }
+      }
+    };
+  }
 }
