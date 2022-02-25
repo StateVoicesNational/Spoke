cc6fc76b361a5a513d8c18322008b23b847e3946
Weston Ellis <ellisweston112@gmail.com>
2022-02-25 16:22:15 -0700

    Add decryption

--- src/extensions/contact-loaders/ngpvan/util.js
+++ src/extensions/contact-loaders/ngpvan/util.js
@@ -1,4 +1,5 @@
 import { getConfig } from "../../../server/api/lib/config";
+import { getSecret } from "../../secret-manager";
 
 export const DEFAULT_NGP_VAN_API_BASE_URL = "https://api.securevan.com";
 export const DEFAULT_NGP_VAN_DATABASE_MODE = 0;
@@ -7,7 +8,14 @@ export const DEFAULT_NGPVAN_TIMEOUT = 32000;
 export default class Van {
   static getAuth = organization => {
     const appName = getConfig("NGP_VAN_APP_NAME", organization);
-    const apiKey = getConfig("NGP_VAN_API_KEY", organization);
+
+    if (organization.features[NGP_VAN_API_KEY_ENC]) {
+      const dbKey = getConfig("NGP_VAN_API_KEY_ENC", organization);
+      const apiKey = getSecret("ngpVanApiKey", dbKey, organization);
+    } else {
+      const apiKey = getConfig("NGP_VAN_API_KEY", organization);
+    }
+
     const databaseMode = getConfig("NGP_VAN_DATABASE_MODE", organization);
 
     if (!appName || !apiKey) {
