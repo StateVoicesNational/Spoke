742589820cc9373be39fe1daad151f609cfc8c49
Chuck Morris <percworld@gmail.com>
2022-03-01 13:04:00 -0500

    Update src/containers/AdminBulkScriptEditor.jsx
    Co-authored-by: Ilona Brand <theilonabrand@gmail.com>

--- src/containers/AdminBulkScriptEditor.jsx
+++ src/containers/AdminBulkScriptEditor.jsx
@@ -193,8 +193,7 @@ class AdminBulkScriptEditor extends Component {
             />
           </FormControl>
           <p style={{ fontStyle: "italic" }}>
-            Note: the text must be an exact match. For example, mind differing
-            apostrophe characters: <span style={styles.code}>'</span> vs{" "}
+            Note: the text must be an exact match. For example, be careful of single quotes vs. double quotes: <span style={styles.code}>'</span> vs{" "}
             <span style={styles.code}>’</span> )
           </p>
         </Paper>
