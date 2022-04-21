StateVoicesNational/Spoke

commit cb4c2a5792ce91dab7d582c41b637c49a4655676

Fix texter sidebars not showing up in texting view
When viewing all messages as a texter, the `messageStatusFiler` will
be `needsMessageOrResponse`. Most of the texter sideboxes's
implementation of `showSidebox` fails to take that into account.

In (nearly?) all cases they should be using the contact's message status
instead, so this updates the sideboxes to do that.

src/extensions/texter-sideboxes/default-releasecontacts/react-component.js
@@ -23,8 +23,8 @@ export const displayName = () => "Release Contacts";
 
 export const showSidebox = ({
   settingsData,
-  messageStatusFilter,
   assignment,
+  contact,
   campaign,
   finished,
   isSummary
@@ -33,12 +33,13 @@ export const showSidebox = ({
   // Return anything Truth-y to show
   // Return 'popup' to force a popup on mobile screens (instead of letting it hide behind a button)
   return (
-    (assignment.hasContacts || assignment.allContactsCount) &&
     !finished &&
     (campaign.useDynamicAssignment ||
       settingsData.releaseContactsNonDynamicToo) &&
+    (assignment.hasContacts || assignment.allContactsCount) &&
     (settingsData.releaseContactsReleaseConvos ||
-      (messageStatusFilter === "needsMessage" && assignment.unmessagedCount) ||
+      (contact.messageStatus === "needsMessage" &&
+        assignment.unmessagedCount) ||
       (isSummary && assignment.unmessagedCount))
   );
 };
@@ -62,16 +63,17 @@ export class TexterSideboxClass extends React.Component {
   };
 
   render() {
-    const { settingsData, messageStatusFilter, assignment } = this.props;
+    const { contact, settingsData, assignment } = this.props;
     const showReleaseConvos =
       settingsData.releaseContactsReleaseConvos &&
-      ((messageStatusFilter && messageStatusFilter !== "needsMessage") ||
+      (contact.messageStatus !== "needsMessage" ||
         assignment.unrepliedCount ||
         assignment.hasUnreplied);
     return (
       <div>
         {assignment.unmessagedCount ||
-        (messageStatusFilter === "needsMessage" && assignment.hasUnmessaged) ? (
+        (contact.messageStatus === "needsMessage" &&
+          assignment.hasUnmessaged) ? (
           <div className={css(styles.marginBottom)}>
             <div>
               {settingsData.releaseContactsBatchTitle
@@ -124,7 +126,6 @@ TexterSideboxClass.propTypes = {
 
   // parent state
   navigationToolbarChildren: type.object,
-  messageStatusFilter: type.string,
   finished: type.bool
 };
 

src/extensions/texter-sideboxes/celebration-gif/react-component.js
@@ -20,7 +20,7 @@ export const showSidebox = ({
   // Return 'popup' to force a popup on mobile screens (instead of letting it hide behind a button)
   if (
     (contact && finished) ||
-    (messageStatusFilter === "needsMessage" &&
+    (["needsMessage", "needsMessageOrResponse"].includes(messageStatusFilter) &&
       assignment.allContactsCount &&
       assignment.unmessagedCount === 0)
   ) {

src/extensions/texter-sideboxes/contact-reference/react-component.js
@@ -17,13 +17,12 @@ export const showSidebox = ({
   campaign,
   assignment,
   texter,
-  navigationToolbarChildren,
-  messageStatusFilter
+  navigationToolbarChildren
 }) => {
   // Return anything False-y to not show
   // Return anything Truth-y to show
   // Return 'popup' to force a popup on mobile screens (instead of letting it hide behind a button)
-  return contact && messageStatusFilter !== "needsMessage";
+  return contact && contact.messageStatus !== "needsMessage";
 };
 
 export class TexterSidebox extends React.Component {
@@ -95,8 +94,7 @@ TexterSidebox.propTypes = {
 
   // parent state
   disabled: type.bool,
-  navigationToolbarChildren: type.object,
-  messageStatusFilter: type.string
+  navigationToolbarChildren: type.object
 };
 
 export const adminSchema = () => ({

src/extensions/texter-sideboxes/default-dynamicassignment/react-component.js
@@ -28,11 +28,10 @@ export const showSidebox = ({
   if (
     finished &&
     campaign.useDynamicAssignment &&
-    (assignment.hasUnassignedContactsForTexter ||
-      messageStatusFilter === "needsMessage" ||
-      assignment.unmessagedCount) &&
-    (messageStatusFilter === "needsMessage" ||
-      messageStatusFilter === "needsResponse")
+    assignment.hasUnassignedContactsForTexter &&
+    ["needsMessage", "needsMessageOrResponse", "needsResponse"].includes(
+      messageStatusFilter
+    )
   ) {
     return "popup";
   }

src/extensions/texter-sideboxes/default-editinitial/react-component.js
@@ -6,8 +6,8 @@ import GSTextField from "../../../components/forms/GSTextField";
 
 export const displayName = () => "Allow editing of initial messages";
 
-export const showSidebox = ({ contact, messageStatusFilter }) =>
-  contact && messageStatusFilter === "needsMessage";
+export const showSidebox = ({ contact }) =>
+  contact && contact.messageStatus === "needsMessage";
 
 const defaultExpandText = "Sending Initial Messages";
 const defaultMessagePre =
@@ -84,8 +84,7 @@ TexterSidebox.propTypes = {
 
   // parent state
   disabled: type.bool,
-  navigationToolbarChildren: type.object,
-  messageStatusFilter: type.string
+  navigationToolbarChildren: type.object
 };
 
 export const adminSchema = () => ({

src/extensions/texter-sideboxes/hide-media/react-component.js
@@ -31,8 +31,7 @@ TexterSidebox.propTypes = {
 
   // parent state
   disabled: type.bool,
-  navigationToolbarChildren: type.object,
-  messageStatusFilter: type.string
+  navigationToolbarChildren: type.object
 };
 
 export const adminSchema = () => ({});

src/extensions/texter-sideboxes/mobilize-event-shifter/react-component.js
@@ -20,9 +20,9 @@ import {
 
 export const displayName = () => "Mobilize Event Shifter";
 
-export const showSidebox = ({ contact, messageStatusFilter, settingsData }) =>
+export const showSidebox = ({ contact, settingsData }) =>
   contact &&
-  messageStatusFilter !== "needsMessage" &&
+  contact.messageStatus !== "needsMessage" &&
   (settingsData.mobilizeEventShifterBaseUrl ||
     window.MOBILIZE_EVENT_SHIFTER_URL);
 
@@ -221,8 +221,7 @@ TexterSidebox.propTypes = {
 
   // parent state
   disabled: type.bool,
-  navigationToolbarChildren: type.object,
-  messageStatusFilter: type.string
+  navigationToolbarChildren: type.object
 };
 
 export const adminSchema = () => ({

src/extensions/texter-sideboxes/tag-contact/react-component.js
@@ -19,15 +19,14 @@ import withMuiTheme from "../../../containers/hoc/withMuiTheme";
 
 export const displayName = () => "Tagging Contacts";
 
-export const showSidebox = ({ contact, campaign, messageStatusFilter }) => {
+export const showSidebox = ({ contact, campaign }) => {
   // Return anything False-y to not show
   // Return anything Truth-y to show
   // Return 'popup' to force a popup on mobile screens (instead of letting it hide behind a button)
-  // console.log("showsidebox", messageStatusFilter, contact, campaign);
   return (
     contact &&
     !contact.optOut &&
-    messageStatusFilter !== "needsMessage" &&
+    contact.messageStatus !== "needsMessage" &&
     campaign.organization.tags.length
   );
 };
@@ -150,7 +149,6 @@ TexterSideboxBase.propTypes = {
   // parent state
   disabled: type.bool,
   navigationToolbarChildren: type.object,
-  messageStatusFilter: type.string,
   onUpdateTags: type.func
 };
 

commit 3ef4753ce9bb2454bdf39a493fcf2214f462416d

Fix texting view broken when dynamic assignment enabled
When a texter views the texting interface and dynamic assignment is
enabled on a campaign they are assigned to, a null error is thrown
and the client view remains blank. This fixes the issue by adding a
null check.

Regression introduced in cb4c2a5792ce91dab7d582c41b637c49a4655676

src/extensions/texter-sideboxes/default-releasecontacts/react-component.js
@@ -38,7 +38,8 @@ export const showSidebox = ({
       settingsData.releaseContactsNonDynamicToo) &&
     (assignment.hasContacts || assignment.allContactsCount) &&
     (settingsData.releaseContactsReleaseConvos ||
-      (contact.messageStatus === "needsMessage" &&
+      (contact &&
+        contact.messageStatus === "needsMessage" &&
         assignment.unmessagedCount) ||
       (isSummary && assignment.unmessagedCount))
   );