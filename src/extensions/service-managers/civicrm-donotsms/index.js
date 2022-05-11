60c0e511cc56cfd066a27792a3ad724dbf4a207b
John Twyman <john.twyman@greens.org.au>
2022-05-11 10:24:34 +1000

    Make minor improvements based on PR review feedback

--- src/extensions/service-managers/civicrm-donotsms/index.js
+++ src/extensions/service-managers/civicrm-donotsms/index.js
@@ -16,8 +16,6 @@ export const metadata = () => ({
   supportsCampaignConfig: true
 });
 
-// When a contact is opted out
-// ! This is not currently functional, see https://github.com/MoveOnOrg/Spoke/issues/2069
 export async function onOptOut({
   organization,
   contact,
