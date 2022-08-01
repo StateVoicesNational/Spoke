d7a980a00937f2d702485fc51e2686c3d26504f5
Jeff Mann <jmann@aflcio.org>
2022-08-01 10:36:52 -0400

    Add setFeatures to cachable organization data
    Mirros method in cachable campaign; adds automatic encryption

--- src/server/models/cacheable_queries/organization.js
+++ src/server/models/cacheable_queries/organization.js
@@ -2,7 +2,8 @@ import {
   getMessageServiceConfig,
   tryGetFunctionFromService
 } from "../../../extensions/service-vendors/service_map";
-import { getConfig } from "../../api/lib/config";
+import { convertSecret } from "../../../extensions/secret-manager";
+import { getFeatures, getConfig } from "../../api/lib/config";
 import { r } from "../../models";
 
 const cacheKey = orgId => `${process.env.CACHE_PREFIX || ""}org-${orgId}`;
@@ -65,6 +66,69 @@ const organizationCache = {
       }
     }
     return dbResult;
+  },
+  setFeatures: async (id, newFeatures) => {
+    console.log("SET FEATURES");
+    console.log(id);
+    console.log(newFeatures);
+    if (!id || !newFeatures) {
+      return;
+    }
+    const features = await r.knex.transaction(async trx => {
+      const orgDb = await trx("organization")
+        .where("id", id)
+        .first("features");
+      const features = getFeatures(orgDb);
+      let changes = false;
+      for (const [featureName, featureValue] of Object.entries(newFeatures)) {
+        if (
+          // Don't save default values that aren't already overridden.
+          (features.hasOwnProperty(featureName) ||
+            getConfig(featureName) !== featureValue) &&
+          // Don't save Encrypted placeholder.
+          featureValue !== "<Encrypted>"
+        ) {
+          features[featureName] = featureValue;
+          // Automatically encrypt token values when needed.
+          if (featureName.endsWith("_ENCRYPTED")) {
+            features[featureName] = await convertSecret(
+              featureName,
+              orgDb,
+              featureValue
+            );
+          }
+          changes = true;
+        }
+      }
+      // Remove feature properties that have been unset
+      Object.keys(newFeatures)
+        .filter(f => newFeatures[f] === "")
+        .forEach(f => {
+          delete features[f];
+          changes = true;
+        });
+      if (changes) {
+        const featuresString = JSON.stringify(features);
+        await trx("organization")
+          .where("id", id)
+          .update("features", featuresString);
+        if (r.redis) {
+          const orgCache = await r.redis.getAsync(cacheKey(id));
+          if (orgCache) {
+            const orgObj = JSON.parse(orgCache);
+            orgObj.feature = features;
+            orgObj.features = featuresString;
+            await r.redis
+              .multi()
+              .set(cacheKey(id), JSON.stringify(orgObj))
+              .expire(cacheKey(id), 10000)
+              .execAsync();
+          }
+        }
+      }
+      return features;
+    });
+    return features;
   }
 };
 
