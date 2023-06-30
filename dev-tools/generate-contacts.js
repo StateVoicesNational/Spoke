f26b649b3ff1d0c07a93151816e4bf4540e02495
Jeremy Heyer <jheyer159@gmail.com>
2023-06-30 10:22:00 -0400

    fix: generate contacts updated

--- dev-tools/generate-contacts.js
+++ dev-tools/generate-contacts.js
@@ -1,20 +1,34 @@
-import faker from "faker";
+import { faker } from "@faker-js/faker";
 import json2csv from "json2csv";
+import { getFormattedPhoneNumber } from '../src/lib/phone-format'
 
-const fields = ["firstName", "lastName", "cell", "companyName", "city", "zip"];
-const numContacts = 10000;
+const numContacts = 100000;
+
+
+const validPhone = () => {
+  let valid = false
+  let testPhone = ""
+  let attempts = 0
+  while (valid === false && attempts < 1000) {
+    testPhone = faker.phone.number("+1-###-###-####")
+    valid = getFormattedPhoneNumber(testPhone) !== ""
+    attempts += 1
+  }
+  return testPhone
+}
 
 const data = [];
 for (let index = 0; index < numContacts; index++) {
   data.push({
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    cell: `1-${faker.phone.phoneNumberFormat()}`,
-    companyName: faker.company.companyName(),
-    city: faker.address.city(),
-    zip: faker.address.zipCode()
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    cell: validPhone(),
+    zip: faker.location.zipCode({ format: "#####" }),
+    sosId: faker.string.uuid()
   });
 }
 
-const csvFile = json2csv({ data, fields });
+const fields = Object.keys(data[0])
+
+const csvFile = json2csv({ data, fields, quotes: '' });
 console.log(csvFile);
