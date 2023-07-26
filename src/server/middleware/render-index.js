0f9bc34cf6a0a89c8b31ceb8292dd3c693cd9760
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2023-07-26 11:23:31 -0700

    Edits merge conflicts

--- src/server/middleware/render-index.js
+++ src/server/middleware/render-index.js
@@ -38,7 +38,7 @@ export default function renderIndex(html, css, assetMap) {
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=0"/>
     <title>Spoke</title>
     ${externalLinks}
     <style>
