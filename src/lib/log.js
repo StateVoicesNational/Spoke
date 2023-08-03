c22c2c3fb6bd9f21ac035766cec1b8b6504fce08
Jeremy Heyer <jheyer159@gmail.com>
2023-08-03 09:12:52 -0500

    fix(gcp): custom gcp logger

--- src/lib/log.js
+++ src/lib/log.js
@@ -23,18 +23,65 @@ if (isClient()) {
     enableRollbar = true;
     rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
   }
+  let enableGcpLog = process.env.GCP_LOG ? true : false;
 
-  minilog.suggest.deny(
-    /.*/,
-    process.env.NODE_ENV === "development" ? "debug" : "debug"
-  );
+  if (!enableGcpLog) {
+    minilog.suggest.deny(
+      /.*/,
+      process.env.NODE_ENV === "development" ? "debug" : "debug"
+    );
 
-  minilog
-    .enable()
-    .pipe(minilog.backends.console.formatWithStack)
-    .pipe(minilog.backends.console);
+    minilog
+      .enable()
+      .pipe(minilog.backends.console.formatWithStack)
+      .pipe(minilog.backends.console);
 
-  logInstance = minilog("backend");
+    logInstance = minilog("backend");
+  }
+
+  const existingLoggerFunctions = {
+    ERROR: logInstance ? logInstance.error : console.error,
+    INFO: logInstance ? logInstance.log : console.log,
+    DEBUG: logInstance ? logInstance.debug : console.debug,
+    WARNING: logInstance ? logInstance.warn : console.warn,
+  };
+  
+  const LOG_LEVELS = {
+    'error': 4,
+    'warning': 3,
+    'info': 2,
+    'debug': 1
+  };
+ 
+  const createLogFunction = severity => (...objs) => {
+    if (LOG_LEVELS[severity.toLowerCase()] < LOG_LEVELS[(process.env.LOG_LEVEL || 'error').toLowerCase()]) {
+      return;
+    }
+    if (enableGcpLog) {
+      objs.forEach(obj => {
+        if(obj instanceof Error) {
+          console.error(obj);
+        } else {
+          const entry = {
+            severity: severity,
+            message: typeof obj === "object" ? JSON.stringify(obj) : obj,
+          };
+          console.log(JSON.stringify(entry));
+        }
+      });
+    } else {
+      const logger = existingLoggerFunctions[severity];
+      logger(...objs);
+    }
+  };
+  
+  logInstance = {
+    error: createLogFunction('ERROR'),
+    info: createLogFunction('INFO'),
+    debug: createLogFunction('DEBUG'),
+    warn: createLogFunction('WARNING'),
+  };
+  
   const existingErrorLogger = logInstance.error;
   logInstance.error = err => {
     if (enableRollbar) {
