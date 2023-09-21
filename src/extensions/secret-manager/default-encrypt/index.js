39a08967e85d638870ec865f6bbbd8273deb15d3
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2023-09-21 16:03:04 -0700

    Updates pathway to crypto file

--- src/extensions/secret-manager/default-encrypt/index.js
+++ src/extensions/secret-manager/default-encrypt/index.js
@@ -1,4 +1,4 @@
-import { symmetricDecrypt, symmetricEncrypt } from "../crypto";
+import { symmetricDecrypt, symmetricEncrypt } from "../crypto.js";
 
 export async function getSecret(name, token, organization) {
   try {
