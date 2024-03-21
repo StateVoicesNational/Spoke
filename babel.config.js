8eeda33c0270f486185d71591158e893d09fd002
engelhartrueben <engelhartrueben@gmail.com>
2024-03-21 14:43:45 -0400

    update babel.config.js for esm support

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,25 @@
-
 // The .babelrc config file takes priority for app compilation.
 // For running Jest tests, this file takes precedence over the settings in .babelrc
 
 module.exports = {
-    presets: [
-      "@babel/preset-react",
-      "@babel/preset-env",
-      ["@babel/preset-typescript", { allExtensions: true, isTSX: true }]
-    ],
-    only: ["./**/*.js", "./**/*.jsx"],
-    plugins: [
-      "@babel/plugin-proposal-export-default-from",
-      [
-        "@babel/plugin-transform-runtime",
-        {
-          regenerator: true
-        }
-      ]
-    ],
-    env: {
-      dev: {
-        plugins: ["react-hot-loader/babel"]
+  presets: [
+    "@babel/preset-react",
+    "@babel/preset-env",
+    ["@babel/preset-typescript", { allExtensions: true, isTSX: true }]
+  ],
+  only: ["./**/*.js", "./**/*.jsx"],
+  plugins: [
+    "@babel/plugin-proposal-export-default-from",
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        regenerator: true
       }
+    ]
+  ],
+  env: {
+    dev: {
+      plugins: ["react-hot-loader/babel"]
     }
-  };
+  }
+};
