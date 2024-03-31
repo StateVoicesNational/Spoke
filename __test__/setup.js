1427fcfbf4a203cab3df27d15913a0192f6bac29
Larry Person <lp10011@gmail.com>
2024-03-31 17:29:01 -0400

    Close redis after each test suite

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,6 +1,7 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { TextEncoder, TextDecoder } from "util";
+import { r } from "../src/server/models";
 
 Object.assign(global, { TextDecoder, TextEncoder });
 
@@ -8,3 +9,16 @@ configure({ adapter: new Adapter() });
 
 // server/api/campaign.test.js has some long tests so we increase from 5sec default
 jest.setTimeout(15000);
+
+afterAll(async () => {
+  if (!r.redis) {
+    return;
+  }
+
+  try {
+    await r.redis.quit();
+    await r.redis.disconnect();
+  } catch {
+    // eat the exception yum yum
+  }
+});
