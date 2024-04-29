2ea8cb184734cd915abdd17932d9dcc2434b1424
Larry Person <lp10011@gmail.com>
2024-04-28 22:35:49 -0400

    get rid of unused import

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,8 +1,6 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-import { r } from "../src/server/models";
-
 configure({ adapter: new Adapter() });
 
 // server/api/campaign.test.js has some long tests so we increase from 5sec default
