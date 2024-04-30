1050240b528f4c5637f7b88b3f5c7cdeaf60d34b
Ruby Engelhart <engelhartrueben@gmail.com>
2024-04-30 13:01:50 -0400

    adjust jest.timeout from 15000ms to 25000ms

--- __test__/setup.js
+++ __test__/setup.js
@@ -4,4 +4,4 @@ import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 // server/api/campaign.test.js has some long tests so we increase from 5sec default
-jest.setTimeout(15000);
+jest.setTimeout(25000);
