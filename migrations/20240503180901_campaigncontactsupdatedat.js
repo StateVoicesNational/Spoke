816df3f1ef8d2766906f9c8b81eddfd45f8ea9f3
Sophie Waldman <62553142+sjwmoveon@users.noreply.github.com>
2024-05-03 14:36:49 -0400

    Add updated_at trigger for campaign_contact table.

---
+++ migrations/20240503180901_campaigncontactsupdatedat.js
@@ -0,0 +1,37 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+const { onUpdateTrigger } = require('./helpers/index')
+const ON_UPDATE_TIMESTAMP_FUNCTION = `
+  CREATE OR REPLACE FUNCTION on_update_timestamp()
+  RETURNS trigger AS $$
+  BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+  END;
+$$ language 'plpgsql';
+`
+
+const DROP_ON_UPDATE_TIMESTAMP_FUNCTION = `DROP FUNCTION on_update_timestamp`
+
+exports.up = async function(knex) {
+    const isSqlite = /sqlite/.test(knex.client.config.client);
+    if (!isSqlite) {
+        await knex.raw(ON_UPDATE_TIMESTAMP_FUNCTION);
+        await knex.raw(onUpdateTrigger('campaign_contact'));
+    }
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    const isSqlite = /sqlite/.test(knex.client.config.client);
+    if (!isSqlite) {
+        await knex.raw("DROP TRIGGER campaign_contact_updated_at on campaign_contact");
+        await knex.raw(DROP_ON_UPDATE_TIMESTAMP_FUNCTION);
+    }
+};
