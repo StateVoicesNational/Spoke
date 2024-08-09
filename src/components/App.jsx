1cb25fa5cce29f5b1d2ac5a5966921bb59a5f11e
mcverter <mitchell.verter@gmail.com>
2024-08-09 18:45:13 -0400

    better

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { createTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -12,29 +12,41 @@ import ThemeContext from "../containers/context/ThemeContext";
  * parts of the theme object. Here we will take the string,
  * parse it, and merge it with other app theme defaults
  */
-const formatTheme = newTheme => {
+const formatTheme = (newTheme, darkMode) => {
+  console.log('nt', newTheme, darkMode);
   return {
     ...defaultTheme,
-    palette: newTheme.palette
+    palette: {...defaultTheme.palette,
+      ...newTheme.palette,
+    'type': darkMode ? 'dark': 'light'}
   };
 };
 
 const App = ({ children }) => {
+  const [darkMode, setDarkMode] = React.useState(false);
   const [theme, setTheme] = useState(defaultTheme);
-  let muiTheme = createTheme(defaultTheme);
+
+ let defaultThemeWithMode = {...defaultTheme,
+    palette: {...defaultTheme.palette, 'type': darkMode ? 'dark': 'light'}};
+
+
+  let muiTheme = createTheme(defaultThemeWithMode);
   try {
     // if a bad value is saved this will fail.
     muiTheme = createTheme(theme);
   } catch (e) {
     console.error("failed to create theme", theme);
   }
+  const handleToggleDark = () => {
+    setDarkMode(!darkMode)
+  }
   const handleSetTheme = newPalette => {
     if (newPalette === undefined) {
       // happpens when OrganizationWrapper unmounts
-      setTheme(defaultTheme);
+      setTheme(defaultThemeWithMode);
     } else {
       try {
-        const newTheme = formatTheme(newPalette);
+        const newTheme = formatTheme(newPalette, darkMode);
         setTheme(newTheme);
       } catch (e) {
         console.error("Failed to parse theme: ", newPalette);
@@ -42,10 +54,17 @@ const App = ({ children }) => {
     }
   };
 
+  useEffect(()=>{
+    const newTheme = formatTheme(theme, darkMode);
+    console.log('dark effect', darkMode)
+    setTheme(newTheme)
+
+  }, [darkMode])
   return (
     <ThemeContext.Provider value={{ muiTheme, setTheme: handleSetTheme }}>
       <ThemeProvider theme={muiTheme}>
         <CssBaseline />
+        <button onClick={handleToggleDark}>toggle dark</button>
         <div styles={{ height: "100%" }}>{children}</div>
       </ThemeProvider>
     </ThemeContext.Provider>
