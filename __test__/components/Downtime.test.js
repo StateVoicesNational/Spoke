93466929f6780b62d655fbdf0ed491bd8631906d
engelhartrueben <engelhartrueben@gmail.com>
2024-08-28 11:13:01 -0400

    re add @testing-library/jest-dom

--- __test__/components/Downtime.test.js
+++ __test__/components/Downtime.test.js
@@ -3,6 +3,7 @@
  */
 
 import * as React from 'react';
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 import { StyleSheetTestUtils } from "aphrodite";
